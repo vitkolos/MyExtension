f6b1a26efac3d35747636416dbd69f5d528d7604
nicolasrhone <nicolasrhone@users.noreply.github.com>
2015-03-29 15:08:15 +0200

    Create module.config.php

---
+++ config/module.config.php
@@ -0,0 +1,58 @@
+<?php
+return array(
+
+    /**
+     * Your block definition : back-office json configuration file
+     */
+    'blocksDefinition' => array(
+       'buttonToPage' => array(
+            'maxlifeTime' => 60,
+            'definitionFile' => realpath(__DIR__ . "/blocks/") . '/buttonToPage.json'
+        ),
+       'contactBlock' => array(
+            'maxlifeTime' => 60,
+            'definitionFile' => realpath(__DIR__ . "/blocks/") . '/contactBlock.json'
+        ),
+       'form' => array(
+            'maxlifeTime' => 60,
+            'definitionFile' => realpath(__DIR__ . "/blocks/") . '/form.json'
+        ),
+       'simpleContact' => array(
+            'maxlifeTime' => 60,
+            'definitionFile' => realpath(__DIR__ . "/blocks/") . '/simpleContact.json'
+        )       
+    ),
+
+    /**
+     * Template definitions
+     */
+
+    'templates' => array(
+ 		'namespaces' => array(
+			'MyExtension' => realpath(__DIR__ . '/../templates')
+		)
+	),
+    'namespaces_api' => array(
+        'MyExtension',
+    ),
+    
+        /*
+     * Surcharge de la vue index du front
+     */
+    'view_manager' => array(
+        'template_map' => array(
+          'rubedo/index/index' => realpath(__DIR__) . '/../views/index/index.phtml'
+        ),
+    ),
+    
+    
+        /*
+     * Surcharge des traductions
+     */   
+    'localisationfiles' => array(
+        100 => 'extensions/WebTales/MyExtension/rubedo-localization/languagekey/FrontOffice/Blocks/General/labels.json'
+        
+    ),
+
+
+);
