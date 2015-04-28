8b884e0437b468c214b54a4c4e4a0077ff05548b
nicolasrhone <nicolasrhone@users.noreply.github.com>
2015-04-28 17:21:02 +0200

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -20,11 +20,8 @@ return array(
        'simpleContact' => array(
             'maxlifeTime' => 60,
             'definitionFile' => realpath(__DIR__ . "/blocks/") . '/simpleContact.json'
-        ),
-        'searchResults' => array(
-            'maxlifeTime' => 60,
-            'definitionFile' => realpath(__DIR__ . "/blocks/") . '/searchResults.json'
-        )               
+        )
+                 
     ),
 
  
