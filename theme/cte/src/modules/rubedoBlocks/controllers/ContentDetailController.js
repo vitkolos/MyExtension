9bbb241894dc80cb421f154ce10b7b79feb3e761
nicolasrhone <nicolas.rhone@gmail.com>
2015-06-03 15:33:53 +0200

    test

--- theme/cte/src/modules/rubedoBlocks/controllers/ContentDetailController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -135,7 +135,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
                      
                     var options4 = {
                         pageId: $scope.rubedo.current.page.id,
-                        type: me.content.id
+                        type: [me.content.id]
                      };
                      TaxonomyService.getTaxonomyByContentId(options4.pageId, options4.type).then(function(response){
                          if(response.data.success){
