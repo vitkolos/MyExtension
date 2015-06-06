64b4dc48fd881ee7e1d3505ce2a67afa45649d90
nicolasrhone <nicolas.rhone@gmail.com>
2015-06-06 09:32:49 +0200

    suit

--- theme/cte/js/cte.js
+++ theme/cte/js/cte.js
@@ -16,7 +16,7 @@ blocksConfig.form={
 };
 blocksConfig.bg_image={
            "template": "/templates/blocks/bg_image.html",
-          "internalDependencies":["/src/modules/rubedoBlocks/controllers/ImageController.js"]
+          "internalDependencies":["/src/modules/rubedoBlocks/controllers/BgImageController.js"]
 };
 angular.module('rubedoBlocks').filter('cleanUrl', function () {
     return function (input) {
