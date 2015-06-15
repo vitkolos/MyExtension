e39e645ca9059c1b8504a1efc829d1353dcfe600
nicolasrhone <nicolas.rhone@gmail.com>
2015-06-15 17:32:13 +0200

    test

--- theme/cte/src/modules/rubedoFields/rubedoFields.js
+++ theme/cte/src/modules/rubedoFields/rubedoFields.js
@@ -174,14 +174,10 @@
             ];
         } else if (CKEMode=="Basic"){
             myTBConfig=[
-            { name: 'clipboard', groups: [ 'undo' ], items: [ 'Undo', 'Redo' ] },
-           { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic','-', 'RemoveFormat' ] },
-            { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: ['PasteText', '-', 'Undo', 'Redo',"Source"  ] },
-           { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [  'Outdent', 'Indent', 'Blockquote']},
-            { name: 'styles', items: [ 'Format' ] },
-            { name: 'insert', items: [ 'Image', 'YouTube', '-', 'HorizontalRule'] },
-            { name: 'links', items: [ 'Link', "Rubedolink", 'Unlink'] },
-            { name: 'colors', items: [ 'Scayt'] }
+                { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline','Strike', '-', 'RemoveFormat' ] },
+                { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-',  'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock','-','Image']},
+                { name: 'colors', items: [ 'TextColor','BGColor' ,'-', 'Scayt'] },
+                { name: 'styles', items: [ 'Font', 'FontSize' ] }
             ];
         }
         var editorOptions={
