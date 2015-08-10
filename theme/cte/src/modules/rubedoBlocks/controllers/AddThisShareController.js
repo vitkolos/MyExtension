0d682aad0f3c51e18092a20df8397f843b613dc7
nicolasrhone <nicolasrhone@users.noreply.github.com>
2015-08-10 20:43:04 +0200

    Create AddThisShareController.js

---
+++ theme/cte/src/modules/rubedoBlocks/controllers/AddThisShareController.js
@@ -0,0 +1,35 @@
+angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope','$http',function($scope,$http){
+    var me = this;
+    var config = $scope.blockConfig;
+    me.like = config.like == 1;
+    me.disposition = config.disposition;
+    me.class = 'addthis_toolbox';
+    if(me.like){
+        if(config.disposition == 'Horizontal'){
+            me.class += ' addthis_default_style';
+        } else {
+            me.class += ' addthis_floating_style addthis_counter_style addthis-pos';
+        }
+    } else {
+        if(config.disposition == 'Horizontal'){
+            me.class += ' addthis_default_style';
+            if(config.small == 1){
+                me.class += ' addthis_32x32_style'
+            }
+        } else {
+            me.class += ' addthis_floating_style';
+            if(config.small == 0){
+                me.class += ' addthis_16x16_style addthis-pos';
+            } else {
+                me.class += ' addthis_32x32_style addthis-pos'
+            }
+        }
+    }
+    me.shareCounter = 0;
+    me.shareCounter += $http.get("https://cdn.api.twitter.com/1/urls/count.json?url=http://actualites.chemin-neuf.fr/fr/accueil/558bcda545205ebe06c1fd2c/fondation-de-la-fraternite-politique").count;
+    console.log(me.shareCounter);
+    me.loadAddThis = function(){
+        addthis.toolbox('.addthis_toolbox');
+
+    };
+}]);
