79e611e465877488c86b715fe091b37da2118b07
nicolasrhone <nicolasrhone@users.noreply.github.com>
2015-08-11 16:08:07 +0200

    Update AddThisShareController.js

--- theme/cte/src/modules/rubedoBlocks/controllers/AddThisShareController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/AddThisShareController.js
@@ -26,8 +26,8 @@ angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope
         }
     }
 
-    me.shareCounter=0;
     me.shares=function(){
+    me.shareCounter=0;
     addthis.sharecounters.getShareCounts(['facebook', 'twitter'], function(obj) {
         for (var i = 0; i < 2; i++) {
             if(obj[i].service=="twitter") {me.shareCounter+=obj[i].count; }
@@ -36,6 +36,7 @@ angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope
         
     });
 };
+me.shares();
     me.loadAddThis = function(){
         addthis.toolbox('.addthis_toolbox');
         if(me.shareCounter>0) me.shares();
