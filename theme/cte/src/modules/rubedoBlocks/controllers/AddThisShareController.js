6d901e2f42e1038c8f4185617185a0aa7bcad9d2
nicolasrhone <nicolasrhone@users.noreply.github.com>
2015-08-18 15:20:59 +0200

    Update AddThisShareController.js

--- theme/cte/src/modules/rubedoBlocks/controllers/AddThisShareController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/AddThisShareController.js
@@ -1,4 +1,4 @@
-angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope','$http','$location',function($scope,$http,$location){
+angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope','$timeout',function($scope,$timeout){
     var me = this;
     var config = $scope.blockConfig;
     me.like = config.like == 1;
@@ -35,6 +35,9 @@ angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope
         
     });*/
     me.loadAddThis = function(){
-        addthis.toolbox('.addthis_toolbox');
+        
+$timeout(function() {
++        addthis.toolbox('.addthis_toolbox');
++    }, 500);
         };
 }]);
