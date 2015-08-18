492d352524fbdedf4b899f8befa82dbe4a899cf6
nicolasrhone <nicolasrhone@users.noreply.github.com>
2015-08-18 15:18:54 +0200

    Update AddThisShareController.js

--- theme/cte/src/modules/rubedoBlocks/controllers/AddThisShareController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/AddThisShareController.js
@@ -1,4 +1,4 @@
-angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope','$timeout',function($scope,$timeout){
+angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope','$http','$location',function($scope,$http,$location){
     var me = this;
     var config = $scope.blockConfig;
     me.like = config.like == 1;
@@ -7,30 +7,26 @@ angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope
     if(me.like){
         if(config.disposition == 'Horizontal'){
             me.class += ' addthis_default_style';
-        } 
-        else {
+        } else {
             me.class += ' addthis_floating_style addthis_counter_style addthis-pos';
         }
-    } 
-    else {
+    } else {
         if(config.disposition == 'Horizontal'){
             me.class += ' addthis_default_style';
             if(config.small == 1){
-                me.class += ' addthis_32x32_style';
+                me.class += ' addthis_32x32_style'
             }
-        } 
-        else {
+        } else {
             me.class += ' addthis_floating_style';
             if(config.small == 0){
                 me.class += ' addthis_16x16_style addthis-pos';
-            } 
-            else {
+            } else {
                 me.class += ' addthis_32x32_style addthis-pos'
             }
         }
-    };
+    }
 
- /*   me.shareCounter=0;
+    /*me.shareCounter=0;
     addthis.sharecounters.getShareCounts(['facebook', 'twitter'], function(obj) {
         for (var i = 0; i < 2; i++) {
             if(obj[i].service=="twitter") {me.shareCounter+=obj[i].count; }
@@ -39,8 +35,6 @@ angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope
         
     });*/
     me.loadAddThis = function(){
-        /*$timeout(function() {*/
-    +        addthis.toolbox('.addthis_toolbox');
-    +   /* }, 500);*/
+        addthis.toolbox('.addthis_toolbox');
         };
 }]);
