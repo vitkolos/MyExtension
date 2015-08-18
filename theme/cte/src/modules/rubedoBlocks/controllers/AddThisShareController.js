15cf696e3bb62495fcea9696d9c14295e5250d46
nicolasrhone <nicolasrhone@users.noreply.github.com>
2015-08-18 14:55:20 +0200

    Update AddThisShareController.js

--- theme/cte/src/modules/rubedoBlocks/controllers/AddThisShareController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/AddThisShareController.js
@@ -7,7 +7,8 @@ angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope
     if(me.like){
         if(config.disposition == 'Horizontal'){
             me.class += ' addthis_default_style';
-        } else {
+        } 
+        else {
             me.class += ' addthis_floating_style addthis_counter_style addthis-pos';
         }
     } 
@@ -15,14 +16,15 @@ angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope
         if(config.disposition == 'Horizontal'){
             me.class += ' addthis_default_style';
             if(config.small == 1){
-                me.class += ' addthis_32x32_style'
+                me.class += ' addthis_32x32_style';
             }
         } 
         else {
             me.class += ' addthis_floating_style';
             if(config.small == 0){
                 me.class += ' addthis_16x16_style addthis-pos';
-            } else {
+            } 
+            else {
                 me.class += ' addthis_32x32_style addthis-pos'
             }
         }
