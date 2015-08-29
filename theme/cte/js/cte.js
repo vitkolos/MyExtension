85e4ccd0224a0126227c5bd674a6ea155397912c
nicolasrhone <nicolasrhone@users.noreply.github.com>
2015-08-29 16:18:21 +0200

    Update cte.js

--- theme/cte/js/cte.js
+++ theme/cte/js/cte.js
@@ -8,7 +8,7 @@ blocksConfig.facebook={
 };
 blocksConfig.d3Script= {
             "template": "/templates/blocks/d3Script.html",
-            "internalDependencies":["/src/modules/rubedoBlocks/controllers/D3ScriptController.js","/js/datamaps.world.min.js"],
+            "internalDependencies":["/src/modules/rubedoBlocks/controllers/D3ScriptController.js"],
             "externalDependencies":['/components/mbostock/d3/d3.min.js','//cdnjs.cloudflare.com/ajax/libs/topojson/1.6.9/topojson.min.js']
         },
 blocksConfig.buttonToPage={
