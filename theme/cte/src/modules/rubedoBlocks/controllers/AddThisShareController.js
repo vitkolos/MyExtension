f1134cda6e7085d4b202a1bf6a01df14a60470b2
nicolasrhone <nicolas.rhone@gmail.com>
2015-09-02 12:19:14 +0200

    share

--- theme/cte/src/modules/rubedoBlocks/controllers/AddThisShareController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/AddThisShareController.js
@@ -1,4 +1,4 @@
-angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope','$location',function($scope,$location){
+angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope','$timeout','$location',function($scope,$timeout,$location){
     var me = this;
     var config = $scope.blockConfig;
     me.like = config.like == 1;
@@ -26,15 +26,15 @@ angular.module("rubedoBlocks").lazy.controller('AddThisShareController',['$scope
         }
     }
 
-    me.shareCounter=0;
+    /*me.shareCounter=0;
     addthis.sharecounters.getShareCounts(['facebook', 'twitter'], function(obj) {
         for (var i = 0; i < 2; i++) {
             if(obj[i].service=="twitter") {me.shareCounter+=obj[i].count; }
             else if(obj[i].service=="facebook") me.shareCounter+=obj[i].share.total_count;
         }
         
-    });
-    me.loadAddThis = function(){
+    });*/
+    $timeout(function() {
         addthis.toolbox('.addthis_toolbox');
-        };
+    }, 500);
 }]);
