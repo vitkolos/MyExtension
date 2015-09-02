c4d8a5cc6c90a2ad5557880cdedf42164a4f290c
nicolasrhone <nicolas.rhone@gmail.com>
2015-09-02 13:21:42 +0200

    fix

--- theme/cte/js/cte.js
+++ theme/cte/js/cte.js
@@ -164,7 +164,7 @@ angular.module('rubedoBlocks').directive('addthisToolbox', ['$timeout','$locatio
 	  link : function($scope, element, attrs) {
 		  $timeout(function () {
         addthis.init();
-        addthis.counter();
+        addthis.counter(angular.element('.addthis_toolbox'));
         var contentUrl = $location.absUrl();
         addthis.toolbox(angular.element('.addthis_toolbox').get(), {}, {
           url: contentUrl,
