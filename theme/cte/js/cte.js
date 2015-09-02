4022d515774e9ccd8d5efa4876cad979653b9df7
nicolasrhone <nicolas.rhone@gmail.com>
2015-09-02 13:06:32 +0200

    test

--- theme/cte/js/cte.js
+++ theme/cte/js/cte.js
@@ -154,17 +154,19 @@ angular.module('rubedoBlocks').directive('loadModal', function () {
             }, true);
          }}
   });
+  
 angular.module('rubedoBlocks').directive('addthisToolbox', ['$timeout','$location', function($timeout,$location) {
   return {
     restrict : 'A',
 	  transclude : true,
 	  replace : true,
 	  template : '<div ng-transclude></div>',
-	  link : function($scope, element, attrs,$location) {
-		  $timeout(function ($location) {
+	  link : function($scope, element, attrs) {
+		  $timeout(function () {
         addthis.init();
+        var contentUrl = $location.absUrl();
         addthis.toolbox(angular.element('.addthis_toolbox').get(), {}, {
-          url: $location.absUrl(),
+          url: contentUrl,
           title : attrs.title,
           description : 'Checkout this awesome post on blog.me'        
         });
