679c4d4ee7f53052ac528291a942fa90c3bbfad6
nicolasrhone <nicolas.rhone@gmail.com>
2015-09-07 09:46:45 +0200

    suite

--- theme/cte/js/cte.js
+++ theme/cte/js/cte.js
@@ -170,7 +170,8 @@ angular.module('rubedoBlocks').directive('addthisToolbox', ['$timeout','$locatio
                                  title : attrs.title,
                                  description : ''        
                       });
-           addthis.layers.refresh();
+                      if (addthis.layers.refresh){
+           addthis.layers.refresh();};
            $timeout(function (){addthis.counter(angular.element('.addthis_counter').get());},100);
            addthis.sharecounters.getShareCounts({service: ['facebook','twitter'], countUrl: $location.absUrl()}, function(obj) {
                       console.log(obj)
