f3090fe9d4e132d66830fe13bf86d4609c41c823
nicolasrhone <nicolasrhone@users.noreply.github.com>
2015-09-08 10:52:45 +0200

    Update cte.js

--- theme/cte/js/cte.js
+++ theme/cte/js/cte.js
@@ -28,7 +28,7 @@ blocksConfig.footer={
 };
 blocksConfig.contentDetail = {
             "template": "/templates/blocks/contentDetail.html",
-            "externalDependencies":['//s7.addthis.com/js/300/addthis_widget.js'],
+            "externalDependencies":['//s7.addthis.com/js/300/addthis_widget.js#async=1'],
             "internalDependencies":["/src/modules/rubedoBlocks/controllers/ContentDetailController.js","/src/modules/rubedoBlocks/directives/DisqusDirective.js"]
         },
 angular.module('rubedoBlocks').filter('cleanUrl', function () {
