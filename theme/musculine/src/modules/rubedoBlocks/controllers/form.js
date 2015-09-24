305a63f9fa9f02c32f0160252be9f33b9aeff569
nicolasrhone <nicolas.rhone@gmail.com>
2015-09-24 14:44:20 +0200

    fix

--- theme/musculine/src/modules/rubedoBlocks/controllers/form.js
+++ theme/musculine/src/modules/rubedoBlocks/controllers/form.js
@@ -85,7 +85,7 @@ angular.module("rubedoBlocks").lazy.controller('FormController',['$scope','$http
         me.contents['MUS700T'].quantite = me.big_trad;
         me.contents['MUS700O'].quantite = me.big_or;
         var prix = me.totalPrice() ;
-        if (prix == 0.00 ) {
+        if (prix <= 0 ) {
             alert("Votre panier est vide");
         }
         else if (!valide) {
