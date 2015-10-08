b40f61f55597ac35cceb06c75db32f0d193c4459
nicolasrhone <nicolas.rhone@gmail.com>
2015-10-08 16:11:08 +0200

    test

--- theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
@@ -1,4 +1,19 @@
 angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope','RubedoContentsService',function($scope,RubedoContentsService){
     var me = this;
-    var config = $scope.blockConfig;
+    me.formId = $scope.content.fields.formulaire;
+    me.propositionId = $scope.content.id;
+    me.propositionTitle = $scope.content.text;
+    //pour récupérer les champs du formulaire
+    me.getFormulaire = function (contentId){
+        RubedoContentsService.getContentById(contentId).then(function(response){
+            if (response.data.success){
+                me.form = response.data.content;
+            }
+        });
+    };
+
+    
+    
+    
+    
  }]);
