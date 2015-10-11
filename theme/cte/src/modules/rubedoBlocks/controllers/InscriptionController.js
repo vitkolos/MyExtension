4b9d6ab2d5dc4be01e8e6a6492e0934a516080e3
nicolasrhone <nicolas.rhone@gmail.com>
2015-10-11 19:04:14 +0200

    validation

--- theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
@@ -51,8 +51,16 @@ angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope'
         
         return value;
     };
+    // VALIDATIONS
     
-
+    //telephones
+    $scope.isTelephoneRequired = function () {
+        if(inscription.public_type == 'adolescent')
+            return !($scope.inscription.tel1 || $scope.inscription.tel2 || $scope.inscription.tel2Pers2);
+        else
+            return  !($scope.inscription.tel1 || $scope.inscription.tel2);
+    };
+    
 
 
     
