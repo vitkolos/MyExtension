8e569cc3c0746f150be55ad0ae0068e9b51cc979
nicolasrhone <nicolas.rhone@gmail.com>
2015-10-16 18:37:31 +0200

    fi

--- theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
@@ -132,7 +132,7 @@ angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope'
             else if (step==6) {
                 
                 InscriptionService.inscrire($scope.inscription, "556088a945205e36757e688f").then(function(response){
-                    console.log(reponse);
+                    console.log(response);
             });
                 
                 
