9ab874e507e32091ad662abbf6d203340ae99317
nicolasrhone <nicolas.rhone@gmail.com>
2015-10-19 18:31:59 +0200

    css puce

--- src/MyExtension/Rest/V1/InscriptionResource.php
+++ src/MyExtension/Rest/V1/InscriptionResource.php
@@ -1,26 +1,18 @@
 <?php
-namespace MyExtension\Rest\V1;
+namespace RubedoAPI\Rest\V1;
+
+use RubedoAPI\Rest\V1\AbstractResource;
+use Rubedo\Services\Manager;
 use RubedoAPI\Entities\API\Definition\FilterDefinitionEntity;
 use RubedoAPI\Entities\API\Definition\VerbDefinitionEntity;
-use RubedoAPI\Rest\V1\AbstractResource;
-use Zend\View\Model\JsonModel;
-use Rubedo\Collection\AbstractLocalizableCollection;
-use Rubedo\Services\Manager;
-use RubedoAPI\Exceptions\APIAuthException;
-use RubedoAPI\Exceptions\APIEntityException;
-use RubedoAPI\Exceptions\APIRequestException;
 use WebTales\MongoFilters\Filter;
-
-
-
-use Rubedo\Interfaces\Collection\IAbstractCollection;
-
-class InscriptionResource extends AbstractResource {
-
-    protected $_dataService;
-    protected $_collectionName;
-
-    
+class InscriptionResource extends AbstractResource
+{
+    /**
+     * native config for this payment means
+     *
+     * @var array
+     */
     public function __construct()
     {
         parent::__construct();
@@ -46,95 +38,65 @@ class InscriptionResource extends AbstractResource {
                         (new FilterDefinitionEntity())
                             ->setDescription('Résultat de l\'inscription')
                             ->setKey('result')
-                    )
-                    ;
+                    );
             });
     }
-    public function postAction($params) {
+    public function postAction($params)
+    {
+        $id = "5625176445205e6b03832548"; // id du contenu "Numéro d'inscription"
+        $language = preg_replace('%^/(\w+?)/.*$%', '$1', $_SERVER["REQUEST_URI"]);
+        //authentication
+        $auth = $this->getAuthAPIService()->APIAuth('admin_inscriptions', '2qs5F7jHf8KD');
+        $output['token'] = $this->subTokenFilter($auth['token']);
+        $token = $output['token']['access_token'];
+        //GET NUMERO D'INSCRIPTION ACTUEL
+        $inscription = $this->getContentsCollection()->findById($id, true, false);
+        $inscriptionNumber = (int)$inscription['fields']['value'] +1;
+        
+        $inscription['fields']['value'] = (string)$inscriptionNumber;
+        
+        //UPDATE NUMERO D'INSCRIPTION
+        $payload = json_encode( array( "content" => $inscription ) );
+        $curl = curl_init();
+        // Set some options - we are passing in a useragent too here
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL =>'http://' . $_SERVER['HTTP_HOST'] . '/api/v1/contents/'.$id.'?access_token='.$token.'&lang=fr'
+        ));
+        curl_setopt($curly, CURLOPT_FOLLOWLOCATION, true);  // Follow the redirects (needed for mod_rewrite)
+        curl_setopt($curly, CURLOPT_FRESH_CONNECT, true);   // Always ensure the connection is fresh
+        curl_setopt( $curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($curl, CURLOPT_ENCODING, 'windows-1252');
+        $result = curl_exec($curl);
+    // Close request to clear up some resources
+        curl_close($curl);
+        
+        
+        
+        
+        
+        
+        
+        
+        return array('success' => true, 'result'=>$result, 'message' =>$inscriptionNumber );
+    
+   }
+   
+   
+   
 
     
-    $query = array();
-    // Prepare query string
-    $query_string = http_build_query($query);
-
-    //authentification
-    //$response = $this->getAuthAPIService()->APIAuth('musculine', 'Musc2015');
-    $response = $this->getAuthAPIService()->APIAuth('admin_inscriptions', '2qs5F7jHf8KD');
-    $output['token'] = $this->subTokenFilter($response['token']);
-    $route['access_token'] = $output['token']['access_token'];
-
-    //create order
-    $data["fields"] = $params['inscription'];
-    $data["writeWorkspace"] = $params['workspace'];
-    $data["typeId"] = "561627c945205e41208b4581";
-    $idNumber = $this->getInscriptionId();
-    //$data["fields"]["text"] = "FR".(string)$this->getInscriptionId();
-    $data["fields"]["text"] = "FR100001";
-    //$data["fields"]["commande"] = $params['products'];
-    
-    
-    
-    
-    $payload = json_encode( array( "content" => $data ) );
-
-    $curl = curl_init();
-    // Set some options - we are passing in a useragent too here
-    curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL =>'http://' . $_SERVER['HTTP_HOST'] . '/api/v1/contents?access_token='.$route['access_token'].'&lang=fr',
-        CURLOPT_POST => 1
-    ));
-    curl_setopt($curly, CURLOPT_FOLLOWLOCATION, true);  // Follow the redirects (needed for mod_rewrite)
-    curl_setopt($curly, CURLOPT_FRESH_CONNECT, true);   // Always ensure the connection is fresh
-    curl_setopt( $curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $payload );
-    curl_setopt($curl, CURLOPT_ENCODING, 'windows-1252');
-    $result = curl_exec($curl);
-// Close request to clear up some resources
-    curl_close($curl);
-    
-    $inscriptionInfo = $this->getInscriptionId();
-    return array(
-            'result' =>  $inscriptionInfo,
-            'success' => true,
-            'message' => $result
-        );
-
-    }
     protected function subTokenFilter(&$token)
     {
         return array_intersect_key($token, array_flip(array('access_token', 'refresh_token', 'lifetime', 'createTime')));
     }
+    
 
-    
-     
-    
-    public function getPaymentMeans($id){
-            $contentId = (string)$id;
-        $className = (string)get_class($this);
+   
+}     
 
-        $this->_dataService = Manager::getService('MongoDataAccess');
-        $this->_dataService->init("Contents");
-        $content = $this->_dataService->findById($id);
-        if (empty($content)) {
-            throw new APIEntityException('Content not found', 404);
-        }
-        return $content['live']['fields'];
-    }
-    public function getInscriptionId(){
-        $id="558d586c45205ee07cc1fd3c";
-        $content = $this->getContentsCollection()->findById($id, true, false);
-        
-        //idNumbr ++
-        $data=array();
-        $data['fields'] = array();
-        $data['fields']['value'] = (int)$content['fields']['value']+1;
-        //$content = array_replace_recursive($content, $data);
-        //$update = $this->getContentsCollection()->update($content, array(), true);
-        return $content;
-    
-    
-    }
-}
+
 
 
