fa68e296e7674f79c3c9953a257a9287d59bccd4
nicolasrhone <nicolas.rhone@gmail.com>
2015-10-19 18:50:27 +0200

    inscription

--- src/MyExtension/Rest/V1/InscriptionResource.php
+++ src/MyExtension/Rest/V1/InscriptionResource.php
@@ -74,13 +74,29 @@ class InscriptionResource extends AbstractResource
         curl_close($curl);
         
         
-        
-        
-        
-        
-        
-        
-        return array('success' => true, 'result'=>$result, 'message' =>$inscriptionNumber );
+        //CREATE INSCRIPTION
+    $inscriptionForm =  $params['inscription'];
+    $inscriptionForm['fields']['text'] = "FR-".(string)$inscriptionNumber;
+    $inscriptionForm['writeWorkspace'] = $params['workspace']
+    $payload2 = json_encode( array( "content" => $inscriptionForm ) );
+
+    $curl = curl_init();
+    // Set some options - we are passing in a useragent too here
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL =>'http://' . $_SERVER['HTTP_HOST'] . '/api/v1/contents?access_token='.$route['access_token'].'&lang=fr',
+        CURLOPT_POST => 1
+    ));
+    curl_setopt($curly, CURLOPT_FOLLOWLOCATION, true);  // Follow the redirects (needed for mod_rewrite)
+    curl_setopt($curly, CURLOPT_FRESH_CONNECT, true);   // Always ensure the connection is fresh
+    curl_setopt( $curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $payload );
+    curl_setopt($curl, CURLOPT_ENCODING, 'windows-1252');
+    $resultInscription = curl_exec($curl);
+// Close request to clear up some resources
+    curl_close($curl);
+
+    return array('success' => true, 'result'=>$resultInscription, 'message' =>$inscriptionNumber );
     
    }
    
