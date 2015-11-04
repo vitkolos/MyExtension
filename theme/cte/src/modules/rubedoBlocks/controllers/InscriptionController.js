148a54ac03c9f9e6f1b139f8b92dd14b5b62c8b2
nicolasrhone <nicolas.rhone@gmail.com>
2015-11-04 09:49:32 +0100

    fix ctrl

--- theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
@@ -201,7 +201,7 @@ angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope'
     $scope.isTelephoneRequired = function () {
         if($scope.inscription.public_type == 'adolescent')
             return !($scope.inscription.tel1 || $scope.inscription.tel2 || $scope.inscription.tel2Pers2); // au moins téléphone fixe / portable / parent
-        else if ($scope.inscription.public_type == 'couple' || $scope.inscription.public_type='famille') {
+        else if ($scope.inscription.public_type == 'couple' || $scope.inscription.public_type=='famille') {
             return !($scope.inscription.tel2 || $scope.inscription.tel2Pers2 || $scope.inscription.tel2Pers2); // au moins portable de lui / elle / téléphone fixe
         }
         else
