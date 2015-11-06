68a59c07e4df32e3f206eb7e5f9826e46d23a986
nicolasrhone <nicolas.rhone@gmail.com>
2015-11-06 18:16:51 +0100

    fiux

--- theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
@@ -64,7 +64,7 @@ angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope'
                 });
 
                 // check infos complémentaires
-                if ((me.form.fields.questions1) && me.form.fields.questions1 && ((me.form.fields.questions1.questions1).length>0)) {
+                if ((me.form.fields.questions1) && me.form.fields.questions1.questions1 && ((me.form.fields.questions1.questions1).length>0)) {
                     angular.forEach(me.form.fields.questions1.questions1, function(option, key){
                         me.form[option] = true;
                         console.log();
