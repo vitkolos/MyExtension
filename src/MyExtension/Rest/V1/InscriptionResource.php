fe9327a5e9b0bc23104667e3c7bdcf4c61165bfb
nicolasrhone <nicolas.rhone@gmail.com>
2015-11-06 18:02:48 +0100

    fix

--- src/MyExtension/Rest/V1/InscriptionResource.php
+++ src/MyExtension/Rest/V1/InscriptionResource.php
@@ -78,7 +78,7 @@ class InscriptionResource extends AbstractResource
     
        $resultInscription = $this->callAPI("POST", $token, $payload2);
     
-        return array('success' => true, 'id' =>$resultInscription);
+        return array('success' => $resultInscription['success'], 'id' =>$inscriptionNumber);
         
    }
    
--- theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
@@ -281,6 +281,7 @@ angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope'
             }
             InscriptionService.inscrire($scope.inscription, $scope.rubedo.current.page.workspace).then(function(response){
                 if (response.success) {
+                    $scope.processForm=false;
                     if ($scope.inscription.modePaiement=='carte') { // paiement par Paybox
                         var payload = {
                             name:''
