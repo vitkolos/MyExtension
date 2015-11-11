1def00f60180b2108d1d30e8cda9f5ca67160cb4
nicolasrhone <nicolas.rhone@gmail.com>
2015-11-11 09:42:15 +0100

    fix

--- src/MyExtension/Rest/V1/PayboxIpnResource - Copie.php
+++ src/MyExtension/Rest/V1/PayboxIpnResource - Copie.php
@@ -8,7 +8,7 @@ use RubedoAPI\Exceptions\APIControllerException;
 use WebTales\MongoFilters\Filter;
 
 
-class PayboxIpnResource extends AbstractResource {
+class PayboxIpn2Resource extends AbstractResource {
     protected $returnedEntityFields = array(
         'fields'
     );
