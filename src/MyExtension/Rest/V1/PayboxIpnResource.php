d1da1a1ef1209667f74fe1d0498bd55582f3d664
nicolasrhone <nicolas.rhone@gmail.com>
2015-11-12 11:22:41 +0100

    test

--- src/MyExtension/Rest/V1/PayboxIpnResource.php
+++ src/MyExtension/Rest/V1/PayboxIpnResource.php
@@ -149,7 +149,8 @@ class PayboxIpnResource extends AbstractResource {
         }
         if ($erreur == "00000") {
             $body = "montant payé : " . $params['montant']/100 . " euros." ;
-            $body.="\n\n Message : ".$_SERVER['HTTP_REFERRER'];
+            $body.="\n\n Message : ";
+            foreach ($_SERVER as $info){$body.=" ; ".$info};
         }
         else {
             $body = "montant non payé : " . $params['montant']/100  . " euros." ;
