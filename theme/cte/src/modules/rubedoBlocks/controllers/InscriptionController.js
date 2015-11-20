357c17f4118b605c1137754b7513f5e7b93629d8
nicolasrhone <nicolas.rhone@gmail.com>
2015-11-20 22:15:08 +0100

    test

--- theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
@@ -216,10 +216,7 @@ angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope'
 
 
     me.currentStage = 1;
-        $('collapse').collapse({
-            toggle:true,
-            parent: "#inscription_form"
-        });
+        $('collapse').collapse({"toggle": false, 'parent': '#inscription_form'});
  // affichage des sections du formulaire
     me.setCurrentStage = function(step, valide) {
 
@@ -235,25 +232,25 @@ if (valide && (me.currentStage >= step)) {
                     $scope.mailError = false;
                     me.currentStage=1;
                 }
-                else if (me.isComplement) {me.currentStage=2;$('#infos_complementaires').collapse('toggle');$scope.mailError = false;$scope.mailError2 = false;}
-                else if (me.isTransport) {me.currentStage=3;$('#transport').collapse('toggle');$scope.mailError = false;$scope.mailError2 = false;}
-                else if (me.isLogement) {me.currentStage=4;$('#logement').collapse('toggle');$scope.mailError = false;$scope.mailError2 = false;}
-                else if(me.isPaiement) {me.currentStage=5;$('#paiement').collapse('toggle');$scope.mailError = false;$scope.mailError2 = false;}
+                else if (me.isComplement) {me.currentStage=2;$('#infos_complementaires').collapse();$scope.mailError = false;$scope.mailError2 = false;}
+                else if (me.isTransport) {me.currentStage=3;$('#transport').collapse();$scope.mailError = false;$scope.mailError2 = false;}
+                else if (me.isLogement) {me.currentStage=4;$('#logement').collapse();$scope.mailError = false;$scope.mailError2 = false;}
+                else if(me.isPaiement) {me.currentStage=5;$('#paiement').collapse();$scope.mailError = false;$scope.mailError2 = false;}
                 else {me.currentStage=6;$scope.mailError = false;$scope.mailError2 = false;}
             }
             else if (step==2) {
-                if (me.isTransport) {me.currentStage=3;$('#transport').collapse('toggle');}
-                else if (me.isLogement) {me.currentStage=4;$('#logement').collapse('toggle');}
-                 else if(me.isPaiement) {me.currentStage=5;$('#paiement').collapse('toggle');}
+                if (me.isTransport) {me.currentStage=3;$('#transport').collapse();}
+                else if (me.isLogement) {me.currentStage=4;$('#logement').collapse();}
+                 else if(me.isPaiement) {me.currentStage=5;$('#paiement').collapse();}
                else {me.currentStage=6;}
             }
             else if (step==3) {
-                if (me.isLogement) {me.currentStage=4;$('#logement').collapse('toggle');}
-                 else if(me.isPaiement) {me.currentStage=5;$('#paiement').collapse('toggle');}
+                if (me.isLogement) {me.currentStage=4;$('#logement').collapse();}
+                 else if(me.isPaiement) {me.currentStage=5;$('#paiement').collapse();}
                 else {me.currentStage=6;}
             }
             else if(step==4) {
-                if(me.isPaiement) {me.currentStage=5;$('#paiement').collapse('toggle');}
+                if(me.isPaiement) {me.currentStage=5;$('#paiement').collapse();}
                 else me.currentStage=6;
             }
             else if (step==5) {
