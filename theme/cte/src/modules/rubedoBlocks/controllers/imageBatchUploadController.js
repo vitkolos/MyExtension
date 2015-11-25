2eca2177476a576c92fbabcbcc177475f391bb25
nicolasrhone <nicolas.rhone@gmail.com>
2015-11-25 18:37:56 +0100

    test

--- theme/cte/src/modules/rubedoBlocks/controllers/imageBatchUploadController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/imageBatchUploadController.js
@@ -44,23 +44,42 @@ angular.module("rubedoBlocks").lazy.controller('ImageBatchUploadController',['$s
 
     };    
     
-angular.module("rubedoBlocks").directive("fileread", [function () {
+angular.module('rubedoBlocks').directive('appFilereader', function($q) {
+    var slice = Array.prototype.slice;
+
     return {
-        scope: {
-            fileread: "="
-        },
-        link: function (scope, element, attributes) {
-            element.bind("change", function (changeEvent) {
-                scope.$apply(function () {
-                    //scope.fileread = changeEvent.target.files[0];
-                    // or all selected files:
-                    scope.fileread = changeEvent.target.files;
-                });
-            });
-        }
-    }
-}]);
-    
+        restrict: 'A',
+        require: '?ngModel',
+        link: function(scope, element, attrs, ngModel) {
+                if (!ngModel) return;
+
+                ngModel.$render = function() {};
+
+                element.bind('change', function(e) {
+                    var element = e.target;
+
+                    $q.all(slice.call(element.files, 0).map(readFile))
+                        .then(function(values) {
+                            if (element.multiple) ngModel.$setViewValue(values);
+                            else ngModel.$setViewValue(values.length ? values[0] : null);
+                        });
+
+                    function readFile(file) {
+                        var deferred = $q.defer();
+
+                        var reader = new FileReader();
+                        reader.onload = function(e) {
+                            deferred.resolve(e.target.result);
+                        };
+                        reader.onerror = function(e) {
+                            deferred.reject(e);
+                        };
+                        reader.readAsDataURL(file);
+
+                        return deferred.promise;
+                    }
+
+                }); //change
     
     
     
