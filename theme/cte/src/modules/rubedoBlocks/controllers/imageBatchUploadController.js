740931443a0abc443b010ce8fcdafc1f9be337c7
nicolasrhone <nicolas.rhone@gmail.com>
2015-11-25 22:58:52 +0100

    fix

--- theme/cte/src/modules/rubedoBlocks/controllers/imageBatchUploadController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/imageBatchUploadController.js
@@ -42,48 +42,48 @@ angular.module("rubedoBlocks").lazy.controller('ImageBatchUploadController',['$s
        }*/
 
     };
-        var dropbox = document.getElementById("dropbox")
-    $scope.dropText = 'Drop files here...'
+    var dropbox = angular.element("#dropbox");
+    $scope.dropText = 'Drop files here...';
 
     // init event handlers
     function dragEnterLeave(evt) {
-        evt.stopPropagation()
-        evt.preventDefault()
+        evt.stopPropagation();
+        evt.preventDefault();
         $scope.$apply(function(){
-            $scope.dropText = 'Drop files here...'
-            $scope.dropClass = ''
-        })
+            $scope.dropText = 'Drop files here...';
+            $scope.dropClass = '';
+        });
     }
-    dropbox.addEventListener("dragenter", dragEnterLeave, false)
-    dropbox.addEventListener("dragleave", dragEnterLeave, false)
+    dropbox.addEventListener("dragenter", dragEnterLeave, false);
+    dropbox.addEventListener("dragleave", dragEnterLeave, false);
     dropbox.addEventListener("dragover", function(evt) {
-        evt.stopPropagation()
-        evt.preventDefault()
-        var clazz = 'not-available'
-        var ok = evt.dataTransfer && evt.dataTransfer.types && evt.dataTransfer.types.indexOf('Files') >= 0
+        evt.stopPropagation();
+        evt.preventDefault();
+        var clazz = 'not-available';
+        var ok = evt.dataTransfer && evt.dataTransfer.types && evt.dataTransfer.types.indexOf('Files') >= 0;
         $scope.$apply(function(){
-            $scope.dropText = ok ? 'Drop files here...' : 'Only files are allowed!'
-            $scope.dropClass = ok ? 'over' : 'not-available'
+            $scope.dropText = ok ? 'Drop files here...' : 'Only files are allowed!';
+            $scope.dropClass = ok ? 'over' : 'not-available';
         })
-    }, false)
+    }, false);
     dropbox.addEventListener("drop", function(evt) {
-        console.log('drop evt:', JSON.parse(JSON.stringify(evt.dataTransfer)))
-        evt.stopPropagation()
-        evt.preventDefault()
+        console.log('drop evt:', JSON.parse(JSON.stringify(evt.dataTransfer)));
+        evt.stopPropagation();
+        evt.preventDefault();
         $scope.$apply(function(){
-            $scope.dropText = 'Drop files here...'
-            $scope.dropClass = ''
-        })
-        var files = evt.dataTransfer.files
+            $scope.dropText = 'Drop files here...';
+            $scope.dropClass = '';
+        });
+        var files = evt.dataTransfer.files;
         if (files.length > 0) {
             $scope.$apply(function(){
-                $scope.files = []
+                $scope.files = [];
                 for (var i = 0; i < files.length; i++) {
-                    $scope.files.push(files[i])
+                    $scope.files.push(files[i]);
                 }
             })
         }
-    }, false)
+    }, false);
     //============== DRAG & DROP =============
 
     $scope.setFiles = function(element) {
@@ -138,7 +138,7 @@ angular.module("rubedoBlocks").lazy.controller('ImageBatchUploadController',['$s
         })
         alert("The upload has been canceled by the user or the browser dropped the connection.")
     }
-}
+
 
 }]);
     
