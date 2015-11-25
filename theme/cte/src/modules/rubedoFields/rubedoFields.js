a6d8be87acf54c72924ad287e5b803b9d02f2010
nicolasrhone <nicolas.rhone@gmail.com>
2015-11-25 23:27:15 +0100

    suite

--- theme/cte/src/modules/rubedoFields/rubedoFields.js
+++ theme/cte/src/modules/rubedoFields/rubedoFields.js
@@ -702,15 +702,36 @@
             link: function(scope, element, attrs) {
                 var model = $parse(attrs.fileModel);
                 var modelSetter = model.assign;
-
+                var isMultiple = attrs.multiple;
                 element.bind('change', function(){
+                    var values = [];
+                    angular.forEach(element[0].files, function (item) {
+                        var value = {
+                           // File Name 
+                            name: item.name,
+                            //File Size 
+                            size: item.size,
+                            //File URL to view 
+                            url: URL.createObjectURL(item),
+                            // File Input Value 
+                            _file: item
+                        };
+                        values.push(value);
+                    });
                     scope.$apply(function(){
-                        modelSetter(scope, element[0].files[0]);
+                        if (isMultiple) {
+                            modelSetter(scope, values);
+                        } else {
+                            modelSetter(scope, values[0]);
+                        }
                     });
                 });
             }
         };
     }]);
+    
+    
+    
 
     module.controller("UserPhotoFieldController",["$scope","RubedoUsersService",function($scope,RubedoUsersService){
         var me=this;
