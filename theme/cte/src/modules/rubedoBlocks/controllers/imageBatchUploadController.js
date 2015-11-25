27baa4c304da155651b441267e08ab544e9cdde5
nicolasrhone <nicolas.rhone@gmail.com>
2015-11-25 17:47:02 +0100

    suite

--- theme/cte/src/modules/rubedoBlocks/controllers/imageBatchUploadController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/imageBatchUploadController.js
@@ -1,3 +1,3 @@
-angular.module("rubedoBlocks").lazy.controller('ImageBatchUpladController',['$scope','RubedoMediaService',function($scope,RubedoMailService){
+angular.module("rubedoBlocks").lazy.controller('ImageBatchUpladController',['$scope','RubedoMediaService',function($scope,RubedoMediaService){
     var me = this;
 }]);
