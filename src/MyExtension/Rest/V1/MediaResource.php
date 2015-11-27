9585e0b01935352c03b09fbcf0e14b6f6bff65cd
nicolasrhone <nicolas.rhone@gmail.com>
2015-11-27 18:22:11 +0100

    fix

--- src/MyExtension/Rest/V1/MediaResource.php
+++ src/MyExtension/Rest/V1/MediaResource.php
@@ -85,10 +85,9 @@ class MediaResource extends AbstractResource
         $media['i18n'] = array();
         $media['i18n'][$nativeLanguage] = array();
         $media['i18n'][$nativeLanguage]['fields'] = $media['fields'];
-        
-            $media["target"]=array($params['target']);
-            $media["writeWorkspace"]=$params['target'];
-        
+        $media['target'] = array("global");
+        $media["writeWorkspace"]="global";
+       
         $media['Content-Type'] = null;
         $media['originalFileId'] = $this->uploadFile($params['file'], $media['Content-Type']);
         AbstractLocalizableCollection::setIncludeI18n(true);
