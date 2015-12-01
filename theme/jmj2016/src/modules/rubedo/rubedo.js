f9a2435dae06a28888920b3de481ea1908808953
nicolasrhone <nicolas.rhone@gmail.com>
2015-12-01 18:24:23 +0100

    rubedo file jmj

--- theme/jmj2016/src/modules/rubedo/rubedo.js
+++ theme/jmj2016/src/modules/rubedo/rubedo.js
@@ -14,7 +14,10 @@
         site:{
 
         },
-        user:null
+        queue:[],
+        rEvents:{},
+        user:null,
+        UXParams:{}
     };
 
     app.config(function($routeProvider,$locationProvider,$controllerProvider, $compileProvider, $filterProvider, $provide) {
@@ -37,6 +40,201 @@
 
     });
 
+    app.factory('UXUserService',["RubedoModuleConfigService",function(RubedoModuleConfigService){
+        var serviceInstance = {};
+        serviceInstance.ISCONNECTED=function(){
+            return(current.user ? true : false);
+        };
+        serviceInstance.RUID=function(){
+            return(RubedoModuleConfigService.getConfig().fingerprint);
+        };
+        serviceInstance.emailRegex= /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        serviceInstance.ISEMAILVALID=function(){
+            if (!current.user){
+                return false;
+            }
+            return(serviceInstance.emailRegex.test(current.user.email));
+        };
+        serviceInstance.SUBSCRIBEDTO=function(mailingList){
+            return current.user&&current.user.mailingLists&&current.user.mailingLists[mailingList]&&current.user.mailingLists[mailingList].status ? true : false;
+        };
+        serviceInstance.ISGEOLOCATED=function(){
+            return navigator.geolocation ? true : false;
+        };
+        return serviceInstance;
+    }]);
+
+    app.factory('UXPageService',["RubedoFingerprintDataService",function(RubedoFingerprintDataService){
+        var serviceInstance = {};
+        serviceInstance.angReferrer=false;
+        serviceInstance.lastPageLoad=Date.now() / 1000 | 0;
+        serviceInstance.resetPageLoad=function(){
+            serviceInstance.lastPageLoad=Date.now() / 1000 | 0;
+        };
+        serviceInstance.TIMEONPAGE=function(){
+            var newTS=Date.now() / 1000 | 0;
+            return (newTS-serviceInstance.lastPageLoad);
+        };
+        serviceInstance.setAngReferrer=function(newReferrer){
+            serviceInstance.angReferrer=newReferrer;
+        };
+        serviceInstance.REFERRER=function(){
+            if (serviceInstance.angReferrer) {
+                return(serviceInstance.angReferrer);
+            } else if (document.referrer&&document.referrer!=""){
+                return(document.referrer);
+            } else {
+                return false
+            }
+
+        };
+        serviceInstance.NBVIEWS=function(){
+            var fingerprintData=RubedoFingerprintDataService.getFingerprintData();
+            if(fingerprintData&&fingerprintData.pages&&fingerprintData.pages[current.page.id]&&fingerprintData.pages[current.page.id].nbViews){
+                return(fingerprintData.pages[current.page.id].nbViews);
+            } else {
+                return(0);
+            }
+        };
+
+        return serviceInstance;
+    }]);
+
+    app.factory('UXSessionService',['ipCookie',function(ipCookie){
+        var serviceInstance = {};
+        serviceInstance.DURATION=function(){
+            var existingTS=ipCookie("sessionStartTS");
+            if (!existingTS){
+                return false;
+            }
+            var newTS=Date.now() / 1000 | 0;
+            return (newTS-existingTS);
+        };
+        return serviceInstance;
+    }]);
+
+    app.factory('UXCore',['RubedoFingerprintDataService','$rootScope','$timeout',function(RubedoFingerprintDataService,$rootScope,$timeout){
+        var serviceInstance = {};
+        var SET=function(var1,var2){
+            RubedoFingerprintDataService.logFDChange(var1,"set",var2);
+        };
+        var INC=function(var1,var2){
+            RubedoFingerprintDataService.logFDChange(var1,"inc",var2);
+        };
+        var DEC=function(var1,var2){
+            RubedoFingerprintDataService.logFDChange(var1,"dec",var2);
+        };
+        var HIDEBLOCK=function(bCode){
+            if(!current.UXParams.hiddenBlocks){
+                current.UXParams.hiddenBlocks={};
+            }
+            current.UXParams.hiddenBlocks[bCode]=true;
+        };
+        var SHOWBLOCK=function(bCode){
+            if(!current.UXParams.hiddenBlocks){
+                current.UXParams.hiddenBlocks={};
+            }
+            current.UXParams.hiddenBlocks[bCode]=false;
+        };
+        var SHOWMODAL=function(bCode,delay){
+            if (delay){
+                current.queue.push(
+                    $timeout(function(){
+                        $rootScope.$broadcast("RubedoShowModal",{block:bCode});
+                    },delay)
+                );
+            } else {
+                if (!current.UXParams.initialEvents){
+                    current.UXParams.initialEvents={};
+                }
+                if(!current.UXParams.initialEvents[bCode]){
+                    current.UXParams.initialEvents[bCode]=[];
+                }
+                $rootScope.$broadcast("RubedoShowModal",{block:bCode});
+                current.UXParams.initialEvents[bCode]["RubedoShowModal"]=true;
+            }
+
+        };
+        serviceInstance.evaluateCondition=function(condition){
+            var replaceArray={
+                'USER.DATA.':"serviceInstance.fingerprintData.",
+                'USER.ISCONNECTED':'USER.ISCONNECTED()',
+                'USER.ISGEOLOCATED':'USER.ISGEOLOCATED()',
+                'USER.RUID':'USER.RUID()',
+                'USER.ISEMAILVALID':'USER.ISEMAILVALID()',
+                'SESSION.DURATION':'SESSION.DURATION()',
+                'PAGE.NBVIEWS':'PAGE.NBVIEWS()',
+                'PAGE.TIMEONPAGE':'PAGE.TIMEONPAGE()',
+                'PAGE.REFERRER':'PAGE.REFERRER()',
+                ' NOT = ':'!=',
+                ' AND ':'&&',
+                ' = ':'==',
+                ' OR ':'||',
+                ' NOT ':'!'
+            };
+            angular.forEach(replaceArray, function(value, key) {
+                var regex = new RegExp(key, "g");
+                condition = condition.replace(regex, value);
+            });
+            return(eval(condition));
+        };
+        serviceInstance.executeAction=function(action){
+            var replaceArray={
+                'USER.DATA.':"serviceInstance.fingerprintData.",
+                'USER.ISCONNECTED':'USER.ISCONNECTED()',
+                'USER.ISGEOLOCATED':'USER.ISGEOLOCATED()',
+                'USER.RUID':'USER.RUID()',
+                'USER.ISEMAILVALID':'USER.ISEMAILVALID()',
+                'SESSION.DURATION':'SESSION.DURATION()',
+                'PAGE.TIMEONPAGE':'PAGE.TIMEONPAGE()',
+                'PAGE.REFERRER':'PAGE.REFERRER()',
+                ' AND ':'&&',
+                ' OR ':'||',
+                ' NOT ':'!',
+                'PAGE.NBVIEWS':"'pages."+current.page.id+".nbViews'"
+            };
+            angular.forEach(replaceArray, function(value, key) {
+                var regex = new RegExp(key, "g");
+                action = action.replace(regex, value);
+            });
+            eval(action);
+        };
+        serviceInstance.parse=function(instruction){
+            if (instruction.indexOf("WHEN")>-1&&instruction.indexOf("DO")>-1){
+                var splittedInstruction=instruction.replace("WHEN","").split("DO");
+                var eventName=splittedInstruction[0].trim();
+                if (!current.rEvents[eventName]){
+                    current.rEvents[eventName]=[];
+                }
+                current.rEvents[eventName].push(splittedInstruction[1])
+
+            } else if(instruction.indexOf("DELAY")>-1){
+                var splittedInstruction=instruction.split("DELAY");
+                current.queue.push(
+                    $timeout(function(){
+                        serviceInstance.segment(splittedInstruction[0]);
+                    },parseInt(splittedInstruction[1]))
+                );
+            } else {
+                serviceInstance.segment(instruction);
+            }
+
+        };
+        serviceInstance.segment=function(instruction){
+            serviceInstance.fingerprintData=RubedoFingerprintDataService.getFingerprintData();
+            if(instruction.indexOf("IF")>-1&&instruction.indexOf("THEN")>-1){
+                var splittedInstruction=instruction.replace("IF","").split("THEN");
+                if(serviceInstance.evaluateCondition(splittedInstruction[0])){
+                    serviceInstance.executeAction(splittedInstruction[1]);
+                }
+            } else {
+                serviceInstance.executeAction(instruction);
+            }
+        };
+        return serviceInstance;
+    }]);
+    
+    
     app.controller("RubedoController",['RubedoBlockTemplateResolver','RubedoImageUrlService','RubedoAuthService','RubedoFieldTemplateResolver','snapRemote','RubedoPageComponents','RubedoTranslationsService','$scope','$routeParams',
         function(RubedoBlockTemplateResolver,RubedoImageUrlService,RubedoAuthService,RubedoFieldTemplateResolver,snapRemote, RubedoPageComponents, RubedoTranslationsService,$scope,$routeParams){
         var me=this;
@@ -53,8 +251,15 @@
                 } else {
                     window.location.href=newLoc.slice(0,newLoc.indexOf("#"));
                 }
-            } else if (window._gaq) {
-                window._gaq.push(['_trackPageview', newLoc]);
+            } else {
+                if (window._gaq) {
+                    window._gaq.push(['_trackPageview', newLoc]);
+                }
+                if (currentLoc&&currentLoc!=""&&currentLoc!=newLoc){
+                    UXPageService.setAngReferrer(currentLoc);
+                }
+                UXPageService.resetPageLoad();
+
             }
         });
         $scope.$on("$locationChangeSuccess",function(scope, newLoc,currentLoc){
@@ -200,7 +405,25 @@
             };
             me.setPageDescription=function(newDescription){
                 me.current.page.description=newDescription;
-            }
+            };
+            $scope.$on("ClickStreamEvent",function(event,args){
+                if (typeof(Fingerprint2)!="undefined"&&args&&args.csEvent){
+                    RubedoClickStreamService.logEvent(args.csEvent,args.csEventArgs);
+                }
+            });
+
+            me.fireCSEvent=function(event,args){
+                $rootScope.$broadcast("ClickStreamEvent",{csEvent:event,csEventArgs:args});
+            };
+
+            USER=UXUserService;
+            $scope.USER=USER;
+
+            PAGE=UXPageService;
+            $scope.PAGE=PAGE;
+
+            SESSION=UXSessionService;
+            $scope.SESSION=SESSION;
 
     }]);
 
@@ -209,6 +432,20 @@
         if ($scope.rubedo.fieldEditMode){
             $scope.rubedo.revertChanges();
         }
+        angular.forEach(current.queue,function(task){
+            $timeout.cancel(task);
+        });
+        current.queue=[];
+        current.rEvents={ };
+        $scope.$on("ClickStreamEvent",function(event,args){
+            if(args.csEvent&&current.rEvents[args.csEvent]){
+                angular.forEach(current.rEvents[args.csEvent],function(instructionToHandle){
+                    if(instructionToHandle!=""){
+                        UXCore.parse(instructionToHandle);
+                    }
+                });
+            }
+        });
         RubedoPagesService.getPageByCurrentRoute().then(function(response){
             if (response.data.success){
                 var newPage=angular.copy(response.data.page);
@@ -257,6 +494,39 @@
                         me.currentBodyTemplate=themePath+'/templates/defaultPageBody.html';
                     }
                 }
+                //UX
+                if (newPage.pageProperties&&newPage.pageProperties.UXInstructions&&newPage.pageProperties.UXInstructions!=""){
+                    if (newPage.pageProperties.UXInstructions.indexOf("\n")>-1){
+                        var maskInstructionsArray=newPage.pageProperties.UXInstructions.split("\n");
+                        angular.forEach(maskInstructionsArray,function(instruction){
+                            if(instruction!=""){
+                                UXCore.parse(instruction);
+                            }
+                        });
+                    } else {
+                        UXCore.parse(newPage.pageProperties.UXInstructions);
+                    }
+
+                }
+                if (newPage.UXInstructions&&newPage.UXInstructions!=""){
+                    if (newPage.UXInstructions.indexOf("\n")>-1){
+                        var instructionsArray=newPage.UXInstructions.split("\n");
+                        angular.forEach(instructionsArray,function(instruction){
+                            if(instruction!=""){
+                                UXCore.parse(instruction);
+                            }
+                        });
+                    } else {
+                        UXCore.parse(newPage.UXInstructions);
+                    }
+
+                }
+                //Page load
+                $rootScope.$broadcast("ClickStreamEvent",{csEvent:"pageView",csEventArgs:{
+                    pageId:newPage.id,
+                    siteId:response.data.site.id,
+                    pageTaxo:newPage.taxonomy
+                }});
 
             }
         },function(response){
