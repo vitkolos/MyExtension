cc45a56e6f0d0fda35062a6f003487be654a13a5
nicolasrhone <nicolas.rhone@gmail.com>
2015-12-04 10:36:48 +0100

    fix

--- theme/worshipteam/src/modules/rubedoFields/rubedoFields.js
+++ theme/worshipteam/src/modules/rubedoFields/rubedoFields.js
@@ -216,7 +216,7 @@
             stylesSet: [
                 { name: 'Titre section', element: 'h4', attributes: { 'class': 'text-center titre-block'} }
             ],
-            templates:[{title:'Image and Title',image:'/theme/worshipteam/img/ckeditor/template1.gif',description:'One main image with a title and text, which surrounds the image.',html:'<h3><img style="margin-right: 10px" height="100" alt="" width="100" align="left"/>Type the title here</h3><p>Type the text here</p>'}],
+            //templates:[{title:'Image and Title',image:'/theme/worshipteam/img/ckeditor/template1.gif',description:'One main image with a title and text, which surrounds the image.',html:'<h3><img style="margin-right: 10px" height="100" alt="" width="100" align="left"/>Type the title here</h3><p>Type the text here</p>'}],
         };
         if ($scope.field.cType!="CKEField"&&$scope.field.cType!="Rubedo.view.CKEField"){
             editorOptions.removePlugins= 'colorbutton,find,flash,font,' + 'forms,iframe,image,newpage,removeformat' + 'smiley,specialchar,stylescombo,templates,wsc';
