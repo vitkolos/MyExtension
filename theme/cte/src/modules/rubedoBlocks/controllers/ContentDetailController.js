5c8342d4db29e790760674ccf501cee5d178285e
nicolasrhone <nicolas.rhone@gmail.com>
2015-12-09 10:36:53 +0100

    suite

--- theme/cte/src/modules/rubedoBlocks/controllers/ContentDetailController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -217,7 +217,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
 
                          }
                      });*/
-                     var taxonomiesArray =[];
+                     var taxonomiesArray ={};
                      var index=0;
                      angular.forEach(me.content.taxonomy,function(value, taxo){
                             if (taxo!='navigation'){
