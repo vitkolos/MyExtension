2c8f7f2e205141a056d680bea7ad5b279b437a8e
nicolasrhone <nicolas.rhone@gmail.com>
2015-12-09 16:19:50 +0100

    fix

--- theme/cte/src/modules/rubedoBlocks/controllers/FacebookController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/FacebookController.js
@@ -19,9 +19,10 @@ angular.module("rubedoBlocks").lazy.controller('FacebookController',['$scope','$
     }
  
     me.loadFcb = function(){
+        FB = null;
          (function(d, s, id) {
          var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) return;
+         //if (d.getElementById(id)) return;
          js = d.createElement(s); js.id = id;
          js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.3";
          fjs.parentNode.insertBefore(js, fjs);
