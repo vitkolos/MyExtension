f454484fc5f3018e4f124c6c5dd2affa30cec6ec
nicolasrhone <nicolas.rhone@gmail.com>
2015-12-10 17:23:18 +0100

    net for god

--- theme/netforgod/js/netforgod.js
+++ theme/netforgod/js/netforgod.js
@@ -2,45 +2,13 @@ blocksConfig.buttonToPage={
            "template": "/templates/blocks/buttonToPage.html",
           "internalDependencies":["/src/modules/rubedoBlocks/controllers/buttonToPage.js"]
 };
-blocksConfig.contactBlock={
-           "template": "/templates/blocks/contactBlock.html",
-          "internalDependencies":["/src/modules/rubedoBlocks/controllers/contactBlock.js"]
-};
-blocksConfig.form={
-           "template": "/templates/blocks/form.html",
-          "internalDependencies":["/src/modules/rubedoBlocks/controllers/form.js"]
-};
+
 blocksConfig.simpleContact={
            "template": "/templates/blocks/simpleContact.html",
           "internalDependencies":["/src/modules/rubedoBlocks/controllers/simpleContact.js"]
 };
 
 
-
-
-angular.module('rubedoBlocks').directive('loadModal', function () {
-    return {
-        restrict: 'A',
-        link: function(scope, $elm, attrs) {
-            $elm.bind('click', function(event) {
-                event.preventDefault();
-                /*angular.element('#myModal iframe').attr('src', src);*/
-                angular.element('#myModal').appendTo('body').modal('show');
-            });
-        }
-    }
-});
-
-
-angular.module('rubedoBlocks').filter('homepage', function() {
-    return function(input){
-		  var delimiter = '/';
-		  var array = input.split(delimiter);
-                  var url ="";
-                  for (i = 0; i < array.length-2; i++) {url = url + array[i]+'/'}
-                  return url;
-    }
-});
 
 
  angular.module('rubedoBlocks').service('NFGFilms', function($http) {
---
+++ theme/netforgod/src/modules/rubedoBlocks/controllers/buttonToPage.js
@@ -0,0 +1,20 @@
+angular.module("rubedoBlocks").lazy.controller("ButtonToPageController",['$scope','RubedoPagesService',function($scope,RubedoPagesService){
+    var me = this;
+    var blockConfig=$scope.blockConfig;
+    me.inputFields=[ ];
+    $scope.fieldEntity={ };
+    me.columns = blockConfig.columns ? 'col-md-'+(12/blockConfig.columns):'col-md-12';
+
+    $scope.fieldInputMode=true;
+    var fields=angular.copy($scope.fieldEntity);
+    
+    if (blockConfig.linkedPage&&mongoIdRegex.test(blockConfig.linkedPage))   {
+	RubedoPagesService.getPageById(blockConfig.linkedPage).then(function(response){
+	    if (response.data.success){
+		    me.pageLink=response.data.url;
+	    }
+	});
+    };
+			
+	
+}]);
