990a3f32afbd41a00e145f57c079e4b388a7a957
nicolasrhone <nicolas.rhone@gmail.com>
2015-12-10 22:37:20 +0100

    goodnew

--- theme/goodnews/src/modules/rubedoBlocks/controllers/SearchResultsController.js
+++ theme/goodnews/src/modules/rubedoBlocks/controllers/SearchResultsController.js
@@ -7,24 +7,18 @@ angular.module("rubedoBlocks").lazy.controller("SearchResultsController",["$scop
         me.facets = [];
         me.activeFacets = [];
         me.start = 0;
-        me.limit = $routeParams.limit?$routeParams.limit:20; // nombre de résultats affichés par défaut
+        me.limit = $routeParams.limit?$routeParams.limit:10;
         me.orderBy = $routeParams.orderby?$routeParams.orderby:"lastUpdateTime";
-       var resolveOrderBy = {
-            '_score': 'relevance',
+        var resolveOrderBy = {
             'lastUpdateTime': 'date',
             'authorName': 'author',
             'text': 'title'
         };
-        me.displayOrderBy = $routeParams.orderby?resolveOrderBy[$routeParams.orderby]:$scope.rubedo.translate('Search.Label.OrderByRelevance');
+        me.displayOrderBy = $routeParams.orderby?resolveOrderBy[$routeParams.orderby]:$scope.rubedo.translate('Search.Label.OrderByDate');
         me.template = themePath+"/templates/blocks/searchResults/"+config.displayMode+".html";
-        me.template_actus = themePath+"/templates/blocks/searchResults/actus.html";
-        
-        me.template_foi = themePath+"/templates/blocks/searchResults/foi.html";
         var predefinedFacets = !config.predefinedFacets?{}:JSON.parse(config.predefinedFacets);
-        me.displayedResults = 20; // nombre de résultats affichés par défaut
-        var facetsId = ['objectType','type','damType','userType','author','userName','lastupdatetime','query'];
-        me.options = {};
-        me.options = {
+        var facetsId = ['objectType','type','damType','userType','author','userName','lastupdatetime','price','inStock','query'];
+        var defaultOptions = {
             start: me.start,
             limit: me.limit,
             constrainToSite: config.constrainToSite,
@@ -33,50 +27,47 @@ angular.module("rubedoBlocks").lazy.controller("SearchResultsController",["$scop
             displayedFacets: config.displayedFacets,
             orderby: me.orderBy,
             pageId: $scope.rubedo.current.page.id,
-            siteId: $scope.rubedo.current.site.id,
-            taxonomies:{}
+            siteId: $scope.rubedo.current.site.id
         };
-        me.options["type[]"] = $scope.block.code? $scope.block.code.split(","):[];
-        me.options.taxonomies = config.taxonomies? JSON.parse(config.taxonomies):{};
-        var taxonomiesReset =config.taxonomies? JSON.parse(config.taxonomies):{};
         if (config.singlePage){
-            me.options.detailPageId = config.singlePage;
+            defaultOptions.detailPageId = config.singlePage;
         }
         if(config.profilePage){
-            me.options.profilePageId = config.profilePage;
+            defaultOptions.profilePageId = config.profilePage;
         }
+        var options = angular.copy(defaultOptions);
         var parseQueryParamsToOptions = function(){
             angular.forEach($location.search(), function(queryParam, key){
                 if(typeof queryParam !== "boolean"){
                     if(key == 'taxonomies'){
-                        me.options[key] = JSON.parse(queryParam);
-                    }
-                    else {
+                        options[key] = JSON.parse(queryParam);
+                    } else {
                         if(key == 'query'){
-                            me.options.query = queryParam;
+                            me.query = queryParam;
                         }
-                        else {
-                        me.options[key] = queryParam;
-                        }
+                        options[key] = queryParam;
                     }
                 }
             });
         };
         if(predefinedFacets.query) {
-            me.options.query =  predefinedFacets.query;
-            me.updateSearch();
+            me.query = options.query = predefinedFacets.query;
+            $location.search('query',me.query);
         }
-
-        me.updateSearch = function() {
-            me.options.start = me.start;
-            me.options.limit = me.limit;
-            me.options.orderBy = me.orderBy;
-            me.searchByQuery(me.options, true);
-        };
+        $scope.$on('$routeUpdate', function(scope, next, current) {
+            options = angular.copy(defaultOptions);
+            options.start = me.start;
+            options.limit = me.limit;
+            options.orderBy = me.orderBy;
+            parseQueryParamsToOptions();
+            me.searchByQuery(options, true);
+        });
         me.checked = function(term){
             var checked = false;
             angular.forEach(me.activeTerms,function(activeTerm){
-                checked = activeTerm.term==term;
+                if (!checked){
+                    checked = activeTerm.term==term;
+                }
             });
             return checked;
         };
@@ -87,22 +78,24 @@ angular.module("rubedoBlocks").lazy.controller("SearchResultsController",["$scop
             });
         };
         me.changePageAction = function(){
-            me.options.start = me.start;
-            me.searchByQuery(me.options);
+            options.start = me.start;
+            me.searchByQuery(options);
         };
         me.onSubmit = function(){
-            me.options.start = 0;
-            me.options.limit = 10;
-            me.options.query = me.query;
-            me.options.taxonomies={};
-            me.updateSearch();
+            me.start = 0;
+            options = angular.copy(defaultOptions);
+            options.start = me.start;
+            options.limit = me.limit;
+            options.query = me.query;
+            options.orderBy = me.orderBy;
+            $location.search('query',me.query);
         };
         me.changeOrderBy = function(orderBy){
             if(me.orderBy != orderBy){
                 me.orderBy = orderBy;
                 me.displayOrderBy = resolveOrderBy[orderBy];
                 me.start = 0;
-                me.updateSearch();
+                $location.search('orderby',me.orderBy);
             }
         };
         me.changeLimit = function(limit){
@@ -119,44 +112,6 @@ angular.module("rubedoBlocks").lazy.controller("SearchResultsController",["$scop
             }
             return res;
         };
-        me.clickOnFacetsType= function(term){
-            if (me.options["type[]"].indexOf(term)>-1) {
-                me.options["type[]"].splice(me.options["type[]"].indexOf(term),1);
-            }
-            else me.options["type[]"]=$scope.block.code.split(",");
-            me.updateSearch();
-        };
-        me.clickOnFacetsRadio = function(facetId,term){
-            if (me.options.taxonomies[facetId]) {
-                var del=false;
-                for (var i = 0; i < me.options.taxonomies[facetId].length; i++) {
-                    if (me.options.taxonomies[facetId][i] == term) {
-                        del=true;
-                    }
-                }
-                if (del) {
-                   me.options.taxonomies[facetId].splice(me.options.taxonomies[facetId].indexOf(term),1);
-                }
-                else {
-                    me.options.taxonomies={};
-                    me.options.taxonomies = angular.copy(taxonomiesReset);
-                    if (!me.options.taxonomies[facetId]) me.options.taxonomies[facetId]=[];
-                     me.options.taxonomies[facetId].push(term);
-                }
-            }
-            else {
-                me.options.taxonomies[facetId] = [];
-                if (!me.options.taxonomies[facetId]) me.options.taxonomies[facetId]=[];
-                me.options.taxonomies[facetId].push(term);
-           }
-            me.updateSearch();
-        }
-        
- 
-  
-
- 
-        
         me.clickOnFacets =  function(facetId,term){
             var del = false;
             angular.forEach(me.activeTerms,function(activeTerm){
@@ -166,53 +121,56 @@ angular.module("rubedoBlocks").lazy.controller("SearchResultsController",["$scop
             });
             if(del){
                 if(facetsId.indexOf(facetId)==-1){
-                    me.options.taxonomies[facetId].splice(me.options.taxonomies[facetId].indexOf(term),1);
-                    if(me.options.taxonomies[facetId].length == 0){
-                        delete me.options.taxonomies[facetId];
+                    options.taxonomies[facetId].splice(options.taxonomies[facetId].indexOf(term),1);
+                    if(options.taxonomies[facetId].length == 0){
+                        delete options.taxonomies[facetId];
                     }
-                    me.updateSearch();
+                    if(Object.keys(options['taxonomies']).length == 0){
+                        $location.search('taxonomies',null);
+                    } else {
+                        $location.search('taxonomies',JSON.stringify(options.taxonomies));
+                    }
                 } else if (facetId == 'query') {
-                    delete me.options.query;
-                    me.updateSearch();
-                } else if(facetId == 'lastupdatetime') {
-                    delete me.options[facetId];
-                    me.updateSearch();
+                    $location.search('query',null);
+                    delete options.query;
+                } else if(facetId == 'lastupdatetime'||facetId == 'price'||facetId == 'inStock') {
+                    delete options[facetId];
+                    $location.search(facetId,null);
                 } else {
-                    if(angular.isArray(me.options[facetId+'[]'])){
-                        me.options[facetId+'[]'].splice(me.options[facetId+'[]'].indexOf(term),1);
+                    if(angular.isArray(options[facetId+'[]'])){
+                        options[facetId+'[]'].splice(options[facetId+'[]'].indexOf(term),1);
                     } else {
-                        delete me.options[facetId+'[]'];
+                        delete options[facetId+'[]'];
                     }
-                    if(!me.options[facetId+'[]'] || me.options[facetId+'[]'].length == 0){
-                        me.updateSearch();
+                    if(!options[facetId+'[]'] || options[facetId+'[]'].length == 0){
+                        $location.search(facetId+'[]',null)
                     } else {
-                        me.updateSearch();
+                        $location.search(facetId+'[]',options[facetId+'[]']);
                     }
                 }
             } else {
                 if(facetsId.indexOf(facetId)==-1){
-                    if(!me.options.taxonomies){
-                        me.options.taxonomies = {};
+                    if(!options.taxonomies){
+                        options.taxonomies = {};
                     }
-                    if(!me.options.taxonomies[facetId]){
-                        me.options.taxonomies[facetId] = [];
+                    if(!options.taxonomies[facetId]){
+                        options.taxonomies[facetId] = [];
                     }
-                    me.options.taxonomies[facetId].push(term);
-                    me.updateSearch();
-                } else if(facetId == 'lastupdatetime') {
-                    me.options[facetId] = term;
-                    me.updateSearch();
+                    options.taxonomies[facetId].push(term);
+                    $location.search('taxonomies',JSON.stringify(options.taxonomies));
+                } else if(facetId == 'lastupdatetime'||facetId == 'price'||facetId == 'inStock') {
+                    options[facetId] = term;
+                    $location.search(facetId,options[facetId]);
                 } else {
-                    if(!me.options[facetId+'[]']){
-                        me.options[facetId+'[]'] = [];
+                    if(!options[facetId+'[]']){
+                        options[facetId+'[]'] = [];
                     }
-                    me.options[facetId+'[]'].push(term);
-                    me.updateSearch();
+                    options[facetId+'[]'].push(term);
+                    $location.search(facetId+'[]',options[facetId+'[]']);
                 }
-                console.log(me.options);
             }
             me.start = 0;
-            me.options.start = me.start;
+            options.start = me.start;
         };
 
         me.searchByQuery = function(options){
@@ -250,15 +208,5 @@ angular.module("rubedoBlocks").lazy.controller("SearchResultsController",["$scop
             })
         };
         parseQueryParamsToOptions();
-        me.searchByQuery(me.options);
-        
-        
-        
-        me.loadMore = function(){
-            me.displayedResults=me.displayedResults+20; // nombre de résultats à ajouter en load more
-            if (me.displayedResults>=me.options.limit) {
-                me.options.limit+=40;// nombre de résultats à ajouter à la liste si elle est trop courte
-                me.searchByQuery(me.options);
-            }
-        }
-  }]);
+        me.searchByQuery(options);
+    }]);
