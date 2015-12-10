304e9c2170b6b633279a7794d3f5c593251d3668
nicolasrhone <nicolas.rhone@gmail.com>
2015-12-10 23:35:41 +0100

    suite

--- theme/goodnews/src/modules/rubedoBlocks/controllers/SearchResultsController.js
+++ theme/goodnews/src/modules/rubedoBlocks/controllers/SearchResultsController.js
@@ -7,7 +7,7 @@ angular.module("rubedoBlocks").lazy.controller("SearchResultsController",["$scop
         me.facets = [];
         me.activeFacets = [];
         me.start = 0;
-        me.limit = $routeParams.limit?$routeParams.limit:10;
+        me.limit = $routeParams.limit?$routeParams.limit:20;
         me.orderBy = $routeParams.orderby?$routeParams.orderby:"lastUpdateTime";
         var resolveOrderBy = {
             'lastUpdateTime': 'date',
