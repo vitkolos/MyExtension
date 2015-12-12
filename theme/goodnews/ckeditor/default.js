ac9cf06c01294cb159b6e27b7c3ad78091f0a19c
nicolasrhone <nicolas.rhone@gmail.com>
2015-12-12 17:24:31 +0100

    fix

--- theme/goodnews/ckeditor/default.js
+++ theme/goodnews/ckeditor/default.js
@@ -2,7 +2,7 @@
  Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.addTemplates("default",{imagesPath:"/theme/cte/ckeditor/img/",templates:[
+CKEDITOR.addTemplates("default",{imagesPath:"/theme/goodnews/ckeditor/img/",templates:[
     {title:"Présentation avec icône",image:"template1.gif",description:"Template avec une icône, un titre et une description",
     html:'<div class="col-xs-6  col-md-3 block-detail" ><p>Icône</p><p>Title</p><p>Description</p></div>'},
     {title:"Texte sur deux colonnes",image:"w50w50.png",description:"Texte sur deux colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
