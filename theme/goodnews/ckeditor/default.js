78b6673d9883544f6b82e1c3c57bf0f5da2875e5
nicolasrhone <nicolas.rhone@gmail.com>
2015-12-12 17:15:56 +0100

    test template users

--- theme/goodnews/ckeditor/default.js
+++ theme/goodnews/ckeditor/default.js
@@ -8,5 +8,7 @@ CKEDITOR.addTemplates("default",{imagesPath:"/theme/cte/ckeditor/img/",templates
     {title:"Texte sur deux colonnes",image:"w50w50.png",description:"Texte sur deux colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
     html:'<div class="col-xs-12  col-sm-6"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-6"><p>Colonne 2 </p></div>'},
     {title:"Texte sur trois colonnes",image:"w33w33.png",description:"Texte sur trois colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
-    html:'<div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div>'}
+    html:'<div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div>'},
+    {title:"Liste de l'équipe",image:"users.jpg",description:"Présentation de l'équipe sur 4 colonnes",
+    html:'<div class="col-xs-12  col-sm-6 col-md-3"><p class="image">Image</p><p class="userName"></p><p class="userDesc"></p></div><div class="col-xs-12  col-sm-6 col-md-3"><p class="image">Image</p><p class="userName"></p><p class="userDesc"></p></div><div class="col-xs-12  col-sm-6 col-md-3"><p class="image">Image</p><p class="userName"></p><p class="userDesc"></p></div><div class="col-xs-12  col-sm-6 col-md-3"><p class="image">Image</p><p class="userName"></p><p class="userDesc"></p></div>'}
     ]});
