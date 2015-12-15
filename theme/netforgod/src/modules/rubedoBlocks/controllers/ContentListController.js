cb5ee62797f3de87007add2ca1afa4a3ae75e0ad
nicolasrhone <nicolas.rhone@gmail.com>
2015-12-15 21:53:27 +0100

    showfilter

--- theme/netforgod/src/modules/rubedoBlocks/controllers/ContentListController.js
+++ theme/netforgod/src/modules/rubedoBlocks/controllers/ContentListController.js
@@ -13,6 +13,13 @@ angular.module("rubedoBlocks").lazy.controller("ContentListController",['$scope'
     me.ismagic = config.magicQuery ? config.magicQuery : false;
     $scope.rubedo.showSearchList = false;
     $scope.showFilters=false;
+    me.showFilters = function(){
+        if ($scope.showFilters) {
+            $scope.rubedo.showSearchList = true;
+        }
+        $scope.showFilters = !$scope.showFilters;
+        
+    }
     var urlCurrentPage=$location.search()[blockPagingIdentifier];
     if (urlCurrentPage){
         me.start=(urlCurrentPage-1)*me.limit;
