eeddcee3bcfb0c23d60d0a8f8b3dcd2ba5a142b6
Nicolas Rhoné <nicolas.rhone@gmail.com>
2016-01-04 13:15:07 +0100

    nfg foi link

--- theme/netforgod/src/modules/rubedoBlocks/controllers/ContentDetailController.js
+++ theme/netforgod/src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -115,6 +115,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
                         pageId: $scope.rubedo.current.page.id
                     };
                     if (me.content.fields.linkedFOI) {
+                        console.log(me.content.fields.linkedFOI);
                         RubedoContentsService.getContentById(me.content.fields.linkedFOI, options).then(
                             function(response){
                                 if(response.data.success){
