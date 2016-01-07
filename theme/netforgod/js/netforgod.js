99cacf2cb7caa99e95af4dd3971bcff339125fd5
nicolasrhone <nicolas.rhone@gmail.com>
2016-01-07 10:24:26 +0100

    fix

--- theme/netforgod/js/netforgod.js
+++ theme/netforgod/js/netforgod.js
@@ -52,6 +52,7 @@ angular.module('rubedoBlocks').directive('jwplayer', ['$compile', function ($com
                                  width:"100%",
                                  aspectratio:"16:9",
                       }]);
+                      jwplayer(id).play();
                 });
         }
     };
