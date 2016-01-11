15535ed32b5888e7800c5b090f05a39df5ffc71f
Nicolas Rhoné <nicolas.rhone@gmail.com>
2016-01-11 19:42:22 +0100

    gd

--- theme/goodnews/src/modules/rubedoBlocks/controllers/sectionPresentation.js
+++ theme/goodnews/src/modules/rubedoBlocks/controllers/sectionPresentation.js
@@ -1,15 +1,15 @@
 angular.module("rubedoBlocks").lazy.controller("PresentationController",['$scope','RubedoPagesService',function($scope,RubedoPagesService){
     var me = this;
-    $scope.config=angular.copy($scope.blockConfig);
+    var blockConfig=$scope.blockConfig;
     me.inputFields=[ ];
     $scope.fieldEntity={ };
-    me.columns = config.columns ? 'col-md-'+(12/config.columns):'col-md-12';
+    me.columns = blockConfig.columns ? 'col-md-'+(12/blockConfig.columns):'col-md-12';
 
     $scope.fieldInputMode=true;
     var fields=angular.copy($scope.fieldEntity);
     
-    if (config.linkedPage&&mongoIdRegex.test(config.linkedPage))   {
-		    RubedoPagesService.getPageById(config.linkedPage).then(function(response){
+    if (blockConfig.linkedPage&&mongoIdRegex.test(blockConfig.linkedPage))   {
+		    RubedoPagesService.getPageById(blockConfig.linkedPage).then(function(response){
 				    if (response.data.success){
 					    me.pageLink=response.data.url;
 				    }
