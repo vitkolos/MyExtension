0a7eb97c07d45fe0606cdfcab740c44b4990935e
Nicolas Rhoné <nicolas.rhone@gmail.com>
2016-01-14 15:42:26 +0100

    gd

--- theme/goodnews/src/modules/rubedoFields/rubedoFields.js
+++ theme/goodnews/src/modules/rubedoFields/rubedoFields.js
@@ -230,7 +230,7 @@
             templates_files: [ '/theme/'+rubedoConfig.siteTheme+'/ckeditor/default.js' ],
             templates_replaceContent : false,
             customConfig: "/theme/"+rubedoConfig.siteTheme+"/ckeditor/config.js",
-            font_names : 'Roboto',
+            font_names : 'Roboto;Bebas',
         };
         if ($scope.field.cType!="CKEField"&&$scope.field.cType!="Rubedo.view.CKEField"){
             editorOptions.removePlugins= 'colorbutton,find,flash,font,' + 'forms,iframe,image,newpage,removeformat' + 'smiley,specialchar,stylescombo,templates,wsc';
