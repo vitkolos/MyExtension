08c8d7d98d31c90e0ee26f7869d97113c7d7fd1d
Nicolas Rhoné <nicolas.rhone@gmail.com>
2016-01-15 09:48:46 +0100

    albums

--- theme/cte/src/modules/rubedoBlocks/controllers/ContentContributionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/ContentContributionController.js
@@ -209,7 +209,7 @@ angular.module("rubedoBlocks").lazy.controller("AlbumUploadController",["$scope"
                         };
                     }
                 );
-            }
+            });
        }
 
     };
