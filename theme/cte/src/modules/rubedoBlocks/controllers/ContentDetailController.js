6130cd9d9ee7e305e41a6546a64aefae96cb20c8
nicolasrhone <nicolas.rhone@gmail.com>
2016-01-20 15:58:28 +0100

    suite

--- theme/cte/src/modules/rubedoBlocks/controllers/ContentDetailController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -96,7 +96,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
                         if(response.data.content.fields.video) {
                             $scope.rubedo.current.page.video = response.data.content.fields.video.url;
                         }
-                        $scope.rubedo.current.page.fbPage = "https://www.facebook.com/CheminNeuf/";
+                        $scope.rubedo.current.page.fbPage = "http://www.facebook.com/cheminneuf/";
                        
                     }
                     $scope.fieldEntity=angular.copy(me.content.fields);
