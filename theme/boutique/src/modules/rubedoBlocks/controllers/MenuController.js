4b0e1bebfdc060e685c327dabe08c205461dad37
EtienneLaval <etienne.laval.moi@gmail.com>
2016-01-21 17:22:24 +0100

    test pour log

---
+++ theme/boutique/src/modules/rubedoBlocks/controllers/MenuController.js
@@ -0,0 +1,53 @@
+angular.module("rubedoBlocks").lazy.controller("MenuController",['$scope','$location','RubedoMenuService','RubedoPagesService',function($scope,$location,RubedoMenuService,RubedoPagesService){
+    var me=this;
+    console.log(me);
+    var themePath="/theme/"+window.rubedoConfig.siteTheme;
+    me.menu={};
+    me.currentRouteline=$location.path();
+    var config=$scope.blockConfig;
+    me.menuTab = true;
+    if ($scope.block.code == '1418') {
+        me.menuClass="menu1418";
+        me.menuTab = true;
+    }
+    else if ($scope.block.code == 'cana'){
+        me.menuClass="menucana";
+        me.menuTab = true;
+    }
+    else if ($scope.block.code && $scope.block.code!="") {
+        me.menuTab = true;
+    }
+    me.searchEnabled = (config.useSearchEngine && config.searchPage);
+    if (config.rootPage){
+        var pageId=config.rootPage;
+    } else if (config.fallbackRoot&&config.fallbackRoot=="parent"&&mongoIdRegex.test($scope.rubedo.current.page.parentId)){
+        var pageId=$scope.rubedo.current.page.parentId;
+    } else {
+        var pageId=$scope.rubedo.current.page.id;
+    }
+    me.onSubmit = function(){
+        var paramQuery = me.query?'?query='+me.query:'';
+        RubedoPagesService.getPageById(config.searchPage).then(function(response){
+            if (response.data.success){
+                $location.url(response.data.url+paramQuery);
+            }
+        });
+    };
+    RubedoMenuService.getMenu(pageId, config.menuLevel).then(function(response){
+        if (response.data.success){
+            me.menu=response.data.menu;
+        } else {
+            me.menu={};
+        }
+    });
+    me.showMenu =function(){
+        $scope.menu = !$scope.menu;
+        if($scope.menu) angular.element('#menuModal').modal('show');
+        else angular.element('#menuModal').modal('hide');
+    };
+    // pour fermer le modal quand on clique sur un lien
+    $scope.$on("$locationChangeStart",function(event, newLoc,currentLoc){
+        angular.element('body .modal-backdrop ').remove();
+    });
+    alert('test');
+}]);
