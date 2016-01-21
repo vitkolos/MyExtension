0d3308323c9b34560d250c8dc6b07f098534abb3
nicolasrhone <nicolas.rhone@gmail.com>
2016-01-21 16:36:46 +0100

    test upload

--- theme/cte/src/modules/rubedoFields/rubedoFields.js
+++ theme/cte/src/modules/rubedoFields/rubedoFields.js
@@ -669,6 +669,24 @@
         me.newFile=null;
         me.uploadNewFile=function(){
            me.notification=null;
+           me.pageId = $scope.blockConfig.listPageId ? $scope.blockConfig.listPageId : $scope.rubedo.current.page.id;
+            if (me.pageId&&mongoIdRegex.test(me.pageId)) {
+                RubedoPagesService.getPageById(me.pageId).then(function(response){
+                    if (response.data.success){
+                        me.pageUrl=response.data.url;
+                        $http.get("/api/v1/pages",{
+                            params:{
+                                site:$location.host(),
+                                route:(me.pageUrl).substr(4)
+                            }
+                        }).then(function(response){if(response.data.success) {me.workspace= response.data.page.workspace; me.uploadNewFileWithWorkspace()}});
+                    };
+                });
+            };
+
+
+        };
+        me.uploadNewFileWithWorkspace=function(){
            if ($scope.fieldInputMode&&me.newFile&&$scope.field.config.allowedDAMTypes){
                var uploadOptions={
                    typeId:$scope.field.config.allowedDAMTypes,
@@ -704,25 +722,9 @@
                        };
                    }
                );
-           }
-
+           }            
         };
-        if ($scope.fieldInputMode) {
-            me.pageId = $scope.blockConfig.listPageId ? $scope.blockConfig.listPageId : $scope.rubedo.current.page.id;
-            if (me.pageId&&mongoIdRegex.test(me.pageId)) {
-                RubedoPagesService.getPageById(me.pageId).then(function(response){
-                    if (response.data.success){
-                        me.pageUrl=response.data.url;
-                        $http.get("/api/v1/pages",{
-                            params:{
-                                site:$location.host(),
-                                route:(me.pageUrl).substr(4)
-                            }
-                        }).then(function(response){if(response.data.success) {me.workspace= response.data.page.workspace; }});
-                    };
-                });
-            };
-        }
+
         if ($scope.fieldInputMode){
             $element.find('.form-control').on('change', function(){
                 setTimeout(function(){
