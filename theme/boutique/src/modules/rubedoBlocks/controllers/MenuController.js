9cb3e7c466275012d07c8ca3ba4f8699f91e4a80
EtienneLaval <etienne.laval.moi@gmail.com>
2016-01-26 15:39:46 +0100

    bugfix MenuController Boutique

--- theme/boutique/src/modules/rubedoBlocks/controllers/MenuController.js
+++ theme/boutique/src/modules/rubedoBlocks/controllers/MenuController.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-angular.module("rubedoBlocks").lazy.controller("MenuController",['$scope','$location','RubedoMenuService','RubedoPagesService',function($scope,$location,RubedoMenuService,RubedoPagesService){
-    var me=this;
-    var themePath="/theme/"+window.rubedoConfig.siteTheme;
-    me.menu={};
-    me.currentRouteline=$location.path();
-    var config=$scope.blockConfig;
-    me.menuTab = false;
-     if ($scope.block.code && $scope.block.code!="") {
-        me.menuTab = true;
-    }
-    me.searchEnabled = (config.useSearchEngine && config.searchPage);
-    if (config.rootPage){
-        var pageId=config.rootPage;
-    } else if (config.fallbackRoot&&config.fallbackRoot=="parent"&&mongoIdRegex.test($scope.rubedo.current.page.parentId)){
-        var pageId=$scope.rubedo.current.page.parentId;
-    } else {
-        var pageId=$scope.rubedo.current.page.id;
-    }
-    me.onSubmit = function(){
-        var paramQuery = me.query?'?query='+me.query:'';
-        RubedoPagesService.getPageById(config.searchPage).then(function(response){
-            if (response.data.success){
-                $location.url(response.data.url+paramQuery);
-            }
-        });
-    };
-    RubedoMenuService.getMenu(pageId, config.menuLevel).then(function(response){
-        if (response.data.success){
-            me.menu=response.data.menu;
-        } else {
-            me.menu={};
-        }
-    });
-    me.showMenu =function(){
-        $scope.menu = !$scope.menu;
-        if($scope.menu) angular.element('#menuModal').modal('show');
-        else angular.element('#menuModal').modal('hide');
-    };
-    // pour fermer le modal quand on clique sur un lien
-    $scope.$on("$locationChangeStart",function(event, newLoc,currentLoc){
-        angular.element('body .modal-backdrop ').remove();
-    });
-=======
 angular.module("rubedoBlocks").lazy.controller("MenuController",['$scope','$location','RubedoMenuService','RubedoPagesService',function($scope,$location,RubedoMenuService,RubedoPagesService){
     var me=this;
     var themePath="/theme/"+window.rubedoConfig.siteTheme;
@@ -85,5 +41,5 @@ angular.module("rubedoBlocks").lazy.controller("MenuController",['$scope','$loca
     $scope.$on("$locationChangeStart",function(event, newLoc,currentLoc){
         angular.element('body .modal-backdrop ').remove();
     });
->>>>>>> c9c428e874fe80dfa2080aa908b673fd110b0d53
+
 }]);
