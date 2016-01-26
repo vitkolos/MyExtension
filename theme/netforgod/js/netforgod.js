ece4a293352c8266b9b371c7b4de47e4183bd77a
nicolasrhone <nicolas.rhone@gmail.com>
2016-01-26 16:24:56 +0100

    point net

--- theme/netforgod/js/netforgod.js
+++ theme/netforgod/js/netforgod.js
@@ -13,7 +13,11 @@ blocksConfig.navigation = {
         },
 
 
-
+angular.module('rubedoBlocks').filter('firstUpper', function() {
+    return function(input, scope) {
+        return input ? input.substring(0,1).toUpperCase()+input.substring(1).toLowerCase() : "";
+    }
+});
 
 
 
