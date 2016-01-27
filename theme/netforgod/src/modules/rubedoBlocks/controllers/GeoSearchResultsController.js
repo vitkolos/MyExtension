89b28e2fa044fcedd4f41b859263e1f295188a8b
nicolasrhone <nicolas.rhone@gmail.com>
2016-01-27 11:08:22 +0100

    test

--- theme/netforgod/src/modules/rubedoBlocks/controllers/GeoSearchResultsController.js
+++ theme/netforgod/src/modules/rubedoBlocks/controllers/GeoSearchResultsController.js
@@ -119,6 +119,7 @@ angular.module("rubedoBlocks").lazy.controller("GeoSearchResultsController",["$s
                     angular.element("body,html").animate({scrollTop: target.offset().top}, "slow");
                 }
                 console.log(me.data);
+                console.log(markerOptions);
                 
                 
             }
