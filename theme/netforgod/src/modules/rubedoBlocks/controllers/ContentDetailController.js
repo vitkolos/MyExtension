1887d7e4cab9b8304cb0446cfd2c2f00adb32df2
nicolasrhone <nicolas.rhone@gmail.com>
2016-01-28 18:39:52 +0100

    test

--- theme/netforgod/src/modules/rubedoBlocks/controllers/ContentDetailController.js
+++ theme/netforgod/src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -164,7 +164,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
                         });
                     }
                     /* déterminer si on a un film ou trailer*/
-                    if ($scope.fieldEntity['filmYT']) {
+                    if ($scope.fieldEntity['filmYT'] && $scope.fieldEntity['filmYT'] !=[]) {
                         me.watch = 'film';
                         console.log("film");
                     }
