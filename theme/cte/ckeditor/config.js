490391140a13c920c6313515bad0dba36991c789
Nicolas Rhoné <nicolas.rhone@gmail.com>
2016-02-01 12:53:14 +0100

    cte box link

--- theme/cte/ckeditor/config.js
+++ theme/cte/ckeditor/config.js
@@ -7,6 +7,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+        config.colorButton_colors = 'Rouge Brique/c1573c,Texte noir/2e2c29,Marron/d5c5b5';
+
 
 };
 CKEDITOR.plugins.addExternal( 'youtube', '/theme/goodnews/ckeditor/plugins/youtube/','plugin.js' );
