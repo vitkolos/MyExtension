57d17d1a2cb273b4de0bcc93b151da044decb5d7
nicolasrhone <nicolas.rhone@gmail.com>
2016-02-05 15:21:31 +0100

    adc

--- src/MyExtension/Rest/V1/AcnProductResource.php
+++ src/MyExtension/Rest/V1/AcnProductResource.php
@@ -14,275 +14,75 @@
  * @copyright  Copyright (c) 2012-2014 WebTales (http://www.webtales.fr)
  * @license    http://www.gnu.org/licenses/gpl.html Open Source GPL 3.0 license
  */
-
 namespace RubedoAPI\Rest\V1;
-use Rubedo\Collection\AbstractLocalizableCollection;
 use Rubedo\Services\Manager;
 use RubedoAPI\Entities\API\Definition\FilterDefinitionEntity;
-use RubedoAPI\Entities\API\Definition\VerbDefinitionEntity;
-use RubedoAPI\Exceptions\APIAuthException;
-use RubedoAPI\Exceptions\APIEntityException;
 use RubedoAPI\Exceptions\APIRequestException;
+use RubedoAPI\Entities\API\Definition\VerbDefinitionEntity;
 use WebTales\MongoFilters\Filter;
-use Rubedo\Content\Context;
 /**
- * Class ContentsResource
+ * Class TaxonomiesResource
  * @package RubedoAPI\Rest\V1
  */
 class AcnproductResource extends AbstractResource
 {
     /**
+     * @var static
+     */
+    /**
      * Cache lifetime for api cache (only for get and getEntity)
      * @var int
      */
-    public $cacheLifeTime=60;
-    /**
-     * @var array
-     */
-    protected $toExtractFromFields = array('text');
-    /**
-     * @var array
-     */
-    protected $otherLocalizableFields = array('text', 'summary');
-    /**
-     * @var array
-     */
-    protected $returnedEntityFields = array(
-        'id',
-        'text',
-        'version',
-        'createUser',
-        'lastUpdateUser',
-        'fields',
-        'taxonomy',
-        'status',
-        'pageId',
-        'maskId',
-        'locale',
-        'readOnly',
-        'createTime',
-        'lastUpdateTime',
-        'isProduct',
-        'productProperties'
-    );
+    public $cacheLifeTime=600;
     /**
      * { @inheritdoc }
      */
     public function __construct()
     {
         parent::__construct();
-        $this->define();
-    }
-     /**
-     * Remove fields if not in content type
-     *
-     * @param $type
-     * @param $fields
-     */
-    protected function filterFields($type, $fields)
-    {
-        $existingFields = array();
-        foreach ($type['fields'] as $field) {
-            if (!($field['config']['localizable'] || in_array($field['config']['name'], $this->otherLocalizableFields))) {
-                $existingFields[] = $field['config']['name'];
-            }
-        }
-        foreach ($fields as $key => $value) {
-            unset($value); //unused
-            if (!in_array($key, $existingFields)) {
-                unset ($fields[$key]);
-            }
-        }
-        return $fields;
-    }
-    /**
-     * Return localizable fields if not in content type
-     *
-     * @param $type
-     * @param $fields
-     */
-    protected function localizableFields($type, $fields)
-    {
-        $existingFields = array();
-        foreach ($type['fields'] as $field) {
-            if ($field['config']['localizable']) {
-                $existingFields[] = $field['config']['name'];
-            }
-        }
-        foreach ($fields as $key => $value) {
-            unset($value); //unused
-            if (!(in_array($key, $existingFields) || in_array($key, $this->otherLocalizableFields))) {
-                unset ($fields[$key]);
-            }
-        }
-        return $fields;
-    }
-    /**
-     * Filter contents
-     *
-     * @param $contents
-     * @param $params
-     * @return mixed
-     */
-    protected function outputContentsMask($contents, $params, $query)
-    {
-        $fields = isset($params['fields']) ? $params['fields'] : array('text', 'summary', 'image');
-        $queryReturnedFields = !empty($query["returnedFields"]) && is_array($query["returnedFields"]) ? $query["returnedFields"] : array();
-        $fields = array_merge($fields, $queryReturnedFields);
-        $urlService = $this->getUrlAPIService();
-        if (isset($params['pageId'],$params['siteId'])){
-            $page = $this->getPagesCollection()->findById($params['pageId']);
-            $site = $this->getSitesCollection()->findById($params['siteId']);
-        }
-        $mask = array('isProduct', 'i18n', 'pageId', 'blockId', 'maskId');
-        foreach ($contents as &$content) {
-            $content['fields'] = array_intersect_key($content['fields'], array_flip($fields));
-            if (isset($params['pageId'],$params['siteId'])) {
-                $content['detailPageUrl'] = $urlService->displayUrlApi($content, 'default', $site,
-                    $page, $params['lang']->getLocale(), isset($params['detailPageId']) ? (string)$params['detailPageId'] : null);
-            }
-            $content = array_diff_key($content, array_flip($mask));
-        }
-        return $contents;
-    }
-    /**
-     * Get content list
-     *
-     * @param $filters
-     * @param $pageData
-     * @param bool $ismagic
-     * @return array
-     * @throws \RubedoAPI\Exceptions\APIEntityException
-     */
-    protected function getContentList($filters, $pageData, $ismagic = false)
-    {
-        $filters["sort"] = isset($filters["sort"]) ? $filters["sort"] : array();
-        $contentArray = $this->getContentsCollection()->getOnlineList($filters["filter"], $filters["sort"], $pageData['start'], $pageData['limit'], $ismagic);
-        $contentArray['page'] = $pageData;
-        if ($contentArray['count'] < $pageData['start']) {
-            throw new APIEntityException('There is only ' . $contentArray['count'] . ' contents. Start parameter must be inferior of this value', 404);
-        }
-        return $contentArray;
-    }
-    /**
-     * Set pagination value
-     *
-     * @param $params
-     * @return mixed
-     * @throws \RubedoAPI\Exceptions\APIEntityException
-     */
-    protected function setPaginationValues($params)
-    {
-        $defaultLimit = isset($params['limit']) ? $params['limit'] : 6;
-        $defaultStart = isset($params['start']) ? $params['start'] : 0;
-        if ($defaultStart < 0) {
-            throw new APIEntityException('Start paramater must be >= 0', 404);
-        }
-        if ($defaultLimit < 1) {
-            throw new APIEntityException('Limit paramater must be >= 1', 404);
-        }
-        $pageData['start'] = $defaultStart;
-        $pageData['limit'] = $defaultLimit;
-        return $pageData;
-    }
+        $this
+            ->definition
+            ->setName('Products')
+            ->setDescription('Deal with taxonomy')
+            ->editVerb('get', function (VerbDefinitionEntity &$entity) {
+                $entity
+                    ->setDescription('Get taxonomy terms')
+                    ->addInputFilter(
+                        (new FilterDefinitionEntity())
+                            ->setKey('codeBarre')
+                            ->setRequired()
+                            ->setDescription('Taxonomy vocabularies to use')
+                    )
+                    ->addOutputFilter(
+                        (new FilterDefinitionEntity())
+                            ->setKey('content')
+                            ->setDescription('Taxonomy terms array for selected vocabularies')
+                    );
+            });
+            $this->contentsService = Manager::getService('Contents');
 
+    }
     /**
-     * Get to acnproduct/{id}
+     * Get from /taxonomy
      *
-     * @param $id
      * @param $params
      * @return array
-     * @throws \RubedoAPI\Exceptions\APIEntityException
-     * @throws \RubedoAPI\Exceptions\APIRequestException
      */
-    public function getEntityAction($id, $params)
+    public function getAction($params)
     {
-        $filter = Filter::factory('Value')->setName('productProperties.sku')->setValue("1155022489");
+        $codeBarre=$params['codeBarre'];
+        $filters = Filter::factory();
+        $filters->addFilter(Filter::factory('Value')->setName('fields.codeBarre')->setValue($codeBarre));
+        $content = $this->contentsService->getList($filters);
 
-        $contentsService = Manager::getService("Contents");
-        //$content = $contentsService->getContentList($filter);
-        
-        //$contentArray = $contentsService->getList($filter);
         
         
-        //$content = $this->getContentsCollection()->findById($id, $getLive, false);
-        $content = $this->getContentsCollection()->getOnlineList($filter);
-        //$content = Manager::getService('Contents')->findOne($filter,false);
         return [
             'success' => true,
-            'content' => $content
+            'content' => $content,
         ];
     }
-    /**
-     * Define the resource
-     */
-    protected function define()
-    {
+    
 
-        $this
-            ->entityDefinition
-            ->setName('Acn product')
-            ->setDescription('Works on single product')
-            ->editVerb('get', function (VerbDefinitionEntity &$definition) {
-                $this->defineEntityGet($definition);
-            });
-    }
 
-    /**
-     * Define get entity
-     *
-     * @param VerbDefinitionEntity $definition
-     */
-    protected function defineEntityGet(VerbDefinitionEntity &$definition)
-    {
-        $definition
-            ->setDescription('Get a content')
-            ->addInputFilter(
-                (new FilterDefinitionEntity())
-                    ->setDescription('Fields to return')
-                    ->setKey('fields')
-            )
-            ->addInputFilter(
-                (new FilterDefinitionEntity())
-                    ->setKey('siteId')
-                    ->setDescription('Id of the site')
-                    ->setFilter('\\MongoId')
-            )
-            ->addInputFilter(
-                (new FilterDefinitionEntity())
-                    ->setKey('pageId')
-                    ->setDescription('Id of the page')
-                    ->setFilter('\\MongoId')
-            )
-            ->addInputFilter(
-                (new FilterDefinitionEntity())
-                    ->setKey('useDraftMode')
-                    ->setDescription('Set to true to preview draft contents')
-            )
-            ->addInputFilter(
-                (new FilterDefinitionEntity())
-                    ->setKey('fingerprint')
-                    ->setDescription('Fingerprint')
-            )->addInputFilter(
-                (new FilterDefinitionEntity())
-                    ->setKey('includeTermLabels')
-                    ->setDescription('Include labels for taxonomy terms')
-            )
-            ->addOutputFilter(
-                (new FilterDefinitionEntity())
-                    ->setDescription('The content')
-                    ->setKey('content')
-                    ->setRequired()
-            );
-    }    /**
-     * Add product filter
-     *
-     * @return $this
-     */
-    protected function productFilter()
-    {
-        return Filter::factory('Or')
-            ->addFilter(Filter::factory('OperatorToValue')->setName('isProduct')->setOperator('$exists')->setValue(false))
-            ->addFilter(Filter::factory('Value')->setName('isProduct')->setValue(false));
-    }
 }
