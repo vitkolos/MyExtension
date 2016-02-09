4a2c3be64a9eab29d04f01474611d675959157ad
nicolasrhone <nicolasrhone@users.noreply.github.com>
2016-02-10 00:26:36 +0100

    Update PayboxIpnResource.php

--- src/MyExtension/Rest/V1/PayboxIpnResource.php
+++ src/MyExtension/Rest/V1/PayboxIpnResource.php
@@ -131,6 +131,7 @@ if(!($erreurStatus) && $securite && $autorisation) {
         }
         if ($erreur == "00000") {
             $body = "Montant payé : " . $params['montant']/100 . " euros.\n" ;
+            $body.=$contentId;
             $body .= "Proposition : " . $inscription['fields']['propositionTitre']."\n";
             $body .= "Code Onesime : " . $inscription['fields']['codeOnesime']."\n";
             $body .= "Code Compta : " . $inscription['fields']['codeCompta']."\n";
