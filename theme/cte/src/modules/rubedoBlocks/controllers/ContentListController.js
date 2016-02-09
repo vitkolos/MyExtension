210ab1527de128f69ffcaa71a7a9ea344c8c2d69
nicolasrhone <nicolas.rhone@gmail.com>
2016-02-09 13:59:23 +0100

    test

--- theme/cte/src/modules/rubedoBlocks/controllers/ContentListController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/ContentListController.js
@@ -299,12 +299,12 @@ angular.module("rubedoBlocks").lazy.controller("ContentListDetailController",['$
 
 angular.module('rubedoBlocks').filter('tags', function() {
     return function(contents, tag) {
-        var contentList=[];
+        /*var contentList=[];
         angular.forEach(contents, function(content){
             if (content.taxonomy['5524db6945205e627a8d8c4e'].indexOf(tag) != -1) {
                 contentList.push(content);
             }            
-        })
-        return contentList;
+        })*/
+        return contents;
     };
 });
