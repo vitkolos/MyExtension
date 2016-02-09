5b89fa2b3919cc3c9b5072084018bddac2c791a1
nicolasrhone <nicolas.rhone@gmail.com>
2016-02-09 14:31:28 +0100

    suite

--- theme/cte/js/cte.js
+++ theme/cte/js/cte.js
@@ -41,13 +41,14 @@ angular.module('rubedo').filter('tags', function() {
     return function(contents, tag) {
         var contentList=[];
         angular.forEach(contents, function(content){
-           console.log(content);
-           if(content.taxonomy['5524db6945205e627a8d8c4e'] && content.taxonomy['5524db6945205e627a8d8c4e'].indexOf(tag) != -1){
+           
+           if(content.taxonomy['5524db6945205e627a8d8c4e'] && (content.taxonomy['5524db6945205e627a8d8c4e']).indexOf(tag) != -1){
                       contentList.push(content);
+                      console.log(content);
            }
         })
         return contentList;
-console.log(contentList.length());
+console.log(contentList.length);
     };
 });
 
