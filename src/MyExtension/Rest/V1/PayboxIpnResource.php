0edf371f3ee4292fcef0fa67a25f9587f58d7d16
nicolasrhone <nicolasrhone@users.noreply.github.com>
2016-02-10 00:20:24 +0100

    Update PayboxIpnResource.php

--- src/MyExtension/Rest/V1/PayboxIpnResource.php
+++ src/MyExtension/Rest/V1/PayboxIpnResource.php
@@ -112,7 +112,23 @@ if(!($erreurStatus) && $securite && $autorisation) {
             $contentsService = Manager::getService("Contents");
             $inscription = $contentsService->findById($contentId,false,false);
             AbstractCollection::disableUserFilter(false);
-            
+            if($inscription['success']) {
+                $inscription = $inscription['content'];
+                //vérifier si le montant payé est le même que celui indiqué lors de l'inscription
+                //if( $montant == (int)$inscription['fields']['montantAPayerMaintenant']) {
+                //    $inscription['fields']['statut'] = "paiement-carte-valide" ;
+                //}
+                //else $erreurMessage .="Le montant du paiement est différent de celui envoyé à Paybox.";
+                $mailSecretariat = $inscription['fields']['mailSecretariat'];
+                
+                $payload = json_encode( array( "content" => $inscription ) );
+               //authentication comme admin inscriptions
+               //$auth = $this->getAuthAPIService()->APIAuth('admin_inscriptions', '2qs5F7jHf8KD');
+                //$output['token'] = $this->subTokenFilter($auth['token']);
+                //$token = $output['token']['access_token'];
+                 //$resultUpdate = $this->callAPI("PATCH", $token, $payload, $contentId);
+             }
+            else $erreurMessage .="Le payement ".$idInscription." n'a pas été retrouvé";
  
         }    
 
