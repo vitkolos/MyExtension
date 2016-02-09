ea47e8e0802c87bf56877ddf7f57e6151ce57755
nicolasrhone <nicolas.rhone@gmail.com>
2016-02-09 11:52:31 +0100

    suite$

--- theme/cte/js/cte.js
+++ theme/cte/js/cte.js
@@ -129,7 +129,16 @@ angular.module('rubedoBlocks').controller("AudioFileController",["$scope","Rubed
     };
 });
 
-
+angular.module('rubedoBlocks').filter('selectedTags', function($filter) {
+    return function(contents, tag) {
+        return contents.filter(function(content) {
+            if (content.taxonomy['5524db6945205e627a8d8c4e'].indexOf(tag) != -1) {
+                return true;
+            }
+            else return false;
+       });
+    };
+});
 
 
 
