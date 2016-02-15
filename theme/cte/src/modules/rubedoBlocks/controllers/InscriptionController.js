f4cdf441a65db03a1365d9397b2f4bc3db2a4fe2
nicolasrhone <nicolas.rhone@gmail.com>
2016-02-15 18:31:46 +0100

    suite

--- theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
@@ -27,7 +27,9 @@ angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope'
     
     $scope.inscription.personneConnue = false;
     $scope.inscription.entretien = false;
-    $scope.inscription.motivation = false;                    
+    $scope.inscription.motivation = false;
+        $scope.inscription.enfants=[];
+    $scope.inscription.enfant={};
     // vérifier les infos complémentaires pour le formulaire
     if ((me.content.fields.questions1) && me.content.fields.questions1.questions1 && ((me.form.content.questions1.questions1).length>0)) {
         if(typeof me.content.fields.questions1.questions1 =='string') {
@@ -62,15 +64,14 @@ angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope'
             pageId: $scope.rubedo.current.page.id
     };
     // ajouter enfants
-    $scope.inscription.enfants=[];
-    $scope.inscription.enfant={};
+
     me.addChild = function(enfant){
         console.log($scope);
         console.log(enfant);
-        if ($scope.inscription.enfants) {
-            $scope.inscription.enfants.push(angular.copy(enfant));
+        if (!$scope.inscription.enfants) {
+            $scope.inscription.enfants=[];
         }
-        else $scope.inscription.enfants=array(enfant);
+        $scope.inscription.enfants=array(enfant);
     }
     // supprimer enfant
     me.removeChild = function(index){
