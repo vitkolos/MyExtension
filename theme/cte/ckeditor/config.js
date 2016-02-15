0fe5ed62a3f88b84723111b841c664834c8c1287
nicolasrhone <nicolas.rhone@gmail.com>
2016-02-15 13:59:22 +0100

    check youtube responsiv

--- theme/cte/ckeditor/config.js
+++ theme/cte/ckeditor/config.js
@@ -9,6 +9,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.uiColor = '#AADC6E';
         config.colorButton_colors = 'c1573c,2e2c29,d5c5b5,898989,ffffff';
         config.colorButton_enableMore = false;
+        config.youtube_responsive = true;
 
 
 
