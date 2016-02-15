358d0866d6588f148508d856e8cc874b8ae8ab7f
nicolasrhone <nicolas.rhone@gmail.com>
2016-02-15 14:28:44 +0100

    fix

---
+++ theme/jmj2016/src/modules/rubedoBlocks/controllers/ExternalMediaController.js
@@ -0,0 +1,15 @@
+angular.module("rubedoBlocks").lazy.controller("ExternalMediaController",['$scope','$http','$sce',function($scope,$http,$sce){
+    var me=this;
+    var config=$scope.blockConfig;
+    me.config=$scope.blockConfig;
+    if ((config)&&(config.url)){
+        var url = "http://iframe.ly/api/oembed?url="+encodeURIComponent(config.url);
+        if ($scope.rubedo.current.site.iframelyKey){
+            url=url+"&api_key="+$scope.rubedo.current.site.iframelyKey;
+        }
+        url=url+"&callback=JSON_CALLBACK";
+        $http.jsonp(url).success(function(response){
+            me.html=$sce.trustAsHtml(response.html);
+        });
+    }
+}]);
