b7999061811b957c4e2b260482c3885ac9e77863
nicolasrhone <nicolas.rhone@gmail.com>
2016-02-15 14:43:37 +0100

    fix

--- theme/jmj2016/js/jmj.js
+++ theme/jmj2016/js/jmj.js
@@ -45,7 +45,7 @@ angular.module('rubedoBlocks').directive('scrollToAnchor', function ($location, 
     
 });
 
- angular.module('rubedoBlocks').directive('jwplayer', ['$compile', '$timeout',function ($compile,$timeout) {
+ angular.module('rubedoBlocks').directive('jwplayer', ['$compile',function ($compile) {
     return {
         restrict: 'EC',
         link: function (scope, element, attrs) {
@@ -63,7 +63,9 @@ angular.module('rubedoBlocks').directive('scrollToAnchor', function ($location, 
                       aspectratio:"16:9"};
             element.html(getTemplate(id));
             $compile(element.contents())(scope);
-            timer(jwplayer(id).setup(options), 100);
+            setTimeout(function(){
+                      jwplayer(id).setup(options);
+           }, 200);
 
             
             
