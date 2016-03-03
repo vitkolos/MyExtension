57b71320882b61f1a249af47f4ea7cb73204edf8
EtienneLaval <etienne.laval.moi@gmail.com>
2016-03-03 17:38:45 +0100

    fix

--- theme/boutique/src/modules/rubedoBlocks/controllers/AuthenticationController.js
+++ theme/boutique/src/modules/rubedoBlocks/controllers/AuthenticationController.js
@@ -15,14 +15,10 @@ angular.module("rubedoBlocks").lazy.controller("AuthenticationController",["$sco
             }
         });
     }
-    alert("out");
     if (me.blockConfig&&me.blockConfig.commandPage&&mongoIdRegex.test(me.blockConfig.commandPage)){
-        alert(0);
         RubedoPagesService.getPageById(me.blockConfig.commandPage).then(function(response){
-            alert(1);
             if (response.data.success){
                 me.commandPageUrl=response.data.url;
-                alert(2);
             }
         });
     }
