07c1bf261d099e882fcad7133f64236eb95f4a3a
Nicolas Rhoné <nicolas.rhone@gmail.com>
2016-03-05 17:30:49 +0100

    css nfg

--- theme/netforgod/ckeditor/default.js
+++ theme/netforgod/ckeditor/default.js
@@ -7,9 +7,9 @@ CKEDITOR.addTemplates("default",{imagesPath:"/theme/netforgod/ckeditor/img/",tem
     {title:"Texte sur deux colonnes",image:"w50w50.png",description:"Texte sur deux colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
     html:'<div class="col-xs-12  col-sm-6"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-6"><p>Colonne 2 </p></div>'},
     {title:"Texte sur trois colonnes",image:"w33w33.png",description:"Texte sur trois colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
-    html:'<div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div>'},
+    html:'<div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div><p></p>'},
 {title:"Texte sur deux colonnes",image:"w33w33.png",description:"Texte sur 2 colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
-    html:'<div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-8"><p>Colonne 2 </p></div>'},
+    html:'<div class="row"><div class="col-xs-12  col-sm-5"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-7"><p>Colonne 2 </p></div></div><p></p>'},
 {title:"Texte sur deux colonnes inversé",image:"w33w33.png",description:"Texte sur 2 colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
-    html:'<div class="col-xs-12  col-sm-8"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div>'}
+    html:'<div class="row"><div class="col-xs-12  col-sm-7"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-5"><p>Colonne 2 </p></div></div><p></p>'}
     ]});
