b63b79bd9975c2d25c1daead038fdb43cf638c8b
Nicolas Rhoné <nicolas.rhone@gmail.com>
2016-03-05 16:53:03 +0100

    css nfg

---
+++ theme/netforgod/ckeditor/config.js
@@ -0,0 +1,22 @@
+﻿/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+        config.colorButton_colors = '00a0ae,1d8f9e,0073b3,f19942,e71623,e3dfd7,807f7d,333333';
+        config.colorButton_enableMore = false;
+        config.youtube_responsive = true;
+
+};
+
+CKEDITOR.stylesSet.add( 'default', [
+
+
+] );
+
+
+CKEDITOR.plugins.addExternal( 'youtube', '/theme/goodnews/ckeditor/plugins/youtube/','plugin.js' );
---
+++ theme/netforgod/ckeditor/default.js
@@ -0,0 +1,15 @@
+﻿/*
+ Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.addTemplates("default",{imagesPath:"/theme/netforgod/ckeditor/img/",templates:[
+
+    {title:"Texte sur deux colonnes",image:"w50w50.png",description:"Texte sur deux colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
+    html:'<div class="col-xs-12  col-sm-6"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-6"><p>Colonne 2 </p></div>'},
+    {title:"Texte sur trois colonnes",image:"w33w33.png",description:"Texte sur trois colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
+    html:'<div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div>'},
+{title:"Texte sur deux colonnes",image:"w33w33.png",description:"Texte sur 2 colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
+    html:'<div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-8"><p>Colonne 2 </p></div>'},
+{title:"Texte sur deux colonnes inversé",image:"w33w33.png",description:"Texte sur 2 colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
+    html:'<div class="col-xs-12  col-sm-8"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div>'}
+    ]});
