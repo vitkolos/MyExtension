76da0f44de05b4603390ba84934d054d2b84bf04
EtienneLaval <etienne.laval.moi@gmail.com>
2016-03-09 21:02:38 +0100

    suite

--- theme/boutique/src/modules/rubedoBlocks/controllers/CarouselController.js
+++ theme/boutique/src/modules/rubedoBlocks/controllers/CarouselController.js
@@ -55,5 +55,5 @@ angular.module("rubedoBlocks").lazy.controller("CarouselController",["$scope","R
     if (blockConfig.query){
         me.getContents();
     }
-    alert('hello world');
+    alert(block.code);
 }]);
