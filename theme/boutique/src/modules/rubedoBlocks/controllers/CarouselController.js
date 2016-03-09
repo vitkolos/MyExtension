6e977db18ba49fa80c0ceb6c067e1e1f4733bcc5
EtienneLaval <etienne.laval.moi@gmail.com>
2016-03-09 20:47:47 +0100

    suite

---
+++ theme/boutique/src/modules/rubedoBlocks/controllers/CarouselController.js
@@ -0,0 +1,59 @@
+/**
+ * Created by Etienne on 09/03/2016.
+ */
+angular.module("rubedoBlocks").lazy.controller("CarouselController",["$scope","RubedoContentsService",function($scope,RubedoContentsService){
+    var me=this;
+    me.contents=[];
+    var blockConfig=$scope.blockConfig;
+    var queryOptions={
+        start: blockConfig.resultsSkip ? blockConfig.resultsSkip : 0,
+        limit: blockConfig.pageSize ? blockConfig.pageSize : 6,
+        'fields[]' : ["text","summary",blockConfig.imageField],
+        ismagic: blockConfig.magicQuery ? blockConfig.magicQuery : false
+    };
+    if (blockConfig.imageField && blockConfig.imageField!="") {
+        queryOptions['requiredFields[]'] = [blockConfig.imageField];
+    }
+    if(blockConfig.singlePage){
+        queryOptions.detailPageId = blockConfig.singlePage;
+    }
+    var pageId=$scope.rubedo.current.page.id;
+    var siteId=$scope.rubedo.current.site.id;
+    $scope.isArray = angular.isArray;
+    me.getContents=function(){
+        RubedoContentsService.getContents(blockConfig.query,pageId,siteId, queryOptions).then(
+            function(response){
+                if (response.data.success){
+                    me.contents=response.data.contents;
+                    setTimeout(function(){me.initCarousel();},100);
+                }
+            }
+        );
+    };
+    me.initCarousel=function(){
+        var targetElSelector="#block"+$scope.block.id;
+        var owlOptions={
+            responsiveBaseWidth:targetElSelector,
+            singleItem:true,
+            pagination: blockConfig.showPager,
+            navigation: blockConfig.showNavigation,
+            autoPlay: blockConfig.autoPlay,
+            stopOnHover: blockConfig.stopOnHover,
+            paginationNumbers:blockConfig.showPagingNumbers,
+            navigationText: ['<span class="glyphicon glyphicon-chevron-left"></span>','<span class="glyphicon glyphicon-chevron-right"></span>'],
+            lazyLoad:true
+        };
+        angular.element(targetElSelector).owlCarousel(owlOptions);
+    };
+    me.getImageOptions=function(){
+        return({
+            height:blockConfig.imageHeight,
+            width:blockConfig.imageWidth ? blockConfig.imageWidth : angular.element("#block"+$scope.block.id).width(),
+            mode:blockConfig.imageResizeMode
+        });
+    };
+    if (blockConfig.query){
+        me.getContents();
+    }
+    alert('hello world');
+}]);
