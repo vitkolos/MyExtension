4977fbaa826c0239a3fcde9eda9d47bb83bc0949
nicolasrhone <nicolas.rhone@gmail.com>
2016-03-14 18:20:51 +0100

    dons

--- theme/cte/src/modules/rubedoBlocks/controllers/DonationController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/DonationController.js
@@ -1,10 +1,13 @@
-angular.module("rubedoBlocks").lazy.controller("DonationController",['$scope',function($scope) {
+angular.module("rubedoBlocks").lazy.controller("DonationController",['$scope','RubedoUserTypesService','RubedoUsersService','RubedoAuthService','RubedoMailingListService',
+                                                                     function($scope,RubedoUserTypesService,RubedoUsersService,RubedoAuthService,RubedoMailingListService) {
     var me = this;
     var themePath='/theme/'+window.rubedoConfig.siteTheme;
     //templates
     me.donationTemplate = themePath+'/templates/blocks/donation.html';
     me.currentStage=1;
+    me.userType="56e6edeac445eccc038b5b8e"; // type d'utilisateurs = donateurs
     $scope.don= {};
+
     me.toggleStage = function(newStage){
        angular.element("#inscriptionStage"+me.currentStage).collapse("hide");
        angular.element("#inscriptionStage"+newStage).collapse("show");
@@ -14,7 +17,125 @@ angular.module("rubedoBlocks").lazy.controller("DonationController",['$scope',fu
         if(valide){
             me.toggleStage(step);
         }  
+    };
+    me.parseUserType=function(userType){
+        me.userType=userType;
+        $scope.fieldIdPrefix="checkout"+"_"+me.userType.type;
+        if (!$scope.rubedo.current.user){
+            me.userType.fields.unshift({
+                cType:"textfield",
+                config:{
+                    name:"confirmPassword",
+                    fieldLabel:$scope.rubedo.translate("Blocks.SignUp.label.confirmPassword"),
+                    allowBlank:false,
+                    vtype:"password"
+                }
+            });
+            me.userType.fields.unshift({
+                cType:"textfield",
+                config:{
+                    name:"password",
+                    fieldLabel:$scope.rubedo.translate("Blocks.SignUp.label.password"),
+                    allowBlank:false,
+                    vtype:"password"
+                }
+            });
+        }
+        me.userType.fields.unshift({
+            cType:"textfield",
+            config:{
+                name:"email",
+                fieldLabel:$scope.rubedo.translate("Label.Email"),
+                allowBlank:false,
+                vtype:"email"
+            }
+        });
+        me.userType.fields.unshift({
+            cType:"textfield",
+            config:{
+                name:"name",
+                fieldLabel:$scope.rubedo.translate("Blocks.SignUp.label.name"),
+                allowBlank:false
+            }
+        });
+        me.inputFields=me.userType.fields;
+    };
+    me.initializeCheckout=function(){
+        $scope.fieldIdPrefix="checkout";
+
+        $scope.fieldInputMode=true;
+        if (!$scope.rubedo.current.user){
+            if (me.userType){
+                RubedoUserTypesService.getUserTypeById(me.userType).then(
+                    function(response){
+                        if (response.data.success){
+                            me.parseUserType(response.data.userType);
+                        }
+                    }
+                );
+            }
+        } else {
+            RubedoUsersService.getUserById($scope.rubedo.current.user.id).then(
+                function(response){
+                    if (response.data.success){
+                        me.currentUser=response.data.user;
+                        var existingData=angular.copy(me.currentUser.fields);
+                        
+                        $scope.fieldEntity=existingData;
+                        me.parseUserType(me.currentUser.type);
+                        me.setCurrentStage(2,true);
+                    }
+                }
+            );
+        }
+    };
+    me.createUser=function(){
+        if ($scope.fieldEntity.confirmPassword!=$scope.fieldEntity.password){
+            me.stage2Error="Passwords do not match.";
+            return;
+        }
+        var newUserFields=angular.copy($scope.fieldEntity);
+        delete (newUserFields.confirmPassword);
+        newUserFields.login=newUserFields.email;
+
+        RubedoUsersService.createUser(newUserFields,me.userType.id).then(
+            function(response){
+                if (response.data.success){
+                    RubedoAuthService.generateToken({login:newUserFields.login,password:newUserFields.password},me.rememberMe).then(
+                        function(authResponse){
+                            var mailingListsSuscribe=[];
+                            angular.forEach(me.mailingLists, function(mailingList){
+                                if(mailingList.checked){
+                                    mailingListsSuscribe.push(mailingList.id);
+                                }
+                            });
+                            if (mailingListsSuscribe.length>0){
+                                var mloptions = {
+                                    mailingLists: mailingListsSuscribe,
+                                    email: newUserFields.email
+                                };
+                                RubedoMailingListService.subscribeToMailingLists(mloptions).then(function(mlresponse){
+                                    window.location.reload();
+                                },function(mlresponse){
+                                    window.location.reload();
+                                });
+                            } else{
+                                window.location.reload();
+                            }
+                        }
+                    );
+                }
+            },
+            function(response){
+                me.stage2Error=response.data.message;
+            }
+        );
     };    
+    
+    
+        me.initializeCheckout();
+
+    
 }]);
 
 
