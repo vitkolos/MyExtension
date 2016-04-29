c52d389edb6243c9f66d55d1ab184491c54f7ba0
nicolasrhone <nicolas.rhone@gmail.com>
2016-04-29 18:03:01 +0200

    suite

--- config/module.config.php
+++ config/module.config.php
@@ -206,7 +206,7 @@ return array(
     'service_manager' => array(
         'invokables' => array(
             'PayboxPayment'=>'Rubedo\\Payment\\PayboxPayment',
-            'ContentsCcn'=>'Rubedo\\Collection\\ContentsCcn'
+            'ContentsCcn'=>'MyExtension\\Collection\\ContentsCcn'
             )
     ),
     /*
--- src/MyExtension/Collection/ContentsCcn.php
+++ src/MyExtension/Collection/ContentsCcn.php
@@ -11,7 +11,7 @@
  * @copyright Copyright (c) 2012-2014 WebTales (http://www.webtales.fr)
  * @license http://www.gnu.org/licenses/gpl.html Open Source GPL 3.0 license
  */
-namespace Rubedo\Collection;
+namespace MyExtension\Collection;
 use Rubedo\Content\Context;
 use Rubedo\Exceptions\Access;
 use Rubedo\Exceptions\Server;
