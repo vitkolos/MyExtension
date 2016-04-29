2075133fc91bc62294d40e97c18d10057d9a496b
nicolasrhone <nicolas.rhone@gmail.com>
2016-04-29 17:49:33 +0200

    fix

--- config/module.config.php
+++ config/module.config.php
@@ -206,7 +206,7 @@ return array(
     'service_manager' => array(
         'invokables' => array(
             'PayboxPayment'=>'Rubedo\\Payment\\PayboxPayment',
-            'ContentsCCN'=>'Rubedo\\Collection\\ContentsCcn'
+            'ContentsCcn'=>'Rubedo\\Collection\\ContentsCcn'
             )
     ),
     /*
