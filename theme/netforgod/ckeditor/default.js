db0fb5e86ecc76c396e9cbe994d2078127a557cf
nicolasrhone <nicolas.rhone@gmail.com>
2016-05-09 11:29:39 +0200

    css

--- theme/netforgod/ckeditor/default.js
+++ theme/netforgod/ckeditor/default.js
@@ -5,7 +5,7 @@
 CKEDITOR.addTemplates("default",{imagesPath:"/theme/netforgod/ckeditor/img/",templates:[
 
     {title:"Texte sur deux colonnes",image:"w50w50.png",description:"Texte sur deux colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
-    html:'<div class="col-xs-12  col-sm-6"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-6"><p>Colonne 2 </p></div>'},
+    html:'<div class="row padding-large"><div class="col-xs-12  col-sm-6 "><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-6"><p>Colonne 2 </p></div></div>'},
     {title:"Texte sur trois colonnes",image:"w33w33.png",description:"Texte sur trois colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
     html:'<div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div><p></p>'},
 {title:"Texte sur deux colonnes",image:"w33w33.png",description:"Image à gauchet et texte à droite",
