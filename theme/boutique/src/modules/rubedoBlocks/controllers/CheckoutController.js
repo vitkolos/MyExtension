294154048337d9aff8ca5ce790ddf1fad08ea9f1
Etienne Laval <etienne.laval.moi@gmail.com>
2016-05-09 17:43:28 +0200

    suite

--- theme/boutique/src/modules/rubedoBlocks/controllers/CheckoutController.js
+++ theme/boutique/src/modules/rubedoBlocks/controllers/CheckoutController.js
@@ -43,7 +43,9 @@ angular.module("rubedoBlocks").lazy.controller("CheckoutController",["$scope","R
             if ((array[i].title=="Papier Cadeau")&&(array[i].price==2)){isHere=true}
     // //       if ((array[i].title=="Papier Cadeau"||array[i].title==rubedo.translate('Label.GiftWrap','Gift Wrap'))&&(array[i].price==2)){isHere=true}
         }
+        alert (isHere||$scope.itIsNowAGift);
         return isHere||$scope.itIsNowAGift;
+
     };
     me.getCart=function(){
         RubedoShoppingCartService.getCart({includeDetail:true}).then(
