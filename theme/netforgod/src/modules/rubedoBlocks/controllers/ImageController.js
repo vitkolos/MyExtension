ecf612a0e512cd4cee8fd89603a51e72cb1234cb
nicolasrhone <nicolas.rhone@gmail.com>
2016-05-17 11:05:34 +0200

    add image nfg

---
+++ theme/netforgod/src/modules/rubedoBlocks/controllers/ImageController.js
@@ -0,0 +1,25 @@
+angular.module("rubedoBlocks").lazy.controller("ImageController",["$scope","$http","RubedoPagesService", function($scope,$http,RubedoPagesService){
+    var me = this;
+    var config = $scope.blockConfig;
+    me.imageTitle = "";
+    if (config.externalURL){
+        me.url=config.externalURL;
+    } else if (config.imageLink&&mongoIdRegex.test(config.imageLink)){
+        RubedoPagesService.getPageById(config.imageLink).then(function(response){
+            if (response.data.success){
+                me.url=response.data.url;
+            }
+        });
+    }
+
+    $http.get("/api/v1/media/" + config.imageFile).then(
+        function(response) {
+            if(response.data.success) {
+                me.imageTitle = response.data.media.fields.title;
+                $scope.rubedo.current.page.image = $scope.rubedo.imageUrl.getUrlByMediaId(config.imageFile,{width:'1200px'});
+                 $scope.clearORPlaceholderHeight();
+            }
+        }
+    );
+
+}]);
