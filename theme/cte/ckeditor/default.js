bee6773ff729023d22d7f599d47629d1dd8aaaef
nicolasrhone <nicolas.rhone@gmail.com>
2016-05-26 21:18:25 +0200

    fix

--- theme/cte/ckeditor/default.js
+++ theme/cte/ckeditor/default.js
@@ -6,7 +6,7 @@ CKEDITOR.addTemplates("default",{imagesPath:"/theme/cte/ckeditor/img/",templates
     {title:"Présentation avec icône",image:"template1.gif",description:"Template avec une icône, un titre et une description",
     html:'<div class="col-xs-6  col-md-3 block-detail" ><p>Icône</p><p>Title</p><p>Description</p></div>'},
     {title:"Texte sur deux colonnes",image:"w50w50.png",description:"Texte sur deux colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
-    html:'<div class="col-xs-12  col-sm-6"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-6"><p>Colonne 2 </p></div>'},
+    html:'<div class="row"><div class="col-xs-12  col-sm-6"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-6"><p>Colonne 2 </p></div></div>'},
     {title:"Texte sur trois colonnes",image:"w33w33.png",description:"Texte sur trois colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
-    html:'<div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div>'}
+    html:'<div class="row"><div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div></div>'}
     ]});
