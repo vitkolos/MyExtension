1219a368d856c81e1e203468f03e17aad2f4c357
nicolasrhone <nicolas.rhone@gmail.com>
2016-05-28 15:54:30 +0200

    dump

--- src/MyExtension/Rest/V1/DonationResource.php
+++ src/MyExtension/Rest/V1/DonationResource.php
@@ -43,6 +43,7 @@ class DonationResource extends AbstractResource
         // create don
         $don=[];
         $donationInfo = json_decode($params["don"],true);
+        var_dump($donationInfo);
         $accountInfos = json_decode($params["account"],true);
         $don['fields'] =  $donationInfo;
         $don['fields']["condition"] = $accountInfos["text"];
@@ -92,11 +93,11 @@ class DonationResource extends AbstractResource
         if($don["fields"]["modePaiement"]=="carte") {
             $payboxAccountId ="";
             if($isProjetInternational) {
-                $this->envoyerMailsDon($don["fields"],$projectDetail,$paymentConfigInt["data"],locale_get_primary_language($params['lang']->getLocale()),true);
+                $this->envoyerMailsDon($don["fields"],$projectDetail,$paymentConfigInt["data"],$params['lang']->getLocale(),true);
                 $payboxAccountId = $paymentConfigInt["data"]["nativePMConfig"]["paybox"];
             }
             else {
-                $this->envoyerMailsDon($don["fields"],$projectDetail,$paymentConfigPays["data"],locale_get_primary_language($params['lang']->getLocale()),true);
+                $this->envoyerMailsDon($don["fields"],$projectDetail,$paymentConfigPays["data"],$params['lang']->getLocale(),true);
                 $payboxAccountId = $paymentConfigPays["data"]["nativePMConfig"]["paybox"];
             }
             /*GET PAYBOX PROPERTIES pas besoin ?
