238039ee7197b958a6040c7762305e13dda24294
nicolasrhone <nicolas.rhone@gmail.com>
2016-05-30 11:19:06 +0200

    langue des mails

--- src/MyExtension/Rest/V1/InscriptionResource.php
+++ src/MyExtension/Rest/V1/InscriptionResource.php
@@ -103,7 +103,7 @@ class InscriptionResource extends AbstractResource
         }
         
         
-       if($resultInscription['success']) {$this->sendInscriptionMail($inscriptionForm['fields'], "fr");}
+       if($resultInscription['success']) {$this->sendInscriptionMail($inscriptionForm['fields'], $params['lang']->getLocale());}
        
        
 
