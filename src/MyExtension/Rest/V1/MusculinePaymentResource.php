a43e05f89d3b9f76709859a9ec216e050e1ee04a
nicolasrhone <nicolas.rhone@gmail.com>
2016-05-30 18:31:17 +0200

    fix

--- src/MyExtension/Rest/V1/MusculinePaymentResource.php
+++ src/MyExtension/Rest/V1/MusculinePaymentResource.php
@@ -141,7 +141,7 @@ class MusculinepaymentResource extends AbstractResource {
     return array(
             'success' => true,
             'url' =>'https://www.paypal.com/cgi-bin/webscr?' . $query_string,
-            'message' => $resultcreate
+            'message' => $resultcreate['success']
         );
 
     }
