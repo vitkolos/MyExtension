ca585c9ca0ed45b8b76c0b898f4cdd52c2c94ade
nicolasrhone <nicolas.rhone@gmail.com>
2016-06-02 15:51:27 +0200

    add puces

--- theme/cte/ckeditor/default.js
+++ theme/cte/ckeditor/default.js
@@ -8,5 +8,7 @@ CKEDITOR.addTemplates("default",{imagesPath:"/theme/cte/ckeditor/img/",templates
     {title:"Texte sur deux colonnes",image:"w50w50.png",description:"Texte sur deux colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
     html:'<div class="row"><div class="col-xs-12  col-sm-6"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-6"><p>Colonne 2 </p></div></div>'},
     {title:"Texte sur trois colonnes",image:"w33w33.png",description:"Texte sur trois colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
-    html:'<div class="row"><div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div></div>'}
+    html:'<div class="row"><div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div></div>'},
+    {title:"Liste de liens",image:"puces.png",description:"Liste de liens avec des bordures en haut et en bas",
+    html:'<div class="regions-list"><ul><li>Lien 1</li><li>Lien 2</li><li>Lien 3</li></div>'}
     ]});
