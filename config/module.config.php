894c6904086c5ceb4d4a636ccd0de50d6ee7ef51
nicolasrhone <nicolas.rhone@gmail.com>
2016-06-06 10:11:58 +0200

    add content detail

--- config/module.config.php
+++ config/module.config.php
@@ -40,6 +40,10 @@ return array(
             'maxlifeTime' => 60,
             'definitionFile' => realpath(__DIR__ . "/blocks/") . '/contentList.json'
         ),
+       'contentDetail' => array(
+            'maxlifeTime' => 60,
+            'definitionFile' => realpath(__DIR__ . "/blocks/") . '/contentDetail.json'
+        ),
        'facebook' => array(
             'maxlifeTime' => 60,
             'definitionFile' => realpath(__DIR__ . "/blocks/") . '/facebook.json'
