a855242b1b66225247823db8b8c22588be862fa1
Nicolas Rhoné <nicolas.rhone@gmail.com>
2016-07-15 18:46:32 +0200

    traductions site CCN

--- theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
@@ -398,7 +398,7 @@ angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope'
                         $scope.processForm=false;
                         $scope.finInscription=true; 
                         $scope.inscription={};
-                        $scope.message += "Votre inscription a bien été prise en compte. Merci et à bientôt !";
+                        $scope.message += $scope.rubedo.translate("Block.Inscription.Succes","Votre inscription a bien été prise en compte. Merci et à bientôt !");
                     }
                     
                     
