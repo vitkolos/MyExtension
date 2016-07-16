2600c7be1f125e2b6075bfc351b2f885c7353d79
nicolasrhone <nicolasrhone@users.noreply.github.com>
2016-07-16 14:38:00 +0200

    add currency filter

--- theme/cte/js/cte.js
+++ theme/cte/js/cte.js
@@ -7,7 +7,13 @@ angular.module('rubedoBlocks').filter('cleanUrl', function () {
         return input.replace("//","/");
      };
   });
-
+angular.module('rubedoBlocks').filter('nfcurrency', [ '$filter', '$locale', function ($filter, $locale) {
+        var currency = $filter('currency'), formats = $locale.NUMBER_FORMATS;
+        return function (amount, symbol) {
+            var value = currency(amount, symbol);
+            return value.replace(new RegExp('\\' + formats.DECIMAL_SEP + '\\d{2}'), '')
+        }
+}])
 angular.module('rubedo').filter('ligneNonVide', function () {
            return function (input) {
                       var filtered = [];
