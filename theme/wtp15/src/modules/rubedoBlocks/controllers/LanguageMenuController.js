35e7d255910a64a01515545c056fa5036d193651
Nicolas Rhoné <nicolas.rhone@gmail.com>
2016-08-11 14:28:50 +0200

    detail page WTP and fixes

--- theme/wtp15/src/modules/rubedoBlocks/controllers/LanguageMenuController.js
+++ theme/wtp15/src/modules/rubedoBlocks/controllers/LanguageMenuController.js
@@ -2,58 +2,69 @@ angular.module("rubedoBlocks").lazy.controller("LanguageMenuController", ['$scop
     function ($scope, RubedoPagesService,RubedoModuleConfigService, RubedoContentsService, $route, $location) {
         var me = this;
         var config = $scope.blockConfig;
+        var themePath="/theme/"+window.rubedoConfig.siteTheme;
         var urlArray = [];
         var contentId = "";
         me.languages = $scope.rubedo.current.site.languages;
         me.currentLang = $scope.rubedo.current.site.languages[$route.current.params.lang];
-        me.mode = config.displayAs == "select";
-        me.showFlags = config.showFlags;
-        me.isDisabled =  function(lang){
-            return me.currentLang.lang == lang;
-        };
         if(!config.showCurrentLanguage){
             delete me.languages[$route.current.params.lang];
         }
         me.getFlagUrl = function(flagCode){
-            return '/assets/flags/16/'+flagCode+'.png';
+            return themePath+'/img/flags/'+flagCode+'.png';
         };
  
         me.changeLang = function (lang) {
             if(lang != me.currentLang.lang){
                 RubedoModuleConfigService.changeLang(lang);
-                RubedoPagesService.getPageById($scope.rubedo.current.page.id).then(function(response){
-                    if (response.data.success){
-                        $location.path(response.data.url);
-                    }
-                });
-            }
-        };
-    }]);       
-     /*  
-        me.changeLang = function (lang) {
-            if(lang != me.currentLang.lang){
-                RubedoModuleConfigService.changeLang(lang);
-                RubedoPagesService.getPageById($scope.rubedo.current.page.id).then(function(response){
+                if ($scope.rubedo.current.site.locStrategy == 'fallback'){
+                    RubedoModuleConfigService.addFallbackLang($scope.rubedo.current.site.defaultLanguage);
+                }
+                RubedoPagesService.getPageById($scope.rubedo.current.page.id,true).then(function(response){
                     if (response.data.success){
                         if($scope.rubedo.current.page.contentCanonicalUrl) {
                             // Get content id
-                            urlArray = $scope.rubedo.current.page.contentCanonicalUrl.split("/");
+                            urlArray = $route.current.params.routeline.split("/");
                             contentId = urlArray[urlArray.length-2];
 
                             // Redirect without title
-                            $location.url(response.data.url + "/" + contentId + "/");
+                            //window.location.href = response.data.url + "/" + contentId + "/title";
 
                             //Redirect with title
-                            //RubedoContentsService.getContentById(contentId).then(function(contentResponse){
-                            //    if (contentResponse.data.success){
-                            //        $location.url(response.data.url + "/" + contentId + "/" + angular.lowercase(contentResponse.data.content.text.replace(/ /g, "-")));
-                            //    }
-                            //});
-                        } else {
-                            $location.url(response.data.url);
+                            RubedoContentsService.getContentById(contentId).then(function(contentResponse){
+                                if (contentResponse.data.success){
+                                    console.log(contentResponse.data.content);
+                                    var contentSegment=contentResponse.data.content.text;
+                                    if (contentResponse.data.content.fields.urlSegment&&contentResponse.data.content.fields.urlSegment!=""){
+                                        contentSegment=contentResponse.data.content.fields.urlSegment;
+                                    }
+                                    window.location.href =response.data.url + "/" + contentId + "/" + angular.lowercase(contentSegment.replace(/ /g, "-"));
+                                } else {
+                                    window.location.href =  response.data.url;
+                                }
+                            },
+                            function(){
+                                window.location.href =  response.data.url;
+                            });
+                        }                        
+                        else{
+                            var currentParams = angular.element.param($location.search());
+                            var url = response.data.url;
+
+                            if(currentParams != "") {
+                                if(response.data.url.indexOf("?") > -1) {
+                                    url = response.data.url + currentParams;
+                                } else {
+                                    url = response.data.url + "?" + currentParams;
+                                }
+                            }
+
+                            window.location.href =  url;
                         }
                     }
                 });
             }
         };
-    }]);*/
+        $scope.clearORPlaceholderHeight();
+    }]);       
+
