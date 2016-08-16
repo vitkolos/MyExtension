b0a69afc305affae210c0f79a7f6add77ac68062
Nicolas Rhoné <nicolas.rhone@gmail.com>
2016-08-16 10:04:55 +0200

    add urlc

--- src/MyExtension/Rest/V1/DotPayUrlcResource.php
+++ src/MyExtension/Rest/V1/DotPayUrlcResource.php
@@ -22,7 +22,7 @@ class DotPayUrlcResource extends AbstractResource {
             ->definition
             ->setName('DotPayUrlc')
             ->setDescription('Retour de DotPay')
-            ->editVerb('get', function (VerbDefinitionEntity &$verbDefinitionEntity) {
+            ->editVerb('post', function (VerbDefinitionEntity &$verbDefinitionEntity) {
                 $verbDefinitionEntity
                     ->setDescription('Get info de paiement Dotpay')
                     ->addInputFilter(
@@ -80,7 +80,7 @@ class DotPayUrlcResource extends AbstractResource {
                     );
             });
     }
-    function getAction($params) {
+    function postAction($params) {
         //vérifier si le payement est réussi
         $erreur = true;
         if($params["operation_status"]=='completed') $erreur=false;
@@ -260,9 +260,10 @@ if(!($erreurStatus) && $securite && $autorisation) {
 
     protected function getMailCompta(){
         switch($_SERVER['HTTP_HOST']) {
-            case "ccn.chemin-neuf.fr" : 
             case "chemin-neuf.fr" : 
                 return "ccn.comptabilite@gmail.com"; break;
+            case "www.chemin-neuf.pl" : 
+                return "nicolas.rhone@gmail.com"; break;
         }
     }
      
@@ -270,80 +271,6 @@ if(!($erreurStatus) && $securite && $autorisation) {
     
     
 
-
-    public function getErrorMessage($error) {
-        $message="";
-        switch($error) {
-            case "00001" : $message = "La connexion au centre d’autorisation a échoué ou une erreur interne est survenue. Dans ce cas, il est souhaitable de faire une tentative sur le site secondaire : tpeweb1.paybox.com."; break;
-            case "00003" : $message = "Erreur Paybox. Dans ce cas, il est souhaitable de faire une tentative sur le site secondaire FQDN tpeweb1.paybox.com."; break;
-            case "00004" : $message = "Numéro de porteur ou cryptogramme visuel invalide."; break;
-            case "00006" : $message = "Accès refusé ou site/rang/identifiant incorrect."; break;
-            case "00008" : $message = "Date de fin de validité incorrecte."; break;
-            case "00009" : $message = "Erreur de création d’un abonnement."; break;
-            case "00010" : $message = "Devise inconnue."; break;
-            case "00011" : $message = "Montant incorrect."; break;
-            case "00015" : $message = "Paiement déjà effectué."; break;
-            case "00016" : $message = "Abonné déjà existant (inscription nouvel abonné). Valeur ‘U’ de la variable PBX_RETOUR."; break;
-            case "00021" : $message = "Carte non autorisée."; break;
-            case "00029" : $message = "Carte non conforme. Code erreur renvoyé lors de la documentation de la variable « PBX_EMPREINTE »."; break;
-            case "00030" : $message = "Temps d’attente > 15 mn par l’internaute/acheteur au niveau de la page de paiements."; break;
-            case "00031" : $message = "Réservé."; break;
-            case "00032" : $message = "Réservé."; break;
-            case "00033" : $message = "Code pays de l’adresse IP du navigateur de l’acheteur non autorisé."; break;
-            case "00040" : $message = "Opération sans authentification 3-DSecure, bloquée par le filtre."; break;
-            case "99999" : $message = "Opération en attente de validation par l’émetteur du moyen de paiement."; break;
-            case "00032" : $message = "Réservé."; break;
-            case "00032" : $message = "Réservé."; break;
-            case "00032" : $message = "Réservé."; break;
-            case "00100": $message = "Transaction approuvée ou traitée avec succès"; break;
-            case "00101": $message =  "Contacter l’émetteur de carte"; break;
-            case "00102": $message = "Contacter l’émetteur de carte"; break;
-            case "00103": $message = "Commerçant invalide"; break;
-            case "00104": $message = "Conserver la carte"; break;
-             case "00105": $message = "Ne pas honorer"; break;
-            case "00107": $message = "Conserver la carte, conditions spéciales"; break;
-            case "00108": $message = "Approuver après identelification du porteur"; break;
-            case "00112": $message = "Transaction invalide"; break;
-            case "00113": $message = "Montant invalide"; break;
-            case "00114": $message = "Numéro de porteur invalide"; break;
-            case "00115": $message = "Emetteur de carte inconnu"; break;
-            case "00117": $message = "Annulation client"; break;
-            case "00119": $message = "Répéter la transaction ultérieurement"; break;
-            case "00120": $message = "Réponse erronée (erreur dans le domaine serveur)"; break;
-            case "00124": $message = "Mise à jour de fichier non supportée"; break;
-            case "00125": $message = "Impossible de localiser l’enregistrement dans le fichier"; break;
-            case "00126": $message = "Enregistrement dupliqué, ancien enregistrement remplacé"; break;
-            case "00127": $message = "Erreur en « edit » sur champ de mise à jour fichier"; break;
-            case "00128": $message = "Accès interdit au fichier"; break;
-            case "00129": $message = "Mise à jour de fichier impossible"; break;
-            case "00130": $message = "Erreur de format"; break;
-            case "00133": $message = "Carte expirée"; break;
-            case "00138": $message =  "Nombre d’essais code confidentiel dépassé"; break;
-            case "00141": $message =  "Carte perdue"; break;
-            case "00143": $message =  "Carte volée"; break;
-            case "00151": $message =  "Provision insuffisante ou crédit dépassé"; break;
-            case "00154": $message =  "Date de validité de la carte dépassée"; break;
-            case "00155": $message =  "Code confidentiel erroné"; break;
-            case "00156": $message =  "Carte absente du fichier"; break;
-            case "00157": $message =  "Transaction non permise à ce porteur"; break;
-            case "00158": $message =  "Transaction interdite au terminal"; break;
-            case "00159": $message =  "Suspicion de fraude"; break;
-            case "00160": $message =  "L’accepteur de carte doit contacter l’acquéreur"; break;
-            case "00161": $message =  "Dépasse la limite du montant de retrait"; break;
-            case "00163": $message =  "Règles de sécurité non respectées"; break;
-            case "00168": $message =  "Réponse non parvenue ou reçue trop tard"; break;
-            case "00175": $message =  "Nombre d’essais code confidentiel dépassé"; break;
-            case "00176": $message =  "Porteur déjà en opposition, ancien enregistrement conservé"; break;
-            case "00189": $message =  "Echec de l’authentelification"; break;
-            case "00190": $message =  "Arrêt momentané du système"; break;
-            case "00191": $message =  "Emetteur de cartes inaccessible"; break;
-            case "00194": $message =  "Demande dupliquée"; break;
-            case "00196": $message =  "Mauvais fonctionnement du système"; break;
-            case "00197": $message =  "Echéance de la temporisation de surveillance globale"; break;
-            default : $message = "Erreur non documentée";
-        }
-        return $message;
-    }
     
     
     public function getContentIdByName($name){
