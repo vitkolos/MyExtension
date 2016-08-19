92663e04d512f75dc63e444c4d03c760ffe7dffd
nicolasrhone <nicolasrhone@users.noreply.github.com>
2016-08-19 15:48:29 +0200

    Update simpleContact.js

--- theme/cte/src/modules/rubedoBlocks/controllers/simpleContact.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/simpleContact.js
@@ -18,6 +18,7 @@ angular.module("rubedoBlocks").lazy.controller('ContactBlockController',['$scope
         delete (contactSnap.to);
         payload.fields=contactSnap;
         angular.element('#myModal'+$scope.block.id+$scope.blockConfig.id).modal('hide');
+        angular.element('#myModalsingle').modal('hide');
         payload.fields["website"] = $location.absUrl();
         RubedoMailService.sendMail(payload).then(
             function(response){
