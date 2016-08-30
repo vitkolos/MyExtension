0c9f8fd8031add64f1b0aa96b5da36a66b65e7ee
nicolasrhone <nicolas.rhone@gmail.com>
2016-08-30 11:58:44 +0200

    suite

--- src/MyExtension/Rest/V1/SearchResource.php
+++ src/MyExtension/Rest/V1/SearchResource.php
@@ -45,7 +45,7 @@ class SearchResource extends AbstractResource
         parent::__construct();
         $this->searchOption = 'all';
         $this->searchParamsArray = array('orderby', 'orderbyDirection', 'query', 'objectType', 'type', 'damType', 'userType', 'author',
-             'userName', 'lastupdatetime', 'start', 'limit', 'searchMode','price','inStock',"isMagic","fingerprint","historyDepth","historySize","useDetailContent","detailContentId");
+             'userName', 'lastupdatetime', 'start', 'limit', 'searchMode','price','inStock','isMagic','fingerprint','historyDepth','historySize','useDetailContent','detailContentId');
         $this
             ->definition
             ->setName('Search')
