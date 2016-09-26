0002aa3683f351d29888f71d51bbf14098c414a6
nicolasrhone <nicolas.rhone@gmail.com>
2016-09-26 17:49:33 +0200

    suite

--- theme/cte/src/modules/rubedoBlocks/controllers/ContentContributionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/ContentContributionController.js
@@ -227,7 +227,7 @@ angular.module("rubedoBlocks").lazy.controller("AlbumUploadController",["$scope"
                 else {
                     imgTitle=me.batchTitle + '_'+i;
                 }
-                console.log(files[i]);
+                console.log(files[i].size);
                 
                 
                 
