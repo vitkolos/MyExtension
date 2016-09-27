64a0de8aa4b1a83648c4cb5a1c38d7f52412bed9
nicolasrhone <nicolas.rhone@gmail.com>
2016-09-27 16:58:22 +0200

    fix albums

--- theme/cte/src/modules/rubedoBlocks/controllers/ContentContributionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/ContentContributionController.js
@@ -218,14 +218,16 @@ angular.module("rubedoBlocks").lazy.controller("AlbumUploadController",["$scope"
                 else {
                     imgTitle=me.batchTitle + '_'+i;
                 }
+                /*
                 Upload.base64DataUrl(files[i]).then(
                     function(response){
-                        console.log(response);
+                        //console.log(base64img);
+                        embeddedImages
                     },
                     function(response){
                     }
-                );
-/*
+                );*/
+
                 Upload.upload({
                     url: '/api/v1/media',
                     method: 'POST',
@@ -244,6 +246,8 @@ angular.module("rubedoBlocks").lazy.controller("AlbumUploadController",["$scope"
                         me.progress=0;
                     }
                     counter++;
+                    var id=response.data.media.id;
+                    ($scope.ccCtrl.imagesForAlbum).push(id);
                 }, function (resp) {
                     console.log('Error status: ' + resp.status);
                     counter++;
@@ -251,7 +255,7 @@ angular.module("rubedoBlocks").lazy.controller("AlbumUploadController",["$scope"
                         me.processing=false;
                         me.progress=0;
                     }
-                });      */              
+                });                  
                 
             }
         }
