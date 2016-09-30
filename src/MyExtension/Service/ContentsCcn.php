b97bb9acf9c069dc27f8739063b91d3d7e80820c
nicolasrhone <nicolas.rhone@gmail.com>
2016-09-30 10:44:36 +0200

    delay

--- src/MyExtension/Service/ContentsCcn.php
+++ src/MyExtension/Service/ContentsCcn.php
@@ -249,6 +249,7 @@ class ContentsCcn extends WorkflowAbstractCollection implements IContents
         if ($this->_isValidInput) {
             $returnArray = parent::create($obj, $options, $live, $ignoreIndex);
            if($returnArray['success']) {
+                usleep(50000);
                 $content = $this->findById($returnArray['data']['id'], true, false);
                 $contentType = Manager::getService('ContentTypes')->findById($content['typeId']);
                 if (!$contentType || (isset($contentType['system']) && $contentType['system'] == true)) {
