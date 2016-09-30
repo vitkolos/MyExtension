0147b949a50671e06fb0bedcdc0f6a8070878fc5
nicolasrhone <nicolas.rhone@gmail.com>
2016-09-30 11:31:38 +0200

    inscriptino

--- theme/cte/src/modules/rubedoBlocks/controllers/ContentDetailController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -223,7 +223,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
                             };
                             RubedoSearchService.searchByQuery(optionsInscriptionsList).then(function(response){
                               if(response.data.success){
-                                //console.log(response.data.results);
+                                console.log(response.data.results);
                               } 
                             });
                         }
