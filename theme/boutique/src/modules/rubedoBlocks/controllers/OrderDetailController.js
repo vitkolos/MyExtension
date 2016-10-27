2fe2f38bf1c7ca6700d717408089dbe418b95ccc
nicolasrhone <nicolas.rhone@gmail.com>
2016-10-27 17:13:30 +0200

    add fonts

--- theme/boutique/src/modules/rubedoBlocks/controllers/OrderDetailController.js
+++ theme/boutique/src/modules/rubedoBlocks/controllers/OrderDetailController.js
@@ -55,16 +55,26 @@ angular.module("rubedoBlocks").lazy.controller('OrderDetailController',['$scope'
                 if (response.data.success){
                    me.billTitle = "FA2" + ('00000'+(response.data.total+1)).substring((response.data.total).length);
                     me.creatingBill = true;                        
-                    
+                    kendo.pdf.defineFont({
+                        "Merriweather"             : "theme/cte/fonts/merriweather.ttf" // this is a URL
+                    })
                     $timeout(function(){
                         
                         kendo.drawing.drawDOM(angular.element("#orderForm"))
                             .then(function(group) {
                                 // Chaining the promise via then
+                                group.options.set("pdf", {
+                                    paperSize: "A4",
+                                    margin: {
+                                        left   : "20mm",
+                                        top    : "40mm",
+                                        right  : "20mm",
+                                        bottom : "40mm"
+                                    }
+                                });
+
                                 kendo.drawing.pdf.toBlob(group, function(blob){
                                     var file = new File([blob], me.billTitle+".pdf", {type: "application/pdf", lastModified: Date.now()});
-
-
                                     // you can now upload it to a server
                                     // this form simulates an <input type="file" name="pdfFile" />
                                     var uploadOptions = {
@@ -76,10 +86,20 @@ angular.module("rubedoBlocks").lazy.controller('OrderDetailController',['$scope'
                                     form.append("file", file);
 
                                     $http.post("/api/v1/media", form, {
-                                      transformRequest: angular.identity,
-                                      params:uploadOptions,
-                                      headers: {'Content-Type': undefined}
-                                  });
+                                        transformRequest: angular.identity,
+                                        params:uploadOptions,
+                                        headers: {'Content-Type': undefined}
+                                    }).then(function(response){
+                                        me.creatingBill = false;
+                                        me.order.billDocument = response.data.media.id;
+                                        RubedoMediaService.getMediaById(me.order.billDocument,{}).then(
+                                            function(mediaResponse){
+                                                if (mediaResponse.data.success){
+                                                    me.billDocumentUrl=mediaResponse.data.media.url;
+                                                }
+                                            }
+                                        );
+                                    });
                                 });
                             })
                             
