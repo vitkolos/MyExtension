062dddd8aa25f704f67850eeb415ff6ca65c06d5
nicolasrhone <nicolasrhone@users.noreply.github.com>
2016-10-31 18:49:11 +0100

    Update ShippersResource.php

--- src/MyExtension/Rest/V1/Ecommerce/ShippersResource.php
+++ src/MyExtension/Rest/V1/Ecommerce/ShippersResource.php
@@ -85,6 +85,8 @@ class ShippersResource extends AbstractResource
 
         foreach ($myCart as $value) {
             $items = $items + $value['amount'];
+            var_dump($value['productId']);
+            var_dump($value['variationId']);
         }
 
         $myShippers = Manager::getService("ShippersCcn")->getApplicableShippers($user['shippingAddress']['country'], $myCart);
