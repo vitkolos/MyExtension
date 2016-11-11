eb3109d148b676d583bde19bdfe14a7852cdccad
nicolasrhone <nicolas.rhone@gmail.com>
2016-11-11 19:10:23 +0100

    suite

--- theme/cte/src/modules/rubedoBlocks/controllers/LogoMissionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/LogoMissionController.js
@@ -1,4 +1,4 @@
-angular.module("rubedoBlocks").lazy.controller('LogoMissionController',['$scope','RubedoMediaService',function($scope,RubedoMediaService){
+angular.module("rubedoBlocks").lazy.controller('LogoMissionController',['$scope','RubedoMediaService','$location',function($scope,RubedoMediaService,$location){
     var me = this;
     var config = $scope.blockConfig;
     me.query = config.query;
@@ -23,12 +23,40 @@ angular.module("rubedoBlocks").lazy.controller('LogoMissionController',['$scope'
             //imageThumbnailWidth: config.imageThumbnailWidth?config.imageThumbnailWidth:100
         };
         me.width = options.imageThumbnailWidth;
-        me.getMedia = function(options){
+        me.getMedia = function(options, reload){
             RubedoMediaService.getMediaByQuery(options).then(function(response){
                 if(response.data.success){
                     me.count = response.data.count;
                     me.images = response.data.media.data;
                     $scope.clearORPlaceholderHeight();
+                    if (me.count=0 && $scope.rubedo.current.breadcrumb.length>2 && !reload) {
+                        //remonter au niveau supérieur
+                        $http.get("/api/v1/pages",{
+                            params:{
+                                site:$location.host(),
+                                route:$scope.rubedo.current.breadcrumb[$scope.rubedo.current.breadcrumb-1]['url']
+                            }
+                        }).then(function(response){
+                            if (response.data.success) {
+                                query["vocabularies"]["navigation"] = {
+                                    rule:'someRec',
+                                    terms:[$scope.rubedo.current.page.id,$scope.rubedo.current.page.parentId,response.data.page.id]
+                                };
+                                me.query = JSON.stringify(query);
+                                options = {
+                                    query: me.query,
+                                    start: me.start,
+                                    limit: me.limit,
+                                    pageWorkspace: $scope.rubedo.current.page.workspace,
+                                    imageThumbnailHeight: config.imageThumbnailHeight?config.imageThumbnailHeight:220,
+                                    //imageThumbnailWidth: config.imageThumbnailWidth?config.imageThumbnailWidth:100
+                                };
+                                me.getMedia(options, true);
+                            }
+                            
+                        });
+                        
+                    }
                 }
             });
         };
