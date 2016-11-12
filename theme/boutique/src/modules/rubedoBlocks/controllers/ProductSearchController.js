067a9660208a05d0bed37ac9aedef031f7bea3e6
nicolasrhone <nicolas.rhone@gmail.com>
2016-11-12 10:20:04 +0100

    fix limit search boutique

--- theme/boutique/src/modules/rubedoBlocks/controllers/ProductSearchController.js
+++ theme/boutique/src/modules/rubedoBlocks/controllers/ProductSearchController.js
@@ -39,7 +39,7 @@ angular.module("rubedoBlocks").lazy.controller("ProductSearchController",["$scop
         me.facets = [];
         me.activeFacets = [];
         me.start = 0;
-        me.limit = $routeParams.limit?$routeParams.limit:10;
+        me.limit = $routeParams.limit?$routeParams.limit:12;
         me.orderBy = $routeParams.orderby?$routeParams.orderby:"_score";
         me.orderByDirection=$routeParams.orderbyDirection?$routeParams.orderbyDirection:"asc";
         var resolveOrderBy = {
