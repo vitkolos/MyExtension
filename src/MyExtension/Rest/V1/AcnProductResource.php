69e1811d8de09832aa501c2cd2d9a2af2055b0bc
nicolasrhone <nicolas.rhone@gmail.com>
2016-11-15 14:32:18 +0100

    suite

--- src/MyExtension/Rest/V1/AcnProductResource.php
+++ src/MyExtension/Rest/V1/AcnProductResource.php
@@ -61,7 +61,7 @@ class AcnproductResource extends AbstractResource
 						->addFilter(Filter::factory('Value')->setName('productProperties.sku')->setValue($codeBarre));
 */
 	$findFilter = Filter::Factory()->addFilter(Filter::factory('Value')->setName('isProduct')->setValue(true))
-						->addFilter(Filter::factory('Value')->setName('fields.reference')->setValue($codeBarre));
+						->addFilter(Filter::factory('Value')->setName('reference')->setValue($codeBarre));
         
         $content = $contentsService->findOne($findFilter,true,false);
 
