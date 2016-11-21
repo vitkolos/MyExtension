a847e2d401a7b8aeccee4f4d963049dc007ef3da
nicolasrhone <nicolas.rhone@gmail.com>
2016-11-21 16:33:59 +0100

    exception

--- src/MyExtension/Authentication/CoreAdapter.php
+++ src/MyExtension/Authentication/CoreAdapter.php
@@ -87,7 +87,7 @@ class CoreAdapter extends AbstractAdapter
         if ($valid) {
             $this->_authenticateResultInfo['code'] = Result::SUCCESS;
             $this->_authenticateResultInfo['messages'][] = 'Authentication successful.';
-			var_dump("Authentication");
+			throw new APIAuthException('Authentication successful', 500);
             $this->_authenticateResultInfo['identity'] = $user;
             return $this->_authenticateCreateAuthResult();
         } else {
