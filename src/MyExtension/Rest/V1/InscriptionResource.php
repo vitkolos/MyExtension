3546dd4664948c0d7357d0ecfb8a3128eb089284
nicolasrhone <nicolas.rhone@gmail.com>
2016-12-08 10:15:46 +0100

    add pt inscription number

--- src/MyExtension/Rest/V1/InscriptionResource.php
+++ src/MyExtension/Rest/V1/InscriptionResource.php
@@ -70,6 +70,9 @@ class InscriptionResource extends AbstractResource
             ),
             "it" =>array(
                 "fields" => array("text"=>$content["fields"]["text"])
+            ),
+            "pt" =>array(
+                "fields" => array("text"=>$content["fields"]["text"])
             )
 		);
 	    $content['i18n'][$params['lang']->getLocale()]['fields']['text'] = $content["fields"]["text"];
