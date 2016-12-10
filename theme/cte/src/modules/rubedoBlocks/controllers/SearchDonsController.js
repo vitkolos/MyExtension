39da7e98b1042c13ed5c9cff9062ab2615b9dcb1
nicolasrhone <nicolas.rhone@gmail.com>
2016-12-10 13:43:22 +0100

    suite

--- theme/cte/src/modules/rubedoBlocks/controllers/SearchDonsController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/SearchDonsController.js
@@ -136,7 +136,7 @@ angular.module("rubedoBlocks").lazy.controller("SearchDonsController",['$scope',
                     }
                     angular.forEach(terms, function(term){
                         if (!me.usedTaxonomies[vocId]["terms"][term]) {
-                            me.usedTaxonomies[vocId]["terms"][term] = $filter('filter')(me.taxo[vocId].terms,{"id":term})[0].text;
+                            me.usedTaxonomies[vocId]["terms"].push({"id":term, "name":$filter('filter')(me.taxo[vocId].terms,{"id":term})[0].text});
                         }
                     });
                 }
