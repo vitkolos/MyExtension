3b0f6dcea87b6a1fa957fe1d678385f91dd20a05
nicolasrhone <nicolas.rhone@gmail.com>
2017-01-14 10:24:45 +0100

    sutie

--- theme/wtp17/src/modules/rubedoBlocks/controllers/MenuController.js
+++ theme/wtp17/src/modules/rubedoBlocks/controllers/MenuController.js
@@ -1,4 +1,5 @@
-    angular.module("rubedoBlocks").lazy.controller("MenuController",['$scope','$rootScope','$location','$route','RubedoMenuService','RubedoPagesService',function($scope,$rootScope,$location,$route,RubedoMenuService,RubedoPagesService){
+    angular.module("rubedoBlocks").lazy.controller("MenuController",['$scope','$rootScope','$location','$route','RubedoMenuService','RubedoPagesService','$http',
+																																																																					function($scope,$rootScope,$location,$route,RubedoMenuService,RubedoPagesService,$http){
         var me=this;
         me.menu={};
         me.pagesBlocks={};
@@ -25,33 +26,39 @@
         RubedoMenuService.getMenu(pageId, config.menuLevel).then(function(response){
             if (response.data.success){
                 me.menu=response.data.menu;
-		angular.forEach(me.menu.pages, function(page, key) {
-		    me.pagesBlocks[key]={};
-		    me.pagesBlocks[key]["title"] = page.text;
-		    me.pagesBlocks[key]["url"] = page.url;
-		    me.pagesBlocks[key].blocks=[]; 
-		    var lang = $route.current.params.lang;
-		    angular.forEach(page.blocks, function(block, key2){
-			if (block.bType=="contentDetail" && block.orderValue<=1) {
-			    if(block.i18n[lang]) me.pagesBlocks[key].blocks.push({"title":block.i18n[lang].title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
-			    //if(block.i18n[lang]) me.pagesBlocks[key].blocks.push({"title":block.i18n[lang].title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
-			    //else me.pagesBlocks[key].blocks.push({"title":block.i18n.fr.title});
-			}
-			else {}
-		    })
-     
-     
-		});
-		    $scope.clearORPlaceholderHeight();
+																angular.forEach(me.menu.pages, function(page, key) {
+																				me.pagesBlocks[key]={};
+																				me.pagesBlocks[key]["title"] = page.text;
+																				me.pagesBlocks[key]["url"] = page.url;
+																				me.pagesBlocks[key].blocks=[]; 
+																				var lang = $route.current.params.lang;
+																				angular.forEach(page.blocks, function(block, key2){
+																								if (block.bType=="contentDetail" && block.orderValue<=1) {
+																												if(block.i18n[lang]) me.pagesBlocks[key].blocks.push({"title":block.i18n[lang].title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
+																												//if(block.i18n[lang]) me.pagesBlocks[key].blocks.push({"title":block.i18n[lang].title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
+																												//else me.pagesBlocks[key].blocks.push({"title":block.i18n.fr.title});
+																								}
+																								else {}
+																				});
+																});
+																$scope.clearORPlaceholderHeight();
 
-            } else {
+            }
+												else {
                 me.menu={};
-		    $scope.clearORPlaceholderHeight();
-
+																$scope.clearORPlaceholderHeight();
             }
         });
 	
-	$rootScope.toggleNav = "false";
-	$rootScope.ToggleNav = function(){$rootScope.toggleNav = !$rootScope.toggleNav};   
+								$rootScope.toggleNav = "false";
+								$rootScope.ToggleNav = function(){$rootScope.toggleNav = !$rootScope.toggleNav};
+								/*Ajouter les traductions*/
+								$scope.rubedo.getCustomTranslations = function(){
+												$http.get('/theme/'+window.rubedoConfig.siteTheme+'/localization/'+lang+'/Texts.json').then(function(res){
+																$scope.rubedo.translations = JSON.parse((JSON.stringify($scope.rubedo.translations) + JSON.stringify(res.data)).replace(/}{/g,","))
+												});	
+        }
+								$scope.rubedo.getCustomTranslations();
+
     }]);
 
