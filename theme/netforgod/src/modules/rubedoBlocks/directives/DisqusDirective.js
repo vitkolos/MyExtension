60263ca64434bf3da2132faf881136b3b9f001c7
nicolasrhone <nicolas.rhone@gmail.com>
2017-01-14 12:37:26 +0100

    suite

--- theme/netforgod/src/modules/rubedoBlocks/directives/DisqusDirective.js
+++ theme/netforgod/src/modules/rubedoBlocks/directives/DisqusDirective.js
@@ -35,8 +35,8 @@ angular.module("rubedoBlocks").lazy.directive('dirDisqus', ['$window', function(
                     $window.disqus_url = scope.disqus_url;
                     $window.disqus_category_id = scope.disqus_category_id;
                     $window.disqus_disable_mobile = scope.disqus_disable_mobile;
-                    $window.disqus_config_language = '"'+scope.disqus_lang+'"';
-																				console.log($window.disqus_config_language );
+                    $window.disqus_config_language = scope.disqus_lang;
+																				console.log(scope.disqus_lang);
                     // get the remote Disqus script and insert it into the DOM, but only if it not already loaded (as that will cause warnings)
                     if (!$window.DISQUS) {
                         var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
