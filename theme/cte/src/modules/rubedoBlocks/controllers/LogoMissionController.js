f684d8759728e53738271d188223c1cb1df5e5df
nicolasrhone <nicolas.rhone@gmail.com>
2017-01-18 15:04:04 +0100

    suite

--- theme/cte/src/modules/rubedoBlocks/controllers/LogoMissionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/LogoMissionController.js
@@ -29,14 +29,16 @@ angular.module("rubedoBlocks").lazy.controller('LogoMissionController',['$scope'
                     if(me.count>0 && me.images[0].taxonomy.navigation) {
                         for (var i = 0, len = me.images[0].taxonomy.navigation.length; i < len; i++) {
                             RubedoPagesService.getPageById(me.images[0].taxonomy.navigation[i]).then(function(response){
-                                console.log(response);
-                                /*if (response.pageData) {
-                                    //code
-                                }*/
+                                if (response.data.success ) {
+                                    if (response.data.pageData.site && response.data.pageData.site==$scope.rubedo.current.site.id) {
+                                        me.images[0].pageId = me.images[0].taxonomy.navigation[i];
+                                        
+                                    }
+                                }
                             });
                         }
                     }
-                    
+                    console.log(me.images[0].pageId);
 
                     
                     
