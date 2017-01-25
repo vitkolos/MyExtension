c7d76f3fdfaa45adfe6776a0dae211e6bd5f89d3
nicolasrhone <nicolas.rhone@gmail.com>
2017-01-25 09:57:12 +0100

    ckeditor worship team

--- theme/worshipteam/ckeditor/default.js
+++ theme/worshipteam/ckeditor/default.js
@@ -5,6 +5,10 @@
 CKEDITOR.addTemplates("default",{imagesPath:"/theme/worshipteam/ckeditor/img/",templates:[
     {title:"Présentation avec icône",image:"template1.gif",description:"Template avec une icône, un titre et une description",
 							html:'<div class="col-xs-6  col-md-3 block-detail" ><p>I</p><p>Title</p><p>Description</p></div>'},
-    {title:"Présentation avec icône",image:"template2.gif",description:"Template avec une icône, un titre et une desciption",
-							html:'<div class="row"><div class="col-xs-6  col-md-4 block-detail" ><p>I</p><p>Title</p><p>Description</p></div><div class="col-xs-6  col-md-4 block-detail" ><p>I</p><p>Title</p><p>Description</p></div><div class="col-xs-6  col-md-4 block-detail" ><p>I</p><p>Title</p><p>Description</p></div></div>'}
+    {title:"Présentation avec icône",image:"template2.jpg",description:"3 colonnes avec icône, titre et texte",
+							html:'<div class="row"><div class="col-xs-6  col-md-4 block-detail" ><p>I</p><p>Title</p><p>Description</p></div><div class="col-xs-6  col-md-4 block-detail" ><p>I</p><p>Title</p><p>Description</p></div><div class="col-xs-6  col-md-4 block-detail" ><p>I</p><p>Title</p><p>Description</p></div></div>'},
+ {title:"Texte sur 2 colonnes",image:"w50w50.png",description:"Texte sur 2 colonnes",
+							html:'<div class="row"><div class="col-xs-12  col-sm-6" ><p>Colonne 1</p></div><div class="col-xs-12  col-sm-6" ><p>Colonne 2</p></div></div>'},
+{title:"Texte sur 3 colonnes",image:"w33w33.png",description:"Texte sur 3 colonnes",
+							html:'<div class="row"><div class="col-xs-12  col-sm-4" ><p>Colonne 1</p></div><div class="col-xs-12  col-sm-4" ><p>Colonne 2</p></div><div class="col-xs-12  col-sm-4" ><p>Colonne 3</p></div></div>'}
 ]});
