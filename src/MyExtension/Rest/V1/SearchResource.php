962871745739a042c68fed5d4503ffccaaee8ca8
nicolasrhone <nicolasrhone@users.noreply.github.com>
2017-01-30 16:26:16 +0100

    add filter special offers only

--- src/MyExtension/Rest/V1/SearchResource.php
+++ src/MyExtension/Rest/V1/SearchResource.php
@@ -17,6 +17,7 @@
 
          /**
          *pour CCN : modifié pour ajouter un tableau avec les labels des taxonomies
+         et ajouter option pour filtrer les promotions sur les produits
         **/
 namespace RubedoAPI\Rest\V1;
 use RubedoAPI\Entities\API\Definition\FilterDefinitionEntity;
@@ -209,6 +210,11 @@ class SearchResource extends AbstractResource
                         (new FilterDefinitionEntity())
                             ->setKey('searchMode')
                             ->setDescription('Search mode : default, count or aggregate')
+                    )
+                    ->addInputFilter(
+                        (new FilterDefinitionEntity())
+                            ->setKey('specialOffersOnly')
+                            ->setDescription('Vrai pour récupérer seulement les promotions')
                     )
                     ->addOutputFilter(
                         (new FilterDefinitionEntity())
