25719372f6cc4a3fd2840ebfeee7659ed609ee9d
nicolasrhone <nicolas.rhone@gmail.com>
2017-02-15 09:40:15 +0100

    log

--- theme/cte/src/modules/rubedoBlocks/controllers/D3ScriptController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/D3ScriptController.js
@@ -6,6 +6,7 @@ angular.module("rubedoBlocks").lazy.controller('D3ScriptController',['$scope','$
     var d3Code = config.d3Code ? config.d3Code : "";
     $scope.predefinedFacets = config.predefinedFacets ? config.predefinedFacets : "{ }";
     $scope.pageSize = config.pageSize ? config.pageSize : 5000;
+				$scope.loading = true;
     $scope.retrieveData=function(params, successFunction, failureFunction ){
         var options={
             start: 0,
