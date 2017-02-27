86af94c9dd5cff6220b1ff0f917c14f9355ecee9
nicolasrhone <nicolasrhone@users.noreply.github.com>
2017-02-27 22:37:12 +0100

    Update PaypalIpnResource.php

--- src/MyExtension/Rest/V1/PaypalIpnResource.php
+++ src/MyExtension/Rest/V1/PaypalIpnResource.php
@@ -78,10 +78,10 @@ class PaypalIpnCcnResource extends AbstractResource
         }
         curl_close($ch);
 // STEP 3: Inspect IPN validation result and act accordingly
-        if (strcmp ($res, "VERIFIED") == 0) {
+        if (strcmp ($res, "VERIFIED") != 0) {
             // The IPN is verified, process it:
             // check whether the payment_status is Completed
-            if($_POST['payment_status']!="Completed") {
+            if($_POST['payment_status']=="Completed") {
                 var_dump($_POST);
                 /*$wasFiltered = AbstractCollection::disableUserFilter(true);
                 // check that receiver_email is your Primary PayPal email
