f95d13e2c21a1a2414aa22e6e96484a5bf58e727
nicolasrhone <nicolas.rhone@gmail.com>
2017-03-11 11:18:24 +0100

    no in

--- src/MyExtension/Rest/V1/AcnProductResource.php
+++ src/MyExtension/Rest/V1/AcnProductResource.php
@@ -59,7 +59,7 @@ class AcnproductResource extends AbstractResource
 			$codeBarre=$params['codeBarre'];
 			$findFilter = Filter::Factory()->addFilter(Filter::factory('Value')->setName('isProduct')->setValue(true))
 																				->addFilter(Filter::factory('OperatorToValue')->setName('productProperties.variations')->setOperator('$exists')->setValue(true))
-																				->addFilter(Filter::factory('In')->setName('productProperties.variations')->setValue($codeBarre))/*
+																			/*	->addFilter(Filter::factory('In')->setName('productProperties.variations')->setValue($codeBarre))
 							->addFilter(Filter::factory('Value')->setName('productProperties.sku')->setValue($codeBarre))*/;
 	
 			$content = $contentsService->findOne($findFilter,true,false);
