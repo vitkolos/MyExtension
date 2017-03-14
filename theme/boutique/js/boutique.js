d3c5967eaeac36fd01e67697bf4e0d99867ebd63
nicolasrhone <nicolas.rhone@gmail.com>
2017-03-14 21:48:24 +0100

    suite

--- theme/boutique/js/boutique.js
+++ theme/boutique/js/boutique.js
@@ -156,7 +156,9 @@ angular.module('rubedoDataAccess').factory('RubedoOrdersService',['$http','ipCoo
 																																												jwplayer(id).setMute(true);
 																																												jwplayer(id).play(false);
 																																												duration =jwplayer(id).getDuration();
-																																												scope.duration = jwplayer(id).getDuration();scope.$apply();
+																																												scope.$apply(function () {
+																																																							scope.duration = jwplayer(id).getDuration();
+																																												});
 																																	// do something with duration here
 																															} else {
 																															}
