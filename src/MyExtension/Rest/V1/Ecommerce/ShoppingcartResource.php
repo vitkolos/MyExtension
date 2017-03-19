c51f7afc2b0d3f82f9a6a852332b5bf5b62a948d
nicolasrhone <nicolasrhone@users.noreply.github.com>
2017-03-19 22:21:49 +0100

    Update ShoppingcartResource.php

--- src/MyExtension/Rest/V1/Ecommerce/ShoppingcartResource.php
+++ src/MyExtension/Rest/V1/Ecommerce/ShoppingcartResource.php
@@ -77,6 +77,7 @@ class ShoppingcartResource extends AbstractResource
         if ($cartUpdate === false) {
             throw new APIEntityException('Update failed');
         }
+     $shoppingCart['shoppingCart'] = array_values($shoppingCart['shoppingCart']);
         return array(
             'success' => true,
             'shoppingCart' => $this->filterShoppingCart($cartUpdate, isset($params['includeDetail'])),
@@ -95,6 +96,7 @@ class ShoppingcartResource extends AbstractResource
         } else {
             $shoppingCart = $this->getShoppingCartCollection()->getCurrentCart($params['shoppingCartToken'],true);
         }
+     
         return array(
             'success' => true,
             'shoppingCart' => $this->filterShoppingCart($shoppingCart, isset($params['includeDetail'])),
