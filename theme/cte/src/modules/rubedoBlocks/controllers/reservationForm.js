a4c2294dba7044581717045824f09ea027285923
nicolasrhone <nicolasrhone@users.noreply.github.com>
2017-03-25 14:09:19 +0100

    Update reservationForm.js

--- theme/cte/src/modules/rubedoBlocks/controllers/reservationForm.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/reservationForm.js
@@ -1,3 +1,4 @@
 angular.module("rubedoBlocks").lazy.controller('FormCtrl',['$scope',function($scope){
+        var me = this;
         $scope.contactCtrl.contactData.test = $scope.contactCtrl.contactData['Nights']*3;
   }]);
