8cb05228e43db916a654fda065f16297b83ccf3e
nicolasrhone <nicolas.rhone@gmail.com>
2017-03-30 15:09:40 +0200

    add discount musculine

--- src/MyExtension/Rest/V1/MusculinePaymentResource.php
+++ src/MyExtension/Rest/V1/MusculinePaymentResource.php
@@ -58,37 +58,7 @@ class MusculinepaymentResource extends AbstractResource {
     $query['lc'] = 'FR'; // langue
     
     
-    /*quantités et prix des produits*/
-    $counter = 1;
-    $poids = 0;
-    foreach ($params['products'] as $sku => $product) {
-        if($product["quantite"]>0){
-            $query['item_name_'.$counter] = $product["titre"] ;
-            $query['quantity_'.$counter] =$product["quantite"];
-            $query['amount_'.$counter] = round($product["prix"],2);
-            $poids+= $product["quantite"] * $product["poids"];
-            $query['tax_rate_'.$counter] = 5.5;
-            $counter++;
-        };
-    };
-    
-    /*frais d'expédition*/
-        $fraisExp = 0;
-        if ($poids>0 AND $poids<=500) {
-           $fraisExp = 6.13;
-        }
-        elseif ($poids>500 AND $poids<=750) {
-            $fraisExp = 6.89;
-        }
-        elseif ($poids>750 AND $poids<=1000) {
-            $fraisExp =7.51;
-        }
-        elseif ($poids>1000 AND $poids<=2000) {
-            $fraisExp =8.50;
-        }
-        elseif ($poids>2000) {
-            $fraisExp =10.93;
-        }
+
     /*infos de facturation*/
 
 
@@ -115,7 +85,52 @@ class MusculinepaymentResource extends AbstractResource {
     else {
         $query['night_phone_b'] = $params['facturation']['telephone'];
     }
-    if(isset($params['facturation']['codePromo'])) {$query['custom'] =$params['facturation']['codePromo']; }
+    if(isset($params['facturation']['codePromo'])) {
+        $query['custom'] =$params['facturation']['codePromo'];
+        $isPromo = false;
+        $wasFiltered = AbstractCollection::disableUserFilter(true);
+        $contentsService = Manager::getService("ContentsCcn");
+        $codePromos = $contentsService->findById("58dd015024564055068b82c7",false,false)['fields'];
+        foreach ($codePromos["multi"] as $codePromo){
+            if($codePromo == $params['facturation']['codePromo']) {$isPromo=true; break;}
+        }
+        
+        $wasFiltered = AbstractCollection::disableUserFilter(false);
+        //58dd015024564055068b82c7
+    }
+    
+        /*quantités et prix des produits*/
+    $counter = 1;
+    $poids = 0;
+    foreach ($params['products'] as $sku => $product) {
+        if($product["quantite"]>0){
+            $query['item_name_'.$counter] = $product["titre"] ;
+            $query['quantity_'.$counter] =$product["quantite"];
+            $query['amount_'.$counter] = round($product["prix"],2);
+            $poids+= $product["quantite"] * $product["poids"];
+            $query['tax_rate_'.$counter] = 5.5;
+            if($isPromo) $query['discount_rate_'.$counter] = 10;
+            $counter++;
+        };
+    };
+    
+    /*frais d'expédition*/
+        $fraisExp = 0;
+        if ($poids>0 AND $poids<=500) {
+           $fraisExp = 6.13;
+        }
+        elseif ($poids>500 AND $poids<=750) {
+            $fraisExp = 6.89;
+        }
+        elseif ($poids>750 AND $poids<=1000) {
+            $fraisExp =7.51;
+        }
+        elseif ($poids>1000 AND $poids<=2000) {
+            $fraisExp =8.50;
+        }
+        elseif ($poids>2000) {
+            $fraisExp =10.93;
+        }
 
     // Prepare query string
     $query_string = http_build_query($query);
