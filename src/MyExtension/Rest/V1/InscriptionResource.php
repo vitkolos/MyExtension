0b4cec13d7db934b05ed42f327ddcfe76da6a753
nicolasrhone <nicolas.rhone@gmail.com>
2017-04-19 10:42:32 +0200

    suite

--- src/MyExtension/Rest/V1/InscriptionResource.php
+++ src/MyExtension/Rest/V1/InscriptionResource.php
@@ -7,6 +7,7 @@ use RubedoAPI\Entities\API\Definition\FilterDefinitionEntity;
 use RubedoAPI\Entities\API\Definition\VerbDefinitionEntity;
 use WebTales\MongoFilters\Filter;
 use Zend\Json\Json;
+use Rubedo\Backoffice\Controller;
 class InscriptionResource extends AbstractResource
 {
     /**
@@ -180,23 +181,15 @@ class InscriptionResource extends AbstractResource
             fputcsv($csvResource, $csvLine, ';');
         }
         $content = file_get_contents($filePath);
-        /*$response = $this->getResponse();
+        $response = $this->getResponse();
         $headers = $response->getHeaders();
         $headers->addHeaderLine('Content-Type', 'text/csv');
         $headers->addHeaderLine('Content-Disposition', "attachment; filename=\"$fileName\"");
         $headers->addHeaderLine('Accept-Ranges', 'bytes');
         $headers->addHeaderLine('Content-Length', strlen($content));
-        $response->setContent($content);*/
+        $response->setContent($content);
         //return $response;
-								
-								header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
-            header("Cache-Control: public"); // needed for internet explorer
-            header("Content-Type: text/csvp");
-            header("Content-Transfer-Encoding: Binary");
-            header("Content-Length:".strlen($content));
-            header("Content-Disposition: attachment; filename='$fileName'");
-            readfile($filePath);
-            die(); 
+
 								
 								return [
             'success' => true,
