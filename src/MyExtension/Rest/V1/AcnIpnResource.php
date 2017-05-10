ba5c7dc19fbf3a47ae4d7100681bdf7a6aabe2cb
nicolasrhone <nicolas.rhone@gmail.com>
2017-05-10 12:31:41 +0200

    suite

--- src/MyExtension/Rest/V1/AcnIpnResource.php
+++ src/MyExtension/Rest/V1/AcnIpnResource.php
@@ -202,8 +202,10 @@ class AcnIpnResource extends AbstractResource {
         //Bonjour Nicolas,
         $body .= '<tr><td valign="top"><h1>Bonjour, '. $order['userName'].'</h1>';
         //Confirmation de commande
-        $body .= '<p>Nous vous confirmons que nous avons bien enregistré votre commande  n°' .$order["orderNumber"] . ' en date du ' . substr($order['dateCode'],6,2).'/'.substr($order['dateCode'],4,2) . '/' . substr($order['dateCode'],0,4) . '. ';
-        $body .= 'Vous pourrez suivre l\'avancement de votre commande <a href="https://www.laboutique-chemin-neuf.com/fr/mes-commandes/detail-commande?order='.$order['id'] .'">en vous connectant sur le site</a></p></td></tr>';
+        $body .= '<p>Nous vous confirmons que nous avons bien enregistré votre commande  n°' .$order["orderNumber"] . ' en date du ' . substr($order['dateCode'],6,2).'/'.substr($order['dateCode'],4,2) . '/' . substr($order['dateCode'],0,4) . '.<br/> ';
+        $body .= 'Votre paiement en ligne par carte bancaire a bien été pris en compte.<br/>';
+        //$body .= 'Vous pourrez suivre l\'avancement de votre commande <a href="https://www.laboutique-chemin-neuf.com/fr/mes-commandes/detail-commande?order='.$order['id'] .'">en vous connectant sur le site</a> ()</p></td></tr>';
+        $body .= 'Vous pourrez suivre l\'avancement de votre commande <a href="https://www.laboutique-chemin-neuf.com/fr/mes-commandes/">en vous connectant sur le site</a> (pour voir vos commandes, vous devrez vous connecter si vous ne l\êtes pas)</p></td></tr>';
         //Récapitulatif de commande
         $body .= '<tr><td><h3>Récapitulatif de votre commande</h3></td></tr>';
         $body .= '<tr><td><table cellspacing="0" cellpadding="0" border="0" width="650" style="border:1px solid #eaeaea">';
