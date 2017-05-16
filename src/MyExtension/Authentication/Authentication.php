94a0409c1c247777f58bc80ec2946bbbbff3b6a5
nicolasrhone <nicolas.rhone@gmail.com>
2017-05-16 15:51:36 +0200

    suite

--- src/MyExtension/Authentication/Authentication.php
+++ src/MyExtension/Authentication/Authentication.php
@@ -49,8 +49,8 @@ class Authentication extends AuthenticationService
             ));
             throw new APIAuthException('Bad credentials', 401);
         }
-								$protocol = stripos($_SERVER['SERVER_PROTOCOL'],'http') === true ? 'http://' : 'https://';
-
+								$protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
+var_dump(stripos($_SERVER['SERVER_PROTOCOL'],'http'));
 								var_dump($protocol);
 		//var_dump($result);
 		//throw new APIAuthException($result->getMessages()[0], 500);
