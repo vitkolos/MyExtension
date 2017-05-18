0e02814fe12f6f1b8e7aac8061ee6de76638da84
nicolasrhone <nicolas.rhone@gmail.com>
2017-05-18 11:24:08 +0200

    scope

--- theme/foyers/js/foyers.js
+++ theme/foyers/js/foyers.js
@@ -35,7 +35,7 @@ angular.module("rubedo").directive("ascensor",[function(){
 											return {
 																						restrict: "A",
 																						template:'<div id="ascensorBuilding"><div ng-repeat="column in row.columns track by $index" ng-include="rubedo.componentsService.getColumnTemplate(column.customTemplate)"></div></div>',
-																						controller:[function() {
+																						controller:['$scope', function($scope) {
 																																	console.log($scope);
 																																	}]
 											}
