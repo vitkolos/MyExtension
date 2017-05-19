743677da7546ab7babda11ae97ba447c58e96dca
nicolasrhone <nicolas.rhone@gmail.com>
2017-05-19 10:54:10 +0200

    sutie

--- theme/foyers/ckeditor/default.js
+++ theme/foyers/ckeditor/default.js
@@ -9,7 +9,7 @@ CKEDITOR.addTemplates("default",{imagesPath:"/theme/cte/ckeditor/img/",templates
     {title:"Texte sur deux colonnes",image:"w33w66.png",description:"Texte sur deux colonnes (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
     html:'<div class="row"><div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-8"><p>Colonne 2 </p></div></div>'},
     {title:"Texte sur trois colonnes",image:"w33w33.png",description:"Texte sur trois colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
-    html:'<div class="row"><div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div></div>'},
+    html:'<div class="row 3cols"><div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div></div>'},
     {title:"Texte sur quatre colonnes",image:"w33w33.png",description:"Texte sur 4 colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
     html:'<div class="row"><div class="col-xs-12  col-sm-3"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-3"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-3"><p>Colonne 3</p></div><div class="col-xs-12  col-sm-3"><p>Colonne 4</p></div></div>'},
 				{title:"Texte sur quatre colonnes avec icôneq",image:"icone.png",description:"Texte sur 4 colonnes de même largeur avec des icônes (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
