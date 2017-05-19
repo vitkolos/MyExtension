22815130ac52503dc0290d65005dca28e50ff5e7
nicolasrhone <nicolas.rhone@gmail.com>
2017-05-19 09:43:46 +0200

    suite

--- theme/foyers/js/foyers.js
+++ theme/foyers/js/foyers.js
@@ -26,9 +26,11 @@ blocksConfig.bg_image={
 	angular.module('rubedoFields').filter('firstword', function() {
         return function(input, splitIndex) {
             // do some bounds checking here to ensure it has that index
-
-												if (!splitIndex) {
-																						 return input.split(" ")[0];
+											if (!input) {
+																						return "";
+											}
+											else	if (!splitIndex) {
+																						 return input.split(' ')[0];
 												}
 												else return input.split(' ').slice(1).join(' ');
         }
