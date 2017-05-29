a93ddde04160a3acf30d920bab0bb7d2375fb457
nicolasrhone <nicolas.rhone@gmail.com>
2017-05-29 11:21:03 +0200

    log

--- theme/wtp17/src/modules/rubedoBlocks/controllers/MenuController.js
+++ theme/wtp17/src/modules/rubedoBlocks/controllers/MenuController.js
@@ -38,6 +38,9 @@
 																								if (block.bType=="contentDetail" && block.orderValue<=1) {
 																												if(block.i18n[lang]) me.pagesBlocks[key].blocks.push({"title":block.i18n[lang].title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
 																												else me.pagesBlocks[key].blocks.push({"title":block.title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
+																												if (!block.code) {
+																																console.log(block);
+																												}
 																												//if(block.i18n[lang]) me.pagesBlocks[key].blocks.push({"title":block.i18n[lang].title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
 																												//else me.pagesBlocks[key].blocks.push({"title":block.i18n.fr.title});
 																								}
