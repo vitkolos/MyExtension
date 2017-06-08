b4672eaede7f021499b02fd70cf2a0374ae1fd53
nicolasrhone <nicolas.rhone@gmail.com>
2017-06-08 16:50:22 +0200

    tes

--- theme/cte/src/modules/rubedoBlocks/controllers/ChatbotController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/ChatbotController.js
@@ -2,16 +2,10 @@ angular.module("rubedoBlocks").lazy.controller('ChatbotController',['$scope','$t
 				$scope.discussion = [];
 				$scope.discussion.push("Que puis-je faire pour vous ?");
 				$scope.submit = function(){
-								$http({
-												method: 'GET',
-												url: 'http://10.66.50.200:5000/parse?q='+$scope.question,
-										}).then(function successCallback(response) {
-												console.log(response)
-														// this callback will be called asynchronously
-														// when the response is available
-												}, function errorCallback(response) {
-														// called asynchronously if an error occurs
-														// or server returns response with an error status.
-												});
+								$http.get("/api/v1/bot",{
+                params:{
+                    query:$$scope.question
+                }
+            }).then(function(response){console.log(response)});
 				}
 }]);
