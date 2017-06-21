17955152f9f7ada11305a6f451f3b8d25186f779
nicolasrhone <nicolas.rhone@gmail.com>
2017-06-21 17:09:31 +0200

    test

---
+++ theme/baskerville/js/baskerville.js
@@ -0,0 +1,204 @@
+blocksConfig.buttonToPage={
+           "template": "/templates/blocks/buttonToPage.html",
+          "internalDependencies":["/src/modules/rubedoBlocks/controllers/buttonToPage.js"]
+};
+blocksConfig.simpleContact={
+           "template": "/templates/blocks/simpleContact.html",
+          "internalDependencies":["/src/modules/rubedoBlocks/controllers/simpleContact.js"]
+};
+
+
+blocksConfig.bg_image={
+           "template": "/templates/blocks/bg_image.html",
+          "internalDependencies":["/src/modules/rubedoBlocks/controllers/BgImageController.js"]
+};
+blocksConfig.footer={
+           "template": "/templates/blocks/footer.html"
+};
+blocksConfig.contentDetail = {
+            "template": "/templates/blocks/contentDetail.html",
+            "externalDependencies":['//s7.addthis.com/js/300/addthis_widget.js'],
+            "internalDependencies":["/src/modules/rubedoBlocks/controllers/ContentDetailController.js","/src/modules/rubedoBlocks/directives/DisqusDirective.js"]
+};
+blocksConfig.sectionPresentation={
+           "template": "/templates/blocks/sectionPresentation.html",
+          "internalDependencies":["/src/modules/rubedoBlocks/controllers/sectionPresentation.js","/src/modules/rubedoBlocks/directives/angular-parallax.js"]
+};
+blocksConfig.carrousel2={
+           "template": "/templates/blocks/carrousel_fullWidth.html",
+          "internalDependencies":["/src/modules/rubedoBlocks/controllers/carrousel_fullWidth.js"]
+};
+blocksConfig.redirect={
+           "template": "/templates/blocks/redirect.html",
+          "internalDependencies":["/src/modules/rubedoBlocks/controllers/redirectController.js"]
+};
+
+angular.module('rubedo').filter('ligneNonVide', function () {
+           return function (input) {
+                      var filtered = [];
+		      var contentDisplay = false;
+                      angular.forEach(input, function(row, index) {
+				// si la 1ère colonne est terminale et non vide
+                                 if (row.columns[0].isTerminal&&row.columns[0].blocks) {
+				    // toujours afficher la 1ère ligne (menu) et la dernière (footer)
+				    if (index ==0 || index >= input.length-1) {
+					filtered.push(row);
+				    }
+				    // si la page sert à afficher un contenu (en 2ème ligne) on n'affiche pas les autres lignes
+				    else if (row.columns[0].blocks[0].configBloc.isAutoInjected)  {
+					filtered.push(row);
+					contentDisplay = true;
+				    }
+				    
+				    // sinon on affiche tout
+				    else if(!contentDisplay) {filtered.push(row);}
+                                            
+					    
+                                 }
+                      });
+                      return filtered;
+		    
+     };
+  });
+
+angular.module('rubedoBlocks').controller("AudioFileController",["$scope","RubedoMediaService",function($scope,RubedoMediaService){
+        var me=this;
+        var mediaId=$scope.audioFileId;
+         me.displayMedia=function(){
+            if (me.media&&me.media.originalFileId){
+
+                        me.jwSettings={
+                            primary:"flash",
+                            width:"100%",
+                            height:40,
+                            file:me.media.url,
+                        };
+                        setTimeout(function(){jwplayer("audio"+me.media.originalFileId).setup(me.jwSettings);}, 200);
+            }
+        };
+        if (mediaId){
+            RubedoMediaService.getMediaById(mediaId).then(
+                function(response){
+                    if (response.data.success){
+                        me.media=response.data.media;
+                        me.displayMedia();
+                    }
+                }
+            );
+        }
+    }]);
+
+ angular.module('rubedoBlocks').directive('jwplayer', ['$compile', function ($compile) {
+    return {
+        restrict: 'EC',
+        link: function (scope, element, attrs) {
+           var filmUrl = attrs.videoUrl;
+            var id = 'random_player_' + Math.floor((Math.random() * 999999999) + 1),
+            getTemplate = function (playerId) {
+                      
+                return '<div id="' + playerId + '"></div>';
+            };
+           var options = {
+                      file: filmUrl,
+                      modestbranding:0,
+                      showinfo:1,
+                      width:"100%",
+                      aspectratio:"16:9"};
+            element.html(getTemplate(id));
+            $compile(element.contents())(scope);
+            jwplayer(id).setup(options);
+            
+        }
+    };
+}]);
+
+ angular.module('rubedoBlocks').directive('balanceText', function ($timeout) {
+    return {
+        restrict: 'C',
+        link:  function (scope, element, attr) {
+           function balanceText() {
+                      element.balanceText();
+            }
+            $timeout(balanceText);
+                    
+        }
+    };
+});
+
+angular.module('rubedoBlocks').directive('loadModal', function () {
+    return {
+        restrict: 'A',
+        link: function(scope, $elm, attrs) {
+            $elm.bind('click', function(event) {
+                event.preventDefault();
+                /*angular.element('#myModal iframe').attr('src', src);*/
+                angular.element('#myModal').appendTo('body').modal('show');
+            });
+            scope.dismiss = function() {
+                      angular.element('#myModal').modal('hide');
+           };
+        }
+    }
+});
+
+
+
+
+    angular.module('rubedoDataAccess').factory('RubedoMailService', ['$http',function($http) {
+        var serviceInstance={};
+        serviceInstance.sendMail=function(payload){
+            return ($http({
+                url:"api/v1/mail",
+                method:"POST",
+                data : payload
+            }));
+        };
+        return serviceInstance;
+    }]);
+    angular.module('rubedoDataAccess').factory('TaxonomyService', ['$http',function($http) {
+        var serviceInstance={};
+	serviceInstance.getTaxonomyByVocabulary=function(vocabularies){
+            return ($http.get("/api/v1/taxonomies",{
+                params:{
+                    vocabularies:vocabularies
+                }
+            }));
+	};
+        return serviceInstance;
+    }]);  
+angular.module('rubedoBlocks').directive('addthisToolbox', ['$timeout','$location','$http', function($timeout,$location,$http) {
+  return {
+    restrict : 'A',
+	  transclude : true,
+	  replace : true,
+	  template : '<div ng-transclude></div>',
+	  link : function($scope, element, attrs) {
+		$timeout(function () {
+                      addthis.init();
+                      var contentUrl = $location.absUrl();
+                      addthis.toolbox(angular.element('.addthis_toolbox').get(), {}, {
+                                 url: contentUrl,
+                                 title : attrs.title,
+                                 description : ''        
+                      });
+		/*if ($window.addthis.layers && $window.addthis.layers.refresh) {
+                        $window.addthis.layers.refresh();
+                    }*/
+		$scope.nbOfLikes=0;
+		$http({method: 'GET',url: 'http://graph.facebook.com/?id='+contentUrl})
+		.then(function successCallback(response) {
+			$scope.nbOfLikes += response.data.shares;
+		},
+		function errorCallback(response) {
+		});
+		$http({method: 'GET',url: 'http://cdn.api.twitter.com/1/urls/count.json?url='+contentUrl})
+		.then(function successCallback(response) {
+			$scope.nbOfLikes += response.data.count;
+		},
+		function errorCallback(response) {
+		});		
+
+		});
+	    }
+	};
+}]);
