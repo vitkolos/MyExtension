ef00614283645be69e7bcf10ef511c1c969d9b5d
nicolasrhone <nicolas.rhone@gmail.com>
2017-06-21 17:59:29 +0200

    css

--- theme/baskerville/js/baskerville.js
+++ theme/baskerville/js/baskerville.js
@@ -1,36 +1,19 @@
-blocksConfig.buttonToPage={
-           "template": "/templates/blocks/buttonToPage.html",
-          "internalDependencies":["/src/modules/rubedoBlocks/controllers/buttonToPage.js"]
-};
 blocksConfig.simpleContact={
            "template": "/templates/blocks/simpleContact.html",
           "internalDependencies":["/src/modules/rubedoBlocks/controllers/simpleContact.js"]
 };
-
-
 blocksConfig.bg_image={
            "template": "/templates/blocks/bg_image.html",
           "internalDependencies":["/src/modules/rubedoBlocks/controllers/BgImageController.js"]
-};
-blocksConfig.footer={
-           "template": "/templates/blocks/footer.html"
 };
 blocksConfig.contentDetail = {
             "template": "/templates/blocks/contentDetail.html",
             "externalDependencies":['//s7.addthis.com/js/300/addthis_widget.js'],
             "internalDependencies":["/src/modules/rubedoBlocks/controllers/ContentDetailController.js","/src/modules/rubedoBlocks/directives/DisqusDirective.js"]
 };
-blocksConfig.sectionPresentation={
-           "template": "/templates/blocks/sectionPresentation.html",
-          "internalDependencies":["/src/modules/rubedoBlocks/controllers/sectionPresentation.js","/src/modules/rubedoBlocks/directives/angular-parallax.js"]
-};
 blocksConfig.carrousel2={
            "template": "/templates/blocks/carrousel_fullWidth.html",
           "internalDependencies":["/src/modules/rubedoBlocks/controllers/carrousel_fullWidth.js"]
-};
-blocksConfig.redirect={
-           "template": "/templates/blocks/redirect.html",
-          "internalDependencies":["/src/modules/rubedoBlocks/controllers/redirectController.js"]
 };
 
 angular.module('rubedo').filter('ligneNonVide', function () {
