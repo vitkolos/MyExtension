d71f4a4ca55b7acbd7f2530ecf5372a53a7755a2
nicolasrhone <nicolas.rhone@gmail.com>
2017-06-22 15:19:11 +0200

    suite

--- theme/baskerville/ckeditor/config.js
+++ theme/baskerville/ckeditor/config.js
@@ -7,13 +7,13 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-    config.colorButton_colors = "ffd600,c5c5c5,2e2c29";
+    config.colorButton_colors = "13c4a5,ffffff,444,";
 
 };
 
 
 CKEDITOR.stylesSet.add( 'default', [
-    { name: 'Titre section', element: 'h3', attributes: { 'class': 'block-title'} },
+    { name: 'Tags', element: 'p', attributes: { 'class': 'tag'} },
     {
 		name: 'Image 100%',
 		element: 'img',
