0de1df69770c89812301f7187d2989f646892a22
nicolasrhone <nicolas.rhone@gmail.com>
2017-06-23 15:26:30 +0200

    bask

--- theme/baskerville/ckeditor/config.js
+++ theme/baskerville/ckeditor/config.js
@@ -8,12 +8,15 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
     config.colorButton_colors = "13c4a5,ffffff,444,";
-
+        config.colorButton_enableMore = false;
+    config.youtube_responsive = true;
+        config.format_tags ='p;h1;h2;h3;h4;h5;h6','button';
 };
 
 
 CKEDITOR.stylesSet.add( 'default', [
     { name: 'Tags', element: 'p', attributes: { 'class': 'tag'} },
+    { name: 'Bouton', element: 'a', attributes: { 'class': 'button '} },
     {
 		name: 'Image 100%',
 		element: 'img',
