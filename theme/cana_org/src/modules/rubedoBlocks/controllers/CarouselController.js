aa2a77be268352b6df84fd1513bb5499441ea902
Nicolas Rhoné <nicolas.rhone@chemin-neuf.org>
2017-07-10 10:04:49 +0200

    test

--- theme/cana_org/src/modules/rubedoBlocks/controllers/CarouselController.js
+++ theme/cana_org/src/modules/rubedoBlocks/controllers/CarouselController.js
@@ -53,7 +53,7 @@ angular.module("rubedoBlocks").lazy.controller("CarouselController",["$scope","R
             autoPlay: blockConfig.autoPlay,
             stopOnHover: blockConfig.stopOnHover,
             paginationNumbers:blockConfig.showPagingNumbers,
-            navigationText: ['<span class="glyphicon glyphicon-menu-left"></span>','<span class="glyphicon glyphicon-menu-right"></span>'],
+            navigationText: ['<span class="fa fa-angle-left"></span>','<span class="fa fa-angle-right"></span>'],
             lazyLoad:true
         };
         angular.element(targetElSelector).owlCarousel(owlOptions);
