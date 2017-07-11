fb7cc7c44f81695b5e8fd83f258cf6872064d4ae
Nicolas Rhoné <nicolas.rhone@chemin-neuf.org>
2017-07-11 18:26:58 +0200

    suite

--- theme/bethanien/ckeditor/default.js
+++ theme/bethanien/ckeditor/default.js
@@ -7,7 +7,7 @@ CKEDITOR.addTemplates("default",{imagesPath:"/theme/cte/ckeditor/img/",templates
     html:'<div class="row"><div class="col-xs-12  col-sm-6"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-6"><p>Colonne 2 </p></div></div>'},
 	 {title:"Texte sur deux colonnes 2/3 - 1/3",image:"w66w33.png",description:"Texte sur deux colonnes  (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
     html:'<div class="row"><div class="col-xs-12  col-sm-8"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div></div>'},
- {title:"Texte sur deux colonnes 1/3 - 2/3",image:"w66w33.png",description:"Texte sur deux colonnes  (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
+ {title:"Texte sur deux colonnes 1/3 - 2/3",image:"w33w66.png",description:"Texte sur deux colonnes  (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
     html:'<div class="row"><div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-8"><p>Colonne 2 </p></div></div>'},
     {title:"Texte sur trois colonnes",image:"w33w33.png",description:"Texte sur trois colonnes de même largeur (qui passent l'une au dessus de l'autre sur tablettes / smartphones)",
     html:'<div class="row"><div class="col-xs-12  col-sm-4"><p>Colonne 1 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 2 </p></div><div class="col-xs-12  col-sm-4"><p>Colonne 3</p></div></div>'}
