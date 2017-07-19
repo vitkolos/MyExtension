14dc80baeb23a14f3b99e70ab90f7d47ed7741a0
davidfaijan <fjn.david@gmail.com>
2017-07-19 10:03:00 +0200

    c

--- theme/foi/src/modules/rubedoBlocks/controllers/ContentDetailController.js
+++ theme/foi/src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -222,7 +222,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
     me.getUserById = function (userId){
             RubedoUsersService.getUserById(userId).then(
                 function(response){
-                    console.log(response);
+                    console.log($scope.response);
                     if(response.data.success){
                         $scope.rubedo.current.user=response.data.user;}
                 })}
