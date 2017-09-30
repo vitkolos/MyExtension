59219a3781caac8d4fd04d0bba4f33c71510aa70
davidfaijan <fjn.david@gmail.com>
2017-09-30 11:09:24 +0200

    suite

--- theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
@@ -529,8 +529,9 @@ angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope'
 				console.log("par getcontents");
 				console.log(RubedoContentsService.getContents($scope.contentDetailCtrl.content.fields.contact));
 				//RubedoContentsService.getContentById($scope.contentDetailCtrl.content.fields.contact, options)*/
-				consol.log("contact");
-				console.log($scope.inscription.contact);
+				console.log("contact");
+				console.log($scope.inscription);
+				console.log($scope.inscription.mails_secretariat);
 
     
 }]);
