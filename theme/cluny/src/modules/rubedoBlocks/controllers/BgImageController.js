b08086b4aa1b5ed76df8257c51b1a17af9d41bbe
davidfaijan <fjn.david@gmail.com>
2017-11-14 16:52:59 +0100

    Creation Cluny

---
+++ theme/cluny/src/modules/rubedoBlocks/controllers/BgImageController.js
@@ -0,0 +1,19 @@
+angular.module("rubedoBlocks").lazy.controller("BgImageController",["$scope","RubedoPagesService", function($scope,RubedoPagesService){
+    var me = this;
+    var config = $scope.blockConfig;
+    if (config.externalURL){
+        me.url=config.externalURL;
+    } else if (config.imageLink&&mongoIdRegex.test(config.imageLink)){
+        RubedoPagesService.getPageById(config.imageLink).then(function(response){
+            if (response.data.success){
+                me.url=response.data.url;
+            }
+        });
+    }
+    
+    
+    if(config.imageFile) {
+        $scope.rubedo.current.page.image = $scope.rubedo.imageUrl.getUrlByMediaId(config.imageFile,{width:'800px'});
+    }    
+
+}]);
---
+++ theme/cluny/src/modules/rubedoBlocks/controllers/D3ScriptController.js
@@ -0,0 +1,62 @@
+angular.module("rubedoBlocks").lazy.controller('D3ScriptController',['$scope','$sce','RubedoSearchService',function($scope,$sce,RubedoSearchService){
+    var me = this;
+    var config = $scope.blockConfig;
+				var themePath="/theme/"+window.rubedoConfig.siteTheme;
+				me.ccnMap = themePath+"/templates/blocks/ccnMap.html";
+    var d3Code = config.d3Code ? config.d3Code : "";
+    $scope.predefinedFacets = config.predefinedFacets ? config.predefinedFacets : "{ }";
+    $scope.pageSize = config.pageSize ? config.pageSize : 5000;
+				$scope.loading = true;
+				var mapType = config.mapType ? config.mapType : 'cte';
+				//$scope.clearORPlaceholderHeight();
+    $scope.retrieveData=function(params, successFunction, failureFunction ){
+        var options={
+            start: 0,
+            limit: $scope.pageSize,
+            predefinedFacets: $scope.predefinedFacets,
+            displayedFacets: "['all']",
+            pageId: $scope.rubedo.current.page.id,
+            siteId: $scope.rubedo.current.site.id,
+            searchMode:"aggregate"
+        };
+        angular.forEach(params, function(value, key){
+            options[key]=value;
+        });
+        RubedoSearchService.searchByQuery(options).then(
+            function(response){
+																me.results = response.data.results.data;
+																me.countryList = {};
+																angular.forEach(me.results, function(country){
+																				var id=country["fields.id"][0];
+																				me.countryList[id] = {
+																								'name':country["title"],
+																								'fillKey':country["presence"],
+																								'text':''
+																				};
+																				if (mapType!='cte') {
+																								if(country[mapType] && country[mapType]!="") me.countryList[id]['url'] = country[mapType];
+																								else if(country["presence"]=="active") me.countryList[id]['fillKey']='not';
+																				}
+																				else {
+																								if(country["fields.url"]&&country["fields.url"][0]!="") me.countryList[id]['url']=country["fields.url"][0];
+
+																								var missions = ["cana","1418","1830","foyers","jet","netforgod"];
+																								for (var i = 0; i < missions.length; i++) {
+																												if(country[missions[i]]&&country[missions[i]]!="") me.countryList[id]['text']+=translations['Missions.'+missions[i]]+'<br/>';
+																								}
+
+																				}
+																				
+																});
+																console.log(me.results);
+                successFunction(me.countryList);
+            },
+            function(response){
+                failureFunction(response.data);
+            }
+        );
+    };
+    me.html=$sce.trustAsHtml(d3Code);
+
+}]);
+
