a1117b453788a949c57ac43f377fed4cc8da3e16
davidfaijan <fjn.david@gmail.com>
2018-01-25 17:29:30 +0100

    suite

--- theme/foi/src/modules/rubedoBlocks/controllers/ContentDetailController.js
+++ theme/foi/src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -290,6 +290,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
       RubedoSearchService.searchByQuery(optionsSommaire).then(function(response){
         if(response.data.success){
           me.foiContents = response.data.results;
+										console.log(response.data.results);
         } 
       });
     };
