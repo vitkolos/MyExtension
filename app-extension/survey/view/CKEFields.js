e7c06a0f901507e8de3290c51f5408c06d1b4ab8
davidfaijan <fjn.david@gmail.com>
2018-01-29 14:32:44 +0100

    suite

--- app-extension/survey/view/CKEFields.js
+++ app-extension/survey/view/CKEFields.js
@@ -116,7 +116,7 @@ Ext.define('Rubedo.view.CKEField', {
             filebrowserImageBrowseUrl:"ext-finder?type=Image",
             filebrowserImageUploadUrl:null,
             forcePasteAsPlainText: true,
-            font_names : 'Merriweather;Montserrat;FontAwesome;Allura;HoneyScript; cheminneuf',
+            font_names : 'Merriweather;Montserrat;FontAwesome;Oswald Light;Oswald SemiBold; cheminneuf',
         });
         component.editor.on("maximize",function(){
             var mainWindow=component.findParentByType("window");
