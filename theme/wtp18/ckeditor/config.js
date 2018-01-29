3c36a02c3e0ee9d36da6fc9e107b9b310134c809
davidfaijan <fjn.david@gmail.com>
2018-01-29 10:23:26 +0100

    suite

--- theme/wtp18/ckeditor/config.js
+++ theme/wtp18/ckeditor/config.js
@@ -7,7 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-        config.colorButton_colors = 'c3357b','c472a72','9c9b9d,2f2e2a,ffffff';
+        config.colorButton_colors = 'c3357b,c472a72,9c9b9d,2f2e2a,ffffff';
         config.colorButton_enableMore = false;
         config.youtube_responsive = true;
         };
