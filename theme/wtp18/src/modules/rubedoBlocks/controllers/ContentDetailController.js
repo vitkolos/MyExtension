89ebeb45ef151e7f7e184926cb762811412ad4ff
davidfaijan <fjn.david@gmail.com>
2018-01-31 08:14:03 +0100

    suite

--- theme/wtp18/src/modules/rubedoBlocks/controllers/ContentDetailController.js
+++ theme/wtp18/src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -179,6 +179,8 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
                                     me.detailTemplate=themePath+'/templates/blocks/contentDetail/'+me.content.type.code+".html";
                                     $scope.fields=me.transformForFront(me.content.type.fields);
                                     $scope.clearORPlaceholderHeight();
+                                    console.log('me.detailTemplate');
+                                    console.log(me.detailTemplate);
                                 },
                                 function (response){
                                     me.detailTemplate=themePath+'/templates/blocks/contentDetail/default.html';
