3914ff5d3c48894df15a1ce6d18e91c9996ca649
davidfaijan <fjn.david@gmail.com>
2018-02-08 20:52:17 +0100

    suite

--- theme/foi/src/modules/rubedoBlocks/controllers/ContentDetailController.js
+++ theme/foi/src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -349,7 +349,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
 																						console.log(response.data.results);
 																							console.log(response.data.results.data);
 																							angular.forEach(me.infoArticles.data,function(data, key){
-																																taxonomies[index]=data[index]['taxonomy.5a114f1b396588d22856706f'];
+																																taxonomies[index]=me.infoArticles.data[index]['taxonomy.5a114f1b396588d22856706f'];
 																																if (taxonomies[index]==taxonomies[index-1] && index!==0) {
 																																				$scope.displayTaxo= false;
 																																} else {
