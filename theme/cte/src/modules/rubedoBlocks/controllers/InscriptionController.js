7cfc3ff24a32626975e5c902896ba2f11ca7f1e0
davidfaijan <fjn.david@gmail.com>
2018-03-02 17:56:09 +0100

    suite

--- theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
@@ -339,6 +339,7 @@ angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope'
 
             if($scope.inscription.paiement_maintenant == 'rien'){$scope.inscription.montantAPayerMaintenant=0}
             else if($scope.inscription.paiement_maintenant == 'accompte'){$scope.inscription.montantAPayerMaintenant=me.content.fields.accompte}
+												else if($scope.inscription.paiement_maintenant == 'accompte2'){$scope.inscription.montantAPayerMaintenant=me.content.fields.accompte2}
             else if($scope.inscription.paiement_maintenant == 'totalite'){$scope.inscription.montantAPayerMaintenant=$scope.inscription.montantTotalAPayer}
             $scope.inscription.isPayment = me.isPaiement;
             /*STATUS DE L'INSCRIPTION*/
