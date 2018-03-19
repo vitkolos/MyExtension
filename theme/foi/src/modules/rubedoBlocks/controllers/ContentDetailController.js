3def70a8a43bf1236bb688c2efdc6cac135c9acd
davidfaijan <fjn.david@gmail.com>
2018-03-19 10:31:58 +0100

    suite

--- theme/foi/src/modules/rubedoBlocks/controllers/ContentDetailController.js
+++ theme/foi/src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -289,7 +289,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
             RubedoUsersService.getUserById($scope.rubedo.current.user.id).then(
                 function(response){
                     if(response.data.success){
-                        if (response.data.user.groups.includes("596e2e483965889a1f7bf6d1")){
+                        if (response.data.user.type.fields.type==="Abonnés FOI"){
                             $scope.isClient=true;
 																												$scope.isVisiteur=true;
 																												console.log("Abonné FOI");
@@ -301,7 +301,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
 																												console.log($scope.isVisiteur);
                         }
 																								/* 58e0b676245640ef008bb635 == groupe Revue FOI */
-																								else if (response.data.user.groups.includes("58e0b676245640ef008bb635")){
+																								else if (response.data.user.groups.include("58e0b676245640ef008bb635"){
                             $scope.isVisiteur=true;
 																												console.log("Simple visiteur");
 																												console.log("5a870ea739658802628b4567");
