5851135a0ced203e1f5bfef46fb9c0f0f597e533
davidfaijan <fjn.david@gmail.com>
2018-04-14 09:28:59 +0200

    suite CL

--- theme/cluny/js/cluny.js
+++ theme/cluny/js/cluny.js
@@ -40,7 +40,89 @@ blocksConfig.imageBatchUpload={
           "internalDependencies":["/src/modules/rubedoBlocks/controllers/imageBatchUploadController.js"]
 };
 
+angular.module('rubedoBlocks').filter('title', function() {
+    return function(input){
+		  var delimiter = '//';
+		  return input.split(delimiter)[0];
+    }
+});
+angular.module('rubedoBlocks').filter('section', function() {
+    return function(input){
+		  var delimiter = '//';
+		  return input.split(delimiter)[1];
+    }
+});
+angular.module('rubedoBlocks').filter('homepage', function() {
+    return function(input){
+		  var delimiter = '/';
+		  var array = input.split(delimiter);
+                  var url ="";
+                  for (i = 0; i < array.length-2; i++) {url = url + array[i]+'/'}
+                  return url;
+    }
+});
 
+
+ angular.module('rubedoFields').filter('split', function() {
+        return function(input, splitChar, splitIndex) {
+            // do some bounds checking here to ensure it has that index
+											if (!splitChar) {
+																				splitChar = "_";
+											}
+											if (input && input !='') {
+																					return input.split(splitChar)[splitIndex];
+											}
+											else return "";
+            
+            
+        }
+    });  
+    
+angular.module('rubedoBlocks').directive('toggleclass', function() {
+            return {
+                      restrict: 'A',
+                      link: function(scope, elm, attrs) {
+                                 var element = $(elm);
+                                  element.bind('click', function(event) {
+                                            element.next().toggleClass("displayed");
+                                 });
+                      }
+           }
+ });
+ 
+	angular.module('rubedoBlocks').directive('scrollPosition', function($window) {
+  return {
+    scope: {
+      scroll: '=scrollPosition'
+    },
+    link: function(scope, element, attrs) {
+      var windowEl = angular.element($window);
+      var handler = function() {
+        scope.scroll = windowEl.scrollTop();
+      }
+      windowEl.on('scroll', scope.$apply.bind(scope, handler));
+      handler();
+    }
+  };
+});
+angular.module('rubedoBlocks').directive('isInView', function($window) {
+    return {
+      scope: {
+        scroll: '=isInView'
+      },
+      link: function(scope, element, attrs) {
+        var windowEl = angular.element($window);
+        var handler = function() {
+          var windowTop = windowEl.scrollTop();
+          var windowBottom = windowEl.scrollTop()+windowEl.height();
+          var elemPosition = Math.round( element.offset().top );
+          if(elemPosition+100<windowBottom && elemPosition>windowTop) {element.addClass("inView");console.log('in view');}
+        }
+        windowEl.on('scroll', scope.$apply.bind(scope, handler));
+        handler();
+      }
+    };
+  });
  angular.module('rubedoBlocks').directive('jwplayer', ['$compile', function ($compile) {
     return {
         restrict: 'EC',
@@ -64,21 +146,6 @@ blocksConfig.imageBatchUpload={
     };
 }]);
 
-	angular.module('rubedoBlocks').directive('scrollPosition', function($window) {
-  return {
-    scope: {
-      scroll: '=scrollPosition'
-    },
-    link: function(scope, element, attrs) {
-      var windowEl = angular.element($window);
-      var handler = function() {
-        scope.scroll = windowEl.scrollTop();
-      }
-      windowEl.on('scroll', scope.$apply.bind(scope, handler));
-      handler();
-    }
-  };
-});
 
 
 
