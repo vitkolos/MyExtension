7a549300bb278ff81e0442a0778223bc8eef0d93
davidfaijan <fjn.david@gmail.com>
2018-04-18 15:29:45 +0200

    Envoi mail CLUNT

--- theme/cluny/src/modules/rubedoBlocks/controllers/simpleContact.js
+++ theme/cluny/src/modules/rubedoBlocks/controllers/simpleContact.js
@@ -1,10 +1,53 @@
-angular.module("rubedoBlocks").lazy.controller('ContactBlockController',['$scope','$location','RubedoMailService',function($scope,$location,RubedoMailService){
+//angular.module("rubedoBlocks").lazy.controller('ContactBlockController',['$scope','$location','RubedoMailService',function($scope,$location,RubedoMailService){
+//    var me = this;
+//    var config = $scope.blockConfig;
+//    me.contactData={ };
+//    me.contactError=null;
+//    
+//    me.submit=function($scope){
+//        me.contactError=null;
+//        var contactSnap=angular.copy(me.contactData);
+//        var payload={
+//            to:config.email,
+//            from:me.contactData.email,
+//            subject:contactSnap.subject
+//        };
+//        /*var destinataires = {'Nicolas':'nicolas.rhone@gmail.com' ,'Nicolas Rhoné':'nicolas.rhone@wanadoo.fr' }*/
+//        delete (contactSnap.subject);
+//        delete (contactSnap.to);
+//        payload.fields=contactSnap;
+//        angular.element('#myModal').modal('hide');
+//        payload.fields["website"] = $location.absUrl();
+//        RubedoMailService.sendMail(payload).then(
+//            function(response){
+//                if (response.data.success){
+//                    me.contactData={ };
+//                    me.showForm=false;
+//                    me.showConfirmMessage=true;
+//                    $scope.rubedo.sendGaEvent('/form/', 'contact');
+//                } else {
+//                    me.contactError=response.data.message;
+//                }
+//            },
+//            function(response){
+//                me.contactError=response.data.message;
+//            }
+//        );
+//    };
+//}]);
+
+
+angular.module("rubedoBlocks").lazy.controller('ContactBlockController',['$scope','$location','$filter','RubedoMailService',function($scope,$location,$filter,RubedoMailService){
     var me = this;
     var config = $scope.blockConfig;
     me.contactData={ };
     me.contactError=null;
-    
-    me.submit=function($scope){
+    $scope.clearORPlaceholderHeight();
+    me.dateDiffence = function(start,end){
+        console.log($filter('number')((end-start)/(3600*24*1000),0));
+        return $filter('number')((end-start)/(3600*24*1000),0);
+    }
+    me.submit=function(){
         me.contactError=null;
         var contactSnap=angular.copy(me.contactData);
         var payload={
@@ -12,11 +55,14 @@ angular.module("rubedoBlocks").lazy.controller('ContactBlockController',['$scope
             from:me.contactData.email,
             subject:contactSnap.subject
         };
+        if(contactSnap.template) {payload.template = contactSnap.template;delete (contactSnap.template);}
         /*var destinataires = {'Nicolas':'nicolas.rhone@gmail.com' ,'Nicolas Rhoné':'nicolas.rhone@wanadoo.fr' }*/
         delete (contactSnap.subject);
         delete (contactSnap.to);
+        
         payload.fields=contactSnap;
-        angular.element('#myModal').modal('hide');
+        angular.element('#myModal'+$scope.block.id+$scope.blockConfig.id).modal('hide');
+        angular.element('#myModalsingle').modal('hide');
         payload.fields["website"] = $location.absUrl();
         RubedoMailService.sendMail(payload).then(
             function(response){
@@ -24,10 +70,14 @@ angular.module("rubedoBlocks").lazy.controller('ContactBlockController',['$scope
                     me.contactData={ };
                     me.showForm=false;
                     me.showConfirmMessage=true;
-                    $scope.rubedo.sendGaEvent('/form/', 'contact');
+                    //$scope.rubedo.sendGaEvent('/form/', 'contact');
+                    if(window.ga) {
+                        window.ga('send', 'event', 'contact', 'envoi de mail',config.email);
+                    }
                 } else {
                     me.contactError=response.data.message;
                 }
+                $('#myModal').modal('hide');
             },
             function(response){
                 me.contactError=response.data.message;
