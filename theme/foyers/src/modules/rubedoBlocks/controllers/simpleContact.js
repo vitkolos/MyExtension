3ec23bdb7e140b657e0fdcd347ec2ee1cd3342da
davidfaijan <fjn.david@gmail.com>
2018-06-08 16:37:07 +0200

    suite

--- theme/foyers/src/modules/rubedoBlocks/controllers/simpleContact.js
+++ theme/foyers/src/modules/rubedoBlocks/controllers/simpleContact.js
@@ -74,6 +74,8 @@ angular.module("rubedoBlocks").lazy.controller('ContactBlockController',['$scope
             
             if (step==0) {me.toggleStage(1);}
             else if (step==1) {
+																concole.log($scope.contactCtrl.contactData.email);
+																concole.log($scope.contactCtrl.contactData.email_verif);
 																if( $scope.contactCtrl.contactData.email != $scope.contactCtrl.contactData.email_verif){
                     $scope.mailError = true;me.currentStage=1;
                 } else {
