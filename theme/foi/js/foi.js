4d111953e86a68b4ebcd4cb47bae6f0a39202e02
davidfaijan <fjn.david@gmail.com>
2018-06-18 12:57:14 +0200

    suite

--- theme/foi/js/foi.js
+++ theme/foi/js/foi.js
@@ -4,7 +4,7 @@ blocksConfig.bg_image={
 };
 blocksConfig.contentDetail = {
             "template": "/templates/blocks/contentDetail.html",
-            "externalDependencies":['//s7.addthis.com/js/300/addthis_widget.js'],
+            "externalDependencies":['//s7.addthis.com/js/300/addthis_widget.js','//cdnjs.cloudflare.com/ajax/libs/masonry/3.3.2/masonry.pkgd.min.js','//cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/3.2.0/imagesloaded.pkgd.min.js'],
             "internalDependencies":["/src/modules/rubedoBlocks/controllers/ContentDetailController.js"]
 };
 
