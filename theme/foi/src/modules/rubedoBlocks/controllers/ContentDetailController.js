05352852c08b9f5ab66fe5096fab3861176ee5e8
davidfaijan <fjn.david@gmail.com>
2018-06-22 19:06:48 +0200

    suite

--- theme/foi/src/modules/rubedoBlocks/controllers/ContentDetailController.js
+++ theme/foi/src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -417,7 +417,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scop
 																				predefinedFacets:{"type":"5a114b5c396588e62456706b","numero_foi":config.contentId},
 																				start:0, 
 																				limit:50,
-																				orderby:'ordre',
+																				orderby:'taxonomy.5a114f1b396588d22856706f',
 																				orderbyDirection:'asc',
 																				displayedFacets:"['all']"
 																		};
