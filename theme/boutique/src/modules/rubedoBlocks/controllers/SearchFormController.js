d3858432c497a14b68f6f88dc104baaae80bd23b
Nicolas Rhoné <nicolas.rhone@gmail.com>
2018-09-17 20:52:23 +0200

    suite et fin

--- theme/boutique/src/modules/rubedoBlocks/controllers/SearchFormController.js
+++ theme/boutique/src/modules/rubedoBlocks/controllers/SearchFormController.js
@@ -157,3 +157,124 @@ angular.module("rubedoBlocks").lazy.controller("AuthenticationController",["$sco
         window.location.reload();
     }
 }]);
+angular.module("rubedoBlocks").lazy.controller("ShoppingCartController",["$scope","RubedoPagesService","$rootScope","RubedoShoppingCartService", function($scope,RubedoPagesService,$rootScope,RubedoShoppingCartService){
+    var me = this;
+    var config = $scope.blockConfig;
+
+
+    if (config.cartDetailPage){
+        RubedoPagesService.getPageById(config.cartDetailPage).then(function(response){
+            if (response.data.success){
+                me.cartDetailPageUrl=response.data.url;
+                $scope.rubedo.current.site.cartDetailPageUrl=response.data.url;
+            }
+        });
+    }
+    if (config.checkoutPage){
+        RubedoPagesService.getPageById(config.checkoutPage).then(function(response){
+            if (response.data.success){
+                me.checkoutPageUrl=response.data.url;
+            }
+        });
+    }
+    me.blockId=($scope.block.id);
+    me.cartIsEmpty=false;
+    me.detailedCart={};
+    me.noNone=function(string){
+        if (string!='none'){return string}
+
+    };
+    me.displayCart=function(){
+        RubedoShoppingCartService.getCart({includeDetail:true}).then(
+            function(response){
+                if (response.data.success){
+                    if (response.data.shoppingCart.detailedCart&&response.data.shoppingCart.detailedCart.totalItems>0){
+                        me.detailedCart=response.data.shoppingCart.detailedCart;
+                        me.cartIsEmpty=false;
+                    } else  {
+                        me.cartIsEmpty=true;
+                        me.detailedCart={};
+                    }
+                }
+            }
+        );
+    };
+    me.displayCart();
+    me.addToCart=function(productId,variationId,amount){
+        var options={
+            includeDetail:true,
+            productId:productId,
+            variationId:variationId,
+            amount:amount
+        };
+        RubedoShoppingCartService.addToCart(options).then(
+            function(response){
+                if (response.data.success){
+                    if (response.data.shoppingCart.detailedCart&&response.data.shoppingCart.detailedCart.totalItems>0){
+                        me.detailedCart=response.data.shoppingCart.detailedCart;
+                        me.cartIsEmpty=false;
+                    } else  {
+                        me.cartIsEmpty=true;
+                        me.detailedCart={};
+                    }
+                    $rootScope.$broadcast("shoppingCartUpdated",{emitter:me.blockId});
+                }
+																else {
+																				angular.element('#notEnoughInStock').appendTo('body').modal('show');
+																}
+            }
+        );
+    };
+    me.removeFromCart=function(productId,variationId,amount){
+        var options={
+            includeDetail:true,
+            productId:productId,
+            variationId:variationId,
+            amount:amount
+        };
+        RubedoShoppingCartService.removeFromCart(options).then(
+            function(response){
+                if (response.data.success){
+                    if (response.data.shoppingCart.detailedCart&&response.data.shoppingCart.detailedCart.totalItems>0){
+                        me.detailedCart=response.data.shoppingCart.detailedCart;
+                        me.cartIsEmpty=false;
+                    } else  {
+                        me.cartIsEmpty=true;
+                        me.detailedCart={};
+                    }
+                    $rootScope.$broadcast("shoppingCartUpdated",{emitter:me.blockId});
+                }
+            }
+        );
+    };
+    $scope.$on("shoppingCartUpdated",function(event,args){
+        if (args&&args.emitter!=me.blockId){
+            me.displayCart();
+        }
+    });
+
+}]);
+
+angular.module("rubedoBlocks").lazy.controller("ImageController",["$scope","$http","RubedoPagesService", function($scope,$http,RubedoPagesService){
+    var me = this;
+    var config = $scope.blockConfig;
+    me.imageTitle = "";
+    if (config.externalURL){
+        me.url=config.externalURL;
+    } else if (config.imageLink&&mongoIdRegex.test(config.imageLink)){
+        RubedoPagesService.getPageById(config.imageLink).then(function(response){
+            if (response.data.success){
+                me.url=response.data.url;
+            }
+        });
+    }
+
+    $http.get("/api/v1/media/" + config.imageFile).then(
+        function(response) {
+            if(response.data.success) {
+                me.imageTitle = response.data.media.fields.title;
+            }
+        }
+    );
+
+}]);
