be81af3908568d4ca2e6d3f22133fadcc5c99c2a
Nicolas Rhoné <nicolas.rhone@gmail.com>
2018-09-17 18:46:59 +0200

    controller

--- theme/boutique/src/modules/rubedoBlocks/controllers/SearchFormController.js
+++ theme/boutique/src/modules/rubedoBlocks/controllers/SearchFormController.js
@@ -4,18 +4,19 @@ angular.module("rubedoBlocks").lazy.controller("SearchFormController",['$scope',
     me.show = config.searchPage;
 me.searchQuery="";
     me.placeholder = config.placeholder;
-				var pageId=$scope.rubedo.current.page.id;
+    /*var pageId=$scope.rubedo.current.page.id;
     var siteId=$scope.rubedo.current.site.id;
 	var options = {
         start: 0,
         limit: 1000,
         'fields[]' : ["text"]
-    };
+    };*/
     me.onSubmit = function(){
         var paramQuery = me.query?'?query='+me.query:'';
         RubedoPagesService.getPageById(config.searchPage).then(function(response){
             if (response.data.success){
                 $location.url(response.data.url+paramQuery);
+                $scope.handleCSEvent("useSearch");
             }
         });
     };
