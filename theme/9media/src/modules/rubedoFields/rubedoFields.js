cf139e0bed41ee87c376b793b1555c54587991ab
Mandorlo <shaqed@outlook.fr>
2018-10-02 17:17:24 +0200

    fixed ckeditor fonts

--- theme/9media/src/modules/rubedoFields/rubedoFields.js
+++ theme/9media/src/modules/rubedoFields/rubedoFields.js
@@ -231,7 +231,7 @@
             templates_files: [ '/theme/'+rubedoConfig.siteTheme+'/ckeditor/default.js' ],
             templates_replaceContent : false,
             customConfig: "/theme/"+rubedoConfig.siteTheme+"/ckeditor/config.js",
-            font_names : 'MyanmarText;MyanmarText Bold;FontAwesome;',
+            font_names : 'Calibri;Oswald;HelveticaNeue;MyriadPro;NexaBold;NexaLight;Raanana;RaananaBold;Hautecombe;FontAwesome;',
         };
         if ($scope.field.cType!="CKEField"&&$scope.field.cType!="Rubedo.view.CKEField"){
             editorOptions.removePlugins= 'colorbutton,find,flash,font,' + 'forms,iframe,image,newpage,removeformat' + 'smiley,specialchar,stylescombo,templates,wsc';
