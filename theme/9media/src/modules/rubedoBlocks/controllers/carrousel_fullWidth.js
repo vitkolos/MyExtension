5caa62e8eb412302d062d1f7b45c236034f2d3b3
davidfaijan <fjn.david@gmail.com>
2018-10-03 16:46:19 +0200

    Update carrousel_fullWidth.js

--- theme/9media/src/modules/rubedoBlocks/controllers/carrousel_fullWidth.js
+++ theme/9media/src/modules/rubedoBlocks/controllers/carrousel_fullWidth.js
@@ -1,7 +1,7 @@
 angular.module("rubedoBlocks").lazy.controller("FWCarouselController",["$scope","RubedoContentsService",function($scope,RubedoContentsService){
     var me=this;
     me.contents=[];
-    me.currentSlideIndex=0;
+    me.currentSlideIndex=1;
     var blockConfig=$scope.blockConfig;
     var queryOptions={
         start: blockConfig.resultsSkip ? blockConfig.resultsSkip : 0,
@@ -35,9 +35,11 @@ angular.module("rubedoBlocks").lazy.controller("FWCarouselController",["$scope",
         $scope.clearORPlaceholderHeight();
     };
     me.slideTo=function(index){
+        if (index==0) index=me.contents.length;
         var targetElSelector="#block"+$scope.block.id;
         angular.element(targetElSelector).carousel(index);
         me.currentSlideIndex=index;
+
         
     }
     me.getImageOptions=function(){
