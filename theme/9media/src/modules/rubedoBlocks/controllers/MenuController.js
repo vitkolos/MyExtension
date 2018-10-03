9321b2d2252edd933a103425b94d4c8ba4a9628d
Carlo Baugé <Carlo Baug>
2018-10-03 16:06:56 +0200

    menu design

--- theme/9media/src/modules/rubedoBlocks/controllers/MenuController.js
+++ theme/9media/src/modules/rubedoBlocks/controllers/MenuController.js
@@ -2,78 +2,76 @@ angular.module("rubedoBlocks").lazy.controller("MenuController",['$scope','$root
 	function($scope,$rootScope,$location,$route,RubedoMenuService,RubedoPagesService,$http){
         var DEBUG = true;
         var log = function(msg, o) {if(DEBUG) console.log(msg,o)}
-	var me=this;
-	me.menu={};
-	me.pagesBlocks={};
-	me.currentRouteline=$location.path();console.log('My location', $location.path());
-	var config=$scope.blockConfig;
-	var lang = $route.current.params.lang;
-	me.searchEnabled = (config.useSearchEngine && config.searchPage);
-    
-    log("parentid",$scope.rubedo.current.page.parentId)
-    log("currpageid",$scope.rubedo.current.page.id)
-	if (config.rootPage){
-        var pageId=config.rootPage;
-        log('1', pageId)
-	} else if (config.fallbackRoot&&config.fallbackRoot=="parent"&&mongoIdRegex.test($scope.rubedo.current.page.parentId)){
-        var pageId=$scope.rubedo.current.page.parentId;
-        log('2', pageId)
-	} else {
-        var pageId=$scope.rubedo.current.page.id;
-        log('3', pageId)
-	}
-	me.onSubmit = function(){
-		var paramQuery = me.query?'?query='+me.query:'';
-		RubedoPagesService.getPageById(config.searchPage).then(function(response){
-			if (response.data.success){
-				$location.url(response.data.url+paramQuery);
-			}
-		});
-	};
-    me.currentLang = $route.current.params.lang;
-    log("$route.current", $route.current)
+        var me=this;
+        me.menu={};
+        me.pagesBlocks={};
+        me.currentRouteline=$location.path();console.log('My location', $location.path());
+        var config=$scope.blockConfig;
+        var lang = $route.current.params.lang;
+        me.searchEnabled = (config.useSearchEngine && config.searchPage);
 
-	RubedoMenuService.getMenu(pageId, config.menuLevel).then(function(response){
-		if (response.data.success){
-            console.log('OK RubedoMeuService.getMenu', response)
-			me.menu=response.data.menu;
-			angular.forEach(me.menu.pages, function(page, key) {
-							me.pagesBlocks[key]={};
-							me.pagesBlocks[key]["title"] = page.text;
-							me.pagesBlocks[key]["url"] = page.url;
-							me.pagesBlocks[key]["id"] = page.id;
-							me.pagesBlocks[key].blocks=[]; 
-							var lang = $route.current.params.lang;
-							angular.forEach(page.blocks, function(block, key2){
-								if (block.bType=="contentDetail" && block.orderValue<=1) {
-												if(block.i18n[lang]) me.pagesBlocks[key].blocks.push({"title":block.i18n[lang].title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
-												else me.pagesBlocks[key].blocks.push({"title":block.title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
+        if (config.rootPage){
+            var pageId=config.rootPage;
+            log('1', pageId)
+        } else if (config.fallbackRoot&&config.fallbackRoot=="parent"&&mongoIdRegex.test($scope.rubedo.current.page.parentId)){
+            var pageId=$scope.rubedo.current.page.parentId;
+            log('2', pageId)
+        } else {
+            var pageId=$scope.rubedo.current.page.id;
+            log('3', pageId)
+        }
+        me.onSubmit = function(){
+            var paramQuery = me.query?'?query='+me.query:'';
+            RubedoPagesService.getPageById(config.searchPage).then(function(response){
+                if (response.data.success){
+                    $location.url(response.data.url+paramQuery);
+                }
+            });
+        };
+        me.currentLang = $route.current.params.lang;
+        log("$route.current", $route.current)
 
-												//if(block.i18n[lang]) me.pagesBlocks[key].blocks.push({"title":block.i18n[lang].title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
-												//else me.pagesBlocks[key].blocks.push({"title":block.i18n.fr.title});
-								}
-								else {}
-							});
-			});
-            $scope.clearORPlaceholderHeight();
-            console.log('DEBUG', me)
-		}
-		else {
-            console.log('ERROR in RubedoMenuService.getMenu', response)
-			me.menu={};
-			$scope.clearORPlaceholderHeight();
-		}
-	});
+        RubedoMenuService.getMenu(pageId, config.menuLevel).then(function(response){
+            if (response.data.success){
+                console.log('OK RubedoMeuService.getMenu', response)
+                me.menu=response.data.menu;
+                angular.forEach(me.menu.pages, function(page, key) {
+                                me.pagesBlocks[key]={};
+                                me.pagesBlocks[key]["title"] = page.text;
+                                me.pagesBlocks[key]["url"] = page.url;
+                                me.pagesBlocks[key]["id"] = page.id;
+                                me.pagesBlocks[key].blocks=[]; 
+                                var lang = $route.current.params.lang;
+                                angular.forEach(page.blocks, function(block, key2){
+                                    if (block.bType=="contentDetail" && block.orderValue<=1) {
+                                                    if(block.i18n[lang]) me.pagesBlocks[key].blocks.push({"title":block.i18n[lang].title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
+                                                    else me.pagesBlocks[key].blocks.push({"title":block.title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
 
-	$rootScope.toggleNav = "false";
-	$rootScope.ToggleNav = function(){$rootScope.toggleNav = !$rootScope.toggleNav};
-	/*Ajouter les traductions*/
-	$scope.rubedo.getCustomTranslations = function(){
-		$http.get('/theme/'+window.rubedoConfig.siteTheme+'/localization/'+lang+'/Texts.json').then(function(res){
-			$scope.rubedo.translations = JSON.parse((JSON.stringify($scope.rubedo.translations) + JSON.stringify(res.data)).replace(/}{/g,","))
-		});	
-	}
-	$scope.rubedo.getCustomTranslations();
+                                                    //if(block.i18n[lang]) me.pagesBlocks[key].blocks.push({"title":block.i18n[lang].title,"code":(block.code).split("/")[1],"order":(block.code).split("/")[0]});
+                                                    //else me.pagesBlocks[key].blocks.push({"title":block.i18n.fr.title});
+                                    }
+                                    else {}
+                                });
+                });
+                $scope.clearORPlaceholderHeight();
+                console.log('DEBUG', me)
+            }
+            else {
+                console.log('ERROR in RubedoMenuService.getMenu', response)
+                me.menu={};
+                $scope.clearORPlaceholderHeight();
+            }
+        });
+
+        $rootScope.toggleNav = "false";
+        $rootScope.ToggleNav = function(){$rootScope.toggleNav = !$rootScope.toggleNav};
+        /*Ajouter les traductions*/
+        $scope.rubedo.getCustomTranslations = function(){
+            $http.get('/theme/'+window.rubedoConfig.siteTheme+'/localization/'+lang+'/Texts.json').then(function(res){
+                $scope.rubedo.translations = JSON.parse((JSON.stringify($scope.rubedo.translations) + JSON.stringify(res.data)).replace(/}{/g,","))
+            });	
+        }
+        $scope.rubedo.getCustomTranslations();
 
 }]);
 
@@ -144,7 +142,7 @@ angular.module("rubedoBlocks").lazy.controller("LanguageMenuController", ['$scop
                 });
             }
         };
-        log('DEBUG LANG', me)
+        console.log('DEBUG LANG', me)
         $scope.clearORPlaceholderHeight();
     }]);       
 
