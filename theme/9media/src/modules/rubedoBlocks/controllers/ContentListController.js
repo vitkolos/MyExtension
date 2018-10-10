50cc594ed96415921ea157d7c1a7425065b170f2
Mandorlo <shaqed@outlook.fr>
2018-10-10 16:58:40 +0200

    9media contentlist

--- theme/9media/src/modules/rubedoBlocks/controllers/ContentListController.js
+++ theme/9media/src/modules/rubedoBlocks/controllers/ContentListController.js
@@ -41,6 +41,7 @@ angular.module("rubedoBlocks").lazy.controller("ContentListController",['$scope'
     me.initTemplates = function() {
         me.currContentType = "56ab6e94c445ecde138b4e2b"; // = Lien vers page
         if (me.contents && me.contents.length > 0) me.currContentType = me.contents[0].typeId;
+        console.log('template debug', me.currContentType, CONTENT_TYPES, CONTENT_TYPES[me.currContentType])
         if (CONTENT_TYPES[me.currContentType]) {
             me.templateUrl = me.buildTemplateUrl(CONTENT_TYPES[me.currContentType].path);
         } else {
