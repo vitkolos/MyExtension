f5ed88afe218a961cbd6dd1557257bf84e74b280
Mandorlo <shaqed@outlook.fr>
2018-10-26 19:07:34 +0200

    add payment mean option for cana

--- theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/InscriptionController.js
@@ -12,6 +12,8 @@ angular.module("rubedoBlocks").lazy.controller("InscriptionController",['$scope'
     me.paiment_complementaire= themePath+'/templates/blocks/formulaire/paiment_complementaire.html';
     me.content = angular.copy($scope.proposition);
 
+    window.$http = $http;
+
     console.log("CONTENT1", me.content);
     if (me.content.fields && !me.content.fields.moyens_paiement) me.content.fields.moyens_paiement = [];
     if (me.content.fields.moyens_paiement.length == 1) me.inscription.modePaiement = me.content.fields.moyens_paiement[0];
