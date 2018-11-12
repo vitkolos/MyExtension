1288c32faff599d1a281bdf858122b86607393ba
Mandorlo <shaqed@outlook.fr>
2018-11-12 21:22:32 +0100

    debug cluny menu lang

--- theme/cluny/src/modules/rubedoBlocks/controllers/MenuController.js
+++ theme/cluny/src/modules/rubedoBlocks/controllers/MenuController.js
@@ -30,7 +30,6 @@ angular.module("rubedoBlocks").lazy.controller("MenuController",['$scope','$root
         let currentLang = $route.current.params.lang;
         if(lang == currentLang) return me.currentRouteline;
         let re = new RegExp("^/" + currentLang + "/", "gi");
-        console.log("new path : ", lang, $location.path(), $location.path().replace(re, `/${lang}/`))
         return $location.path().replace(re, `/${lang}/`)
     }
 
