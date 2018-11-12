6ea0a7778d872a2d89fc836ded45d8e594f85e32
Mandorlo <shaqed@outlook.fr>
2018-11-12 21:09:46 +0100

    debug cluny menu lang

--- theme/cluny/src/modules/rubedoBlocks/controllers/MenuController.js
+++ theme/cluny/src/modules/rubedoBlocks/controllers/MenuController.js
@@ -28,6 +28,8 @@ angular.module("rubedoBlocks").lazy.controller("MenuController",['$scope','$root
     console.log("$scope", $scope, $route, $location)
     me.changeLangUrl = function(lang) {
         if(lang != me.currentLang.lang) return me.currentRouteline;
+        let re = new RegExp("^/" + lang + "/", "gi");
+        return $location.path().replace(re, `/${lang}/`)
     }
 
     RubedoMenuService.getMenu(pageId, config.menuLevel).then(function(response){
