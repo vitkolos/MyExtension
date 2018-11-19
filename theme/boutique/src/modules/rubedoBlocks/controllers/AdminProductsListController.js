121e9f5c73bfed27d00d8a92fd23c39fbfe20d4f
Mandorlo <shaqed@outlook.fr>
2018-11-19 15:28:41 +0100

    add boutique products administration

--- theme/boutique/src/modules/rubedoBlocks/controllers/AdminProductsListController.js
+++ theme/boutique/src/modules/rubedoBlocks/controllers/AdminProductsListController.js
@@ -1,20 +1,31 @@
 angular.module("rubedoBlocks").lazy.controller('AdminProductsListController',['$scope', '$http', 'RubedoOrdersService',function($scope, $http, RubedoOrdersService){
     var me = this;
     
-    me.fields = [{id: 'all', label: 'Tout'}, {id: 'text', label: 'Nom de produit'}, {id: 'typeId', label: 'Type de produit'}]
-    me.productTypes = [
-        {id: "", label: ""},
-        {id: "56014a7d45205e42506e124b", label: "Musculine"},
-        {id: "55c87ae245205e8019c62e08", label: "20 Produits boutique"},
-        {id: "56bcb849c445ec56018b5051", label: "Pdf Boutique"},
+    me.fields = [
+        //{id: 'all', label: 'Tout'}, 
+        {id: 'text', label: 'Nom de produit'}, 
+        {id: 'typeId', label: 'Type de produit'},
+        {id: 'status', label: 'Statut'},
     ]
+    me.subfields = {
+        typeId: [
+            {id: "", label: ""},
+            {id: "56014a7d45205e42506e124b", label: "Musculine"},
+            {id: "55c87ae245205e8019c62e08", label: "20 Produits boutique"},
+            {id: "56bcb849c445ec56018b5051", label: "Pdf Boutique"},
+        ],
+        status: [
+            {id: 'published', label: "Publié"},
+            {id: 'draft', label: "Brouillon"},
+        ]
+    }
 
     me.products = [];
     me.allProducts = [];
     me.loading = false;
     me.search_text = "";
-    me.search_field = "all";
-    me.search_product_type = "";
+    me.search_field = "";
+    me.search_subfield = "";
 
     
     var config = $scope.blockConfig;
@@ -29,9 +40,15 @@ angular.module("rubedoBlocks").lazy.controller('AdminProductsListController',['$
         let keyCode = $event.which || $event.keyCode;
         if (keyCode !== 13) return;
 
-        let texte = (me.search_field == 'typeId' && me.search_product_type) ? me.search_product_type: me.search_text;
-        me.products = me.allProducts.filter(el => el[me.search_field] && el[me.search_field].indexOf(texte) >= 0);
-        console.log(me.search_field, texte);
+        if (me.subfields[me.search_field] && me.search_subfield) {
+            console.log('in subfield search fun')
+            me.products = me.allProducts.filter(el => el[me.search_field] && el[me.search_field] == me.search_subfield && (!el['text'] || el['text'].indexOf(me.search_text) >= 0));
+        } else if (me.search_field != 'all') {
+            me.products = me.allProducts.filter(el => el[me.search_field] && new RegExp(me.search_text, 'gi').test(el[me.search_field]) >= 0);
+        } else {
+            // search_field == all
+
+        }
         console.log("new product list", me.products.length, me.products);
     }
 
