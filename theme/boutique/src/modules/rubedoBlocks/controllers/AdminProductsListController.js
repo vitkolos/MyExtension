c19c1fa134417fa126eef653279a73502746edab
Mandorlo <shaqed@outlook.fr>
2018-11-19 16:41:30 +0100

    add boutique products administration

--- theme/boutique/src/modules/rubedoBlocks/controllers/AdminProductsListController.js
+++ theme/boutique/src/modules/rubedoBlocks/controllers/AdminProductsListController.js
@@ -16,7 +16,7 @@ angular.module("rubedoBlocks").lazy.controller('AdminProductsListController',['$
             {id: "56bcb849c445ec56018b5051", label: "Pdf Boutique"},
             {id: "575e74b124564068118bfd42", label: "5.5 Librairie"},
             {id: "57c93550245640eb2b8c0cba", label: "2.1 Revue FOI"},
-            {id: "580b29be24564092558b95fd", lable: "20 Films Net for God"},
+            {id: "580b29be24564092558b95fd", label: "20 Films Net for God"},
             {id: "5810a72f24564001018bc024", label: "5.5 Epicerie"},
         ],
         status: [
@@ -62,10 +62,9 @@ angular.module("rubedoBlocks").lazy.controller('AdminProductsListController',['$
         console.log("new product list", me.products.length, me.products);
     }
 
-    $http({url: '/backoffice/contents/get-stock', method:'GET', params: {_dc:'1542638855666', 'type-id':'55c87ae245205e8019c62e08','page':1,'start':0,'limit':100000,'workingLanguage':'fr'}}).then(res => console.log('sku', res)).catch(err => console.log('sku err', err))
-
     me.getProducts = async function() {
         me.loading = true;
+        // on récupère tous les produits
         let result = await $http({
             url: '/api/v1/ecommerce/products',
             method: "GET",
@@ -79,24 +78,59 @@ angular.module("rubedoBlocks").lazy.controller('AdminProductsListController',['$
                 limit: 1000
             }
         });
-        me.loading = false;
         me.products = result.data.contents.slice(0, 10);
         me.allProducts = result.data.contents;
+
+        // on récupère les niveaux de stock pour chaque type de produit
+        let typeid_list = me.subfields.typeId.map(el => el.id).filter(el => !!el);
+        let plist = typeid_list.map(tid => {
+            return $http({url: '/backoffice/contents/get-stock', method:'GET', params: {_dc:'1542638855666', 'type-id':tid, 'page':1,'start':0,'limit':100000,'workingLanguage':'fr'}})
+        })
+        let stock_list = await Promise.all(plist);
+        stock_list = flatten(stock_list);
+        console.log('stocks', stock_list)
+
+        me.allProducts = mergeList(result.data.contents, 'stock', stock_list, 'id');
+        me.products = me.allProducts.slice(0, 10);
+
+        me.loading = false;
         return result;
     }
     me.getProducts().then(res => console.log('products', res, me)).catch(err => console.log('err products', err));
+
+
+    // ==========================================================
+    //                  HELPER FUNCTIONS
+    // ==========================================================
+
+    // merge l2 in l1 on id_attr : l1[i][attr] = l2[j]
+    function mergeList(l1, attr, l2, id_attr) {
+        for (let i = 0; i < l1.length; i++) {
+            let el2 = l2.find(el => el[id_attr] == l1[i][id_attr]);
+            if (el2) l1[i][attr] = el2;
+        }
+        return l1
+    }
+
+    function flatten(arr) {
+        let newarr = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) newarr = newarr.concat(arr[i]);
+            else newarr.push(arr[i]);
+        }
+        return newarr;
+    }
 
     function RemoveAccents(str) {
         var accents    = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
         var accentsOut = "AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz";
         str = str.split('');
-        var strLen = str.length;
-        var i, x;
-        for (i = 0; i < strLen; i++) {
-          if ((x = accents.indexOf(str[i])) != -1) {
-            str[i] = accentsOut[x];
-          }
+        let x;
+        for (let i = 0; i < str.length; i++) {
+            if ((x = accents.indexOf(str[i])) != -1) {
+                str[i] = accentsOut[x];
+            }
         }
         return str.join('');
-      }
+    }
 }]);
