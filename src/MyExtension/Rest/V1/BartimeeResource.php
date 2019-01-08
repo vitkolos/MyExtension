879ee624b80e2a72ef6f4713e4ffa2d83d39f54d
Mandorlo <shaqed@outlook.fr>
2019-01-08 14:51:18 +0100

    debug Bartimee

--- src/MyExtension/Rest/V1/BartimeeResource.php
+++ src/MyExtension/Rest/V1/BartimeeResource.php
@@ -124,7 +124,8 @@ class BartimeeResource extends AbstractResource
         //$sort = array(["property"=>"text","direction"=>"DESC"]);
         try {
             $query=$this->getQueriesCollection()->findById("56502ef945205ea962ebefd0");
-            $myfilter = Filter::factory('Value')->setName('typeId')->setValue("5652dcb945205e0d726d6caf");
+            //$myfilter = Filter::factory('Value')->setName('typeId')->setValue("5652dcb945205e0d726d6caf");
+            $myfilter = $this->getQueriesCollection()->getFilterArrayByQuery($query);
             $contextualContent=serialize($myfilter);//$this->getContentsCollection()->getOrderedList($myfilter); //getByType("5652dcb945205e0d726d6caf");
             return [
                 'success' => false,
