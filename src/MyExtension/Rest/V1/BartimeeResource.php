47fa5513900ce7a73ce58fa0a54cb37a402716ef
Mandorlo <shaqed@outlook.fr>
2019-01-08 14:17:07 +0100

    debug Bartimee

--- src/MyExtension/Rest/V1/BartimeeResource.php
+++ src/MyExtension/Rest/V1/BartimeeResource.php
@@ -119,8 +119,8 @@ class BartimeeResource extends AbstractResource
         $output['currentUser'] = $rightsSubRequest->getVariables()['currentUser'];
 
         // test request contents directly
-        $contextualContent=$this->getContentsCollection()->findById("54818dd345205e1f538b456c", true, false);
-        file_put_contents('/var/www/html/rubedo/log/custom_debug.log', date("Y-m-d H:i") . " -- BartimeeResource.php > contents query ".json_encode(get_class_methods($this->getContentsCollection()))."\n", FILE_APPEND | LOCK_EX);            
+        $contextualContent=$this->getContentsCollection()->getByType("5652dcb945205e0d726d6caf");
+        file_put_contents('/var/www/html/rubedo/log/custom_debug.log', date("Y-m-d H:i") . " -- BartimeeResource.php > contents query ".json_encode($contextualContent)."\n", FILE_APPEND | LOCK_EX);            
 
         $this->_dataService = Manager::getService('ContentTypes');
         
