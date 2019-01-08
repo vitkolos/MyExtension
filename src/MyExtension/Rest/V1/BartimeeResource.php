3171f4be57dbcbfc8ca8dffee87e852aff2de651
Mandorlo <shaqed@outlook.fr>
2019-01-08 14:37:38 +0100

    debug Bartimee

--- src/MyExtension/Rest/V1/BartimeeResource.php
+++ src/MyExtension/Rest/V1/BartimeeResource.php
@@ -122,8 +122,9 @@ class BartimeeResource extends AbstractResource
         //$filter = array();
         //$sort = array(["property"=>"text","direction"=>"DESC"]);
         try {
-            $contextualContent=$this->getContentsCollection()->getOrderedList(); //getByType("5652dcb945205e0d726d6caf");
-            file_put_contents('/var/www/html/rubedo/log/custom_debug.log', date("Y-m-d H:i") . " -- BartimeeResource.php > contents query ".json_encode($contextualContent)."\n", FILE_APPEND | LOCK_EX);    
+            $a=1;
+            //$contextualContent=$this->getContentsCollection()->getOrderedList(); //getByType("5652dcb945205e0d726d6caf");
+            //file_put_contents('/var/www/html/rubedo/log/custom_debug.log', date("Y-m-d H:i") . " -- BartimeeResource.php > contents query ".json_encode($contextualContent)."\n", FILE_APPEND | LOCK_EX);    
         } catch (Exception $e) {
             file_put_contents('/var/www/html/rubedo/log/custom_debug.log', date("Y-m-d H:i") . " -- BartimeeResource.php > contents query ERROR \n", FILE_APPEND | LOCK_EX);
             return [
