b6368fe317873958aececb8d1608369ac4c6ccce
Carlo Baugé <shaqed@outlook.fr>
2019-10-28 17:15:04 +0100

    add week menu for bethanien

--- theme/bethanien/js/bethanien.js
+++ theme/bethanien/js/bethanien.js
@@ -223,7 +223,7 @@ angular.module('rubedoBlocks').directive('menuweek', function() {
       // build templates
       element.html('');
       for (menu of menus) {
-        element.append($(`<div class="col col-xs-12 col-md-3 menuday">
+        element.append($(`<div class="col col-xs-12 col-md-6 menuday">
             <div class="menuprice">${menu.price}</div>
             <h2>${menu.title|moment(menu.date, 'YYYYMMDD').format('dddd')}</h2>
             <img src="/theme/bethanien/img/olivier.png" alt="olivetree" class="olivier1">
