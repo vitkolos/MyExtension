fef6879091c22481109461d3311d47c3d2c324ba
Carlo Baugé <shaqed@outlook.fr>
2019-10-29 10:35:01 +0100

    add rgpd for abuna faraj

--- theme/cte/src/modules/rubedoBlocks/controllers/simpleContact.js
+++ theme/cte/src/modules/rubedoBlocks/controllers/simpleContact.js
@@ -1,9 +1,17 @@
-angular.module("rubedoBlocks").lazy.controller('ContactBlockController',['$scope','$location','$filter','RubedoMailService',function($scope,$location,$filter,RubedoMailService){
+angular.module("rubedoBlocks").lazy.controller('ContactBlockController',['$scope','$location','$filter','RubedoMailService','RubedoRgpd',function($scope,$location,$filter,RubedoMailService,RubedoRgpd){
     var me = this;
     var config = $scope.blockConfig;
     me.contactData={ };
     me.contactError=null;
     $scope.clearORPlaceholderHeight();
+
+    // RGPD
+    console.log("Init RGPD")
+    $scope.parameters = {'rgpd_media_id': ''}
+    RgpdService.getPolitiqueConfidentialiteId().then(id => {
+        $scope.parameters.rgpd_media_id = id;
+    })
+
     me.dateDiffence = function(start,end){
         console.log($filter('number')((end-start)/(3600*24*1000),0));
         return $filter('number')((end-start)/(3600*24*1000),0);
