13cd401fa0d648da60ba82a7296de094b9c4d283
Carlo Baugé <shaqed@outlook.fr>
2019-11-14 22:17:59 +0100

    add uploadXLS block

--- theme/netforgod/src/modules/rubedoBlocks/controllers/uploadXlsController.js
+++ theme/netforgod/src/modules/rubedoBlocks/controllers/uploadXlsController.js
@@ -27,17 +27,29 @@ function($scope, $http, RubedoPagesService, RubedoContentsService, RubedoOrdersS
     async function updatePNs(onesime_pns) {
         // the id below is the id of the Rubedo Query called "Points Net", it returns all Points Net in Rubedo
         let rubedo_pns = await RubedoContentsService.getContents("5dcdbf568e707529379d34b1",null,null,{limit:20000});
+        if (rubedo_pns.status != 200) return log("error", rubedo_pns);
+        if (!rubedo_pns.data.success) return log("error", rubedo_pns);
+        rubedo_pns = rubedo_pns.data.contents;
 
         let o_pn = onesime_pns[0];
-        $scope.logs.push({'level': 'info', 'msg': `updating PN ${o_pn['Code PN']}`});
+        log('info', `updating PN ${o_pn['Code PN']}`);
         let r_pn = rubedo_pns.filter(pn => pn.fields.pointNetId == o_pn['Code PN']);
         if (r_pn.length == 0) {
-            $scope.logs.push({'level': 'info', 'msg': `PN ${o_pn['Code PN']} does not exist yet`});
+            log('info', `PN ${o_pn['Code PN']} does not exist yet`);
         } else if (r_pn.length > 1) {
-            $scope.logs.push({'level': 'warning', 'msg': `There are ${r_pn.length} PN with the code ${o_pn['Code PN']}. All will be updated`});
+            log('warning', `There are ${r_pn.length} PN with the code ${o_pn['Code PN']}. All will be updated`);
         } else {
             
         }
         console.log(o_pn, r_pn);
+    }
+
+    function log(level, data) {
+        level = levle.toLowerCase();
+        let data_s = (typeof data == 'string') ? data: JSON.stringify(data);
+        $scope.logs.push({level, msg: data_s});
+        if (level == "error" || level == "err") console.error(data);
+        else if (level == "warning" || level == 'warn') console.warn(data);
+        else console.log(data);
     }
 }]);
