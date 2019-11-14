94dccc27880416bf86ac82123cef1321f872ec88
Carlo Baugé <shaqed@outlook.fr>
2019-11-14 20:32:23 +0100

    add uploadXLS block

--- theme/netforgod/src/modules/rubedoBlocks/controllers/uploadXlsController.js
+++ theme/netforgod/src/modules/rubedoBlocks/controllers/uploadXlsController.js
@@ -1,4 +1,22 @@
 angular.module("rubedoBlocks").lazy.controller('uploadXlsController',['$scope', '$http', 'RubedoPagesService', 'RubedoContentsService', 'RubedoOrdersService',
 function($scope, $http, RubedoPagesService, RubedoContentsService, RubedoOrdersService){
     console.log("UploadXlsController")
+
+    $scope.uploadXLS = function() {
+        let f = document.getElementById('file').files[0],
+        r = new FileReader();
+
+        r.onloadend = function(e) {
+            var data = e.target.result;
+            //send your binary data via $http or $resource or do anything else with it
+            let d = r.readAsBinaryString(data);
+            console.log("loadend", d);
+        }
+
+        r.onload = function () {
+            console.log(reader.result);
+        }
+
+        r.readAsBinaryString(f);
+    }
 }]);
